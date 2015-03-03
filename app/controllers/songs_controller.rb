d9d2a2a6c73ed1a357df42919661d61d6ed44b9c
Rose Weixel <rose.weixel@gmail.com>
2015-03-03 14:15:25 -0500

    fix solution

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,12 @@
-require 'rack-flash'
-
 class SongsController < ApplicationController
-  use Rack::Flash
-
   get '/songs' do
     @songs = Song.all
-    erb :'/songs/index'
+
+    erb :'songs/index'
   end
 
   get '/songs/new' do
-    erb :'/songs/new'
+    erb :'songs/new'
   end
 
   get '/songs/:slug' do
@@ -19,30 +16,29 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.genre_ids = params[:genres]
+    @song = Song.create(params[:song])
+
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
 
-    flash[:message] = "Successfully created song."
-
-    redirect("/songs/#{@song.slug}")
+    erb :'songs/show', locals: {message: "Successfully created song."}
   end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+
     erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    @song.genres.clear
     @song.update(params[:song])
+
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    @song.genre_ids = params[:genres]
     @song.save
 
-    flash[:message] = "Successfully updated song."
-    redirect("/songs/#{@song.slug}")
+    erb :'songs/show', locals: {message: "Song successfully updated."}
   end
 
 end
