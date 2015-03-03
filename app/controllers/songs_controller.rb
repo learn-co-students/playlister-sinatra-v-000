f1977f588a69dc276e4bca7ae75df8037b136442
Rose Weixel <rose.weixel@gmail.com>
2015-03-03 13:23:16 -0500

    solution

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,44 @@
+class SongsController < ApplicationController
+  get '/songs' do
+    @songs = Song.all
+
+    erb :'songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    erb :'songs/show', locals: {message: "Successfully created song."}
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    #binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.genres.clear
+    @song.update(params[:song])
+
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    erb :'songs/show', locals: {message: "Song successfully updated."}
+  end
+
+end
