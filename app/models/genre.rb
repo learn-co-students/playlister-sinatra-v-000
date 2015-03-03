7c00f1dba8f1315c852e05c85e53e2fded1fb35a
Rose Weixel <rose.weixel@gmail.com>
2015-03-03 13:23:16 -0500

    solution

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,13 +1,7 @@
 class Genre < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
-
-  def slug
-    name.downcase.tr(' ', '-')
-  end
-
-  def self.find_by_slug(slug)
-    Genre.all.find { |genre| genre.slug == slug }
-  end
 end
