960ba9be8987e85423a2e97b007379013c630753
deniznida <denizunat@hotmail.com>
2015-06-29 09:43:49 -0400

    add solutions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302182247) do
+ActiveRecord::Schema.define(version: 20150629133457) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
