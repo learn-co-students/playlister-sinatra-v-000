192d0ea923355601961432eff42195c6295f0a86
deniznida <denizunat@hotmail.com>
2015-06-29 09:43:49 -0400

    add solutions

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,13 @@
 class Song < ActiveRecord::Base
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
-  
+  belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
 
-  belongs_to :artist
+  def slug
+    name.downcase.tr(' ', '-')
+  end
 
+  def self.find_by_slug(slug)
+    Song.all.find { |song| song.slug == slug }
+  end
 end
