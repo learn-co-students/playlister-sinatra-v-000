db7665190d6dd6e65d552fdb287db795bd3333cf
deniznida <denizunat@hotmail.com>
2015-06-29 09:43:49 -0400

    add solutions

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,7 +7,6 @@ class GenresController < ApplicationController
 
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params[:slug])
-
-    erb :'/genres/show'
+    erb :'genres/show'
   end
 end
