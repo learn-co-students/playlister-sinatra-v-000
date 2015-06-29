f1e4a6271a36561b3708032fb9594279d8256057
deniznida <denizunat@hotmail.com>
2015-06-29 09:43:49 -0400

    add solutions

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,16 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
-  has_many :genres, :through => :song_genres
+  has_many :genres, through: :song_genres
 
-# We are repeating a ton of code here. These two methods could go into a module.
-
-  def slug 
-    name.downcase.gsub(" ","-")
+  def slug
+    name.downcase.tr(' ', '-')
   end
 
   def self.find_by_slug(slug)
-    Song.all.find{|song| song.slug == slug}
+    Song.all.find { |song| song.slug == slug }
   end
 end
 
