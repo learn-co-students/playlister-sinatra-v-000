58985339c2232c5afaa51817d957cadb539fc54e
Deniz Unat <deniznida@users.noreply.github.com>
2015-07-02 13:45:10 -0400

    Update song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
-  has_many :genres, through: :song_genres
+  has_many :genres, :through => :song_genres
 
   def slug
-    name.downcase.tr(' ', '-')
+    name.downcase.gsub(" ","-")
   end
 
   def self.find_by_slug(slug)
-    Song.all.find { |song| song.slug == slug }
+    Song.all.find{|song| song.slug == slug}
   end
 end
 
