a9bf8d5c8832033360d33bac54592ab907b1b419
Deniz Unat <deniznida@users.noreply.github.com>
2015-07-07 11:04:05 -0400

    Update song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,8 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, :through => :song_genres
 
+# We are repeating a ton of code here. These two methods could go into a module.
+
   def slug 
     name.downcase.gsub(" ","-")
   end
