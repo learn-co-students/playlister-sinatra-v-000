c6c6478a40588e28eb7df472ea170dc73e77fd5e
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-09 09:58:27 -0500

    issues 16, 17, 19

--- spec/features/04_basic_view_spec.rb
+++ spec/features/04_basic_view_spec.rb
@@ -16,7 +16,7 @@ describe "Playlister Basics" do
     @song.save
   end
 
-  context "index pages" do
+  describe "index pages" do
     describe "/songs" do
       before do
         visit "/songs"
@@ -50,7 +50,7 @@ describe "Playlister Basics" do
         expect(page).to have_content(artist_name)
       end
 
-      it "contains links to each song's show page" do
+      it "contains links to each artist's show page" do
         expect(page).to have_css("a[href='/artists/#{@artist.slug}']")
       end
     end
@@ -69,13 +69,13 @@ describe "Playlister Basics" do
         expect(page).to have_content(genre_name)
       end
 
-      it "contains links to each song's show page" do
+      it "contains links to each genre's show page" do
         expect(page).to have_css("a[href='/genres/#{@genre.slug}']")
       end
     end
   end
 
-  context "show pages" do
+  describe "show pages" do
     describe "/songs/:slug" do
       before do
         visit "/songs/#{@song.slug}"
