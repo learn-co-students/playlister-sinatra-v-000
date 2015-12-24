9db52e28fc143c893080cd5c07d4cb2639b53109
onyoo <roberto9m9@gmail.com>
2015-12-24 15:17:11 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,48 +8,54 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/songs' do
-    erb :songs
+    erb :"songs/songs"
   end
 
   get '/artists' do
-    erb :artists
+    erb :"artists/artists"
   end
 
   get '/genres' do
-    erb :genres
+    erb :"genres/genres"
   end
 
   get '/songs/new' do
-    erb :new_song
+    erb :"songs/new_song"
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb :song_slug
+    erb :"songs/song_slug"
   end
 
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params[:slug])
-    erb :genre_slug
+    erb :"genres/genre_slug"
   end
 
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
-    erb :artist_slug
+    erb :"artists/artist_slug"
   end
 
   post '/songs/new' do
-    @song = Song.first_or_create(name: params["Name"], artist: params["Artist Name"], genre: params["genre"])
-    redirect '/songs'
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    params["genre"].each{|g| @song.genres << Genre.find_or_create_by(name: g)}
+    @song.save
+    erb :"songs/song_slug", :locals => {:message => "Successfully created song."}
   end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    erb :edit_song
+    erb :"songs/edit_song"
   end
 
   post '/songs/:slug/edit' do
-    @song = Song.find_by_slug(params[:slug])
-    @song.update(name: params["Name"], artist: params["Artist Name"], genre: params["genre"])
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    params["genre"].each{|g| @song.genres << Genre.find_or_create_by(name: g)}
+    @song.save
+    erb :"songs/song_slug", :locals => {:message => "Song successfully updated."}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# Add seed data here. Seed your database with `rake db:seed`
 
 Song.delete_all
 Genre.delete_all
 Artist.delete_all
 
-thriller = Song.create(:name=>'Thriller')
-other_song = Song.create(:name=>'other_song')
 
-mj = Artist.create(:name=>'Michael Jackson')
-not_so_good = Artist.create(:name=>'not_so_good')
-
-New_Age_Garbage = Genre.create(:name=>'New Age Garbage')
-pop = Genre.create(:name=>'Pop')
-Hippity_Hop = Genre.create(:name=>'Hippity Hop')
+genre_1 = Genre.create(name: "New Age Garbage")
+genre_1 = Genre.create(name: "Hippity Hop")
