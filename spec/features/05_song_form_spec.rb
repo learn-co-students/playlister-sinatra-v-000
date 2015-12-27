a0a86afe490f082857a5b66a13d881f458b6c43b
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-12-27 17:04:44 -0500

    made last test of edit form for song more rigorous prev. test would fill out form but not test if song's show page reflected the changes made by what was filled out in form. Now, tests for show page reflecting new genre that was selected via edit form

--- spec/features/05_song_form_spec.rb
+++ spec/features/05_song_form_spec.rb
@@ -124,6 +124,7 @@ describe "Song Forms" do
         expect(page).to have_content(song_name)
         expect(page).to have_content(artist_name)
         expect(page).to have_content("Hippity Hop")
+<<<<<<< HEAD
         expect(page).not_to have_content("New Age Garbage")
       end
 
@@ -132,6 +133,8 @@ describe "Song Forms" do
         check "New Age Garbage"
         click_on "Save"
         expect(page.current_path).to eq("/songs/that-one-with-the-guitar")
+=======
+>>>>>>> made last test of edit form for song more rigorous
       end
     end
   end
