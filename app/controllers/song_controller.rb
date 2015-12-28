b10432ad90375dbeaaa0e1bb46a35cc966d604f3
frankNowinski <nowinski.frank@gmail.com>
2015-12-28 07:02:52 -0500

    complete rough copy

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -2,12 +2,40 @@ class SongController < ApplicationController
 
   get '/songs' do
     @songs = Song.all.sort_by(&:name)
-    erb :'songs/index'
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @genre = Genre.all.find(SongGenre.all.find(@song.id).genre_id)
     erb :'songs/show'
   end
+
+  post '/songs' do
+    @song = Song.create(name: params[:song_name])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.genre_ids = params[:genre]
+    @song.save
+
+    erb :'songs/show', :locals => { success: "Successfully created song." }
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.all.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.genre_ids = params[:genre]
+    @song.save
+
+    erb :'songs/show', :locals => { success: "Song successfully updated." }
+  end
+
+
 end
