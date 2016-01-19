990f618524b6f89b0e4a4cbfabf3198bdf0892ef
dannyd4315 <dannyd4315@gmail.com>
2016-01-20 10:37:38 +1100

    Controllers and Views updated

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,20 @@
+class SongsController < ApplicationController
+
+  get '/songs' do 
+    @song = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs/:slug' do 
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+
+  end  
+end
