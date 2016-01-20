45e70e90c45929b3248c8ccba480e031d8966e36
Alvinluipo <lualvin1234@gmail.com>
2016-01-20 17:25:20 -0500

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,14 @@
+class GenresController < ApplicationController
+
+  get '/genres' do 
+    @genres = Genre.all 
+    erb :'/genres/genres'
+  end
+
+  get '/genres/:slug' do 
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
+
+
+end
---
+++ app/models/song_genres.rb
@@ -0,0 +1,15 @@
+
+class SongGenre < ActiveRecord::Base
+
+  belongs_to :song
+  belongs_to :genre
+
+  def slug
+    name.downcase.strip.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|song_genre| song_genre.slug == slug}
+  end
+
+end
