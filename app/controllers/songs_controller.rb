512b40501180179c8515f1e0a744423f9da37b67
dannyd4315 <dannyd4315@gmail.com>
2016-01-20 12:51:13 +1100

    Done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,20 +1,40 @@
+require 'pry'
+
 class SongsController < ApplicationController
 
   get '/songs' do 
-    @song = Song.all
+    @songs = Song.all
     erb :'songs/index'
   end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end  
 
   get '/songs/:slug' do 
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
 
-  get '/songs/new' do
-    erb :'/songs/new'
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end  
+
+  post '/songs' do 
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    erb :'songs/show', locals: {message: "Successfully created song."}
   end
 
-  post '/songs' do
+  patch '/songs/:slug' do 
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    # binding.pry
+    @song.save
+    erb :'songs/show', locals: {message: "Song successfully updated."}
+  end
 
-  end  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  
+
   has_many :song_genres
   belongs_to :artist
   has_many :genres, through: :song_genres
