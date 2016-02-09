0f6080f6d5b4d599b2c3c5a9d25032adaaa5f799
Kevin McCormack <wittyman37@yahoo.com>
2016-02-09 09:37:57 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,14 +6,35 @@ class SongsController < Sinatra::Base
     erb :index
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    #binding.pry
-    erb :show
+  get '/songs/new' do
+    erb :new
   end
 
   post '/songs' do
   #code to create a new song and save to DB
+  song = Song.find_or_create_by_name("params[:song_name]")
+  song.artist = Artist.find_or_create_by_name("params[:artist_name]")
+  song.genres = params[:genres]
   erb :'songs/show', locals: {message: "Successfully created song."}
+end
+
+get '/songs/:slug' do
+  @song = Song.find_by_slug(params[:slug])
+  #binding.pry
+  erb :show
+end
+
+patch '/songs/:slug' do
+  song = Song.find_by_slug(params[:slug])
+  song.name = params[:song_name]
+  song.artist = Artist.find_or_create_by_name("params[:artist_name]")
+  song.genres = params[:genres]
+
+  redirect to "/songs/#{song.slug}"
+end
+
+get '/songs/:slug/edit' do
+  @song = Song.find_by_slug(params[:slug])
+  erb :edit
 end
 end
