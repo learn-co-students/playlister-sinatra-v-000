cc4e699af0d92f30b9b920c0de5dd920eedf2cc9
Adam King <adam.king0126@gmail.com>
2016-02-12 13:19:46 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
@@ -12,9 +13,39 @@ class ApplicationController < Sinatra::Base
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    artist = Artist.find_or_create_by(name: params["Artist Name"])
+    genres = params["genres"]
+    @song = Song.new(name: params["Name"], artist: artist)
+    genres.each{|genre| @song.genres << Genre.find(genre.to_i)}
+    @song.save
+    erb :'songs/show', locals: {message: "Successfully created song."}
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    genres = params["genres"]
+    @song.update(name: params["Name"], artist: params["Artist Name"])
+    if genres
+      @song.genres.clear
+      genres.each{|genre| @song.genres << Genre.find(genre.to_i)}
+    end
+    @song.save
+    erb :'songs/show', locals: {message: "Successfully updated song."}
   end
 
   get '/artists' do
