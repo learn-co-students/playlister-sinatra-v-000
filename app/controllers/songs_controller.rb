18c9add038ecf75173a90d904cefc9033f483bd4
Brad1974 <danieltbrad@gmail.com>
2016-02-28 17:17:19 -0800

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,46 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'  
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit' 
+  end
+  
+  post '/songs' do
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    erb :'songs/show', locals: {message: "Successfully created song."}
+  end
+
+  post '/songs/:slug' do
+  params.to_s
+  @song=Song.find_by_slug(params[:slug])
+  @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+  @song.genre_ids = params[:genres]
+  # @song.artist.name
+  # @song.genre_ids = params[:genres]  # @song = Song.find_by_slug(params[:slug])
+    # @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    # @song.genre_ids = params[:genres]
+     @song.save
+     erb :'/songs/show', locals: {message: "Song successfully updated."}
+  end
+
+
+
+
+end
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
+
 require './config/environment'
 
 if ActiveRecord::Migrator.needs_migration?
@@ -5,4 +6,8 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 use Rack::MethodOverride
+
+use SongsController
+use ArtistsController
+use GenresController
 run ApplicationController
