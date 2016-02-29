c9c91faefe72c00ded427a3232d79321d60f0393
authorbeard <msnolan@gmail.com>
2016-02-28 22:43:07 -0500

    song/new route built, prob. saving artist id

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,9 +6,30 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song=Song.find_by_slug(params[:slug])
+# binding.pry
     erb :'songs/show'
+  end
+
+
+  post '/songs' do
+# binding.pry
+    @song=Song.create(name: params["song"]["name"]) 
+    
+    if params["song"]["artist_id"]
+      @song.artist_id=params["song"]["artist_id"]
+    elsif !params["artist_name"].empty?
+      @song.artist=Artist.create(name: params["artist_name"])
+    end
+# binding.pry
+    @song.save
+    
+    redirect "/songs/#{@song.slug}"
   end
 
 
