10ccd685005ee9478ceff4b54f73f9038b4c5b2c
Tracy <tracetrace@gmail.com>
2016-03-02 20:51:53 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,14 +1,9 @@
 class Artist < ActiveRecord::Base
-  has_many :songs
-  has_many :genres, :through => :songs
-
-  def slug
-    name.downcase.gsub(" ","-")
-  end
   
-  def self.find_by_slug(slug)
-    Artist.all.find{|artist| artist.slug == slug}
-  end    
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
 
-
-end  
+  has_many :songs
+  has_many :genres, through: :songs
+  
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,10 @@
 class Song < ActiveRecord::Base
+  
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
   belongs_to :artist
   has_many :song_genres
-  has_many :genres, :through => :song_genres
-
-  def slug
-    name.downcase.gsub(" ","-")
-  end
+  has_many :genres, through: :song_genres
   
-  def self.find_by_slug(slug)
-    Song.all.find{|song| song.slug == slug}
-  end
-
 end
