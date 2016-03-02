7189fa28fdf35bff426baade16d4ed175c24e785
Tracy <tracetrace@gmail.com>
2016-03-02 18:56:33 +0000

    update artist, genre, song classes to include slug methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,13 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
 
-end
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+  
+  def self.find_by_slug(slug)
+    Artist.all.find{|artist| artist.slug == slug}
+  end    
+
+
+end  
