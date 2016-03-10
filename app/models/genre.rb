10584d3674f0f94e6aa72ce7a4b294ea18d25e3f
lcorr8 <l.corr.v@gmail.com>
2016-03-10 15:47:45 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,12 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
   has_many :songs, through: :song_genres
   has_many :song_genres
+
+  def slug
+  self.name.downcase.gsub!(" ", "-")
+  end
+
+  def self.find_by_slug(artist_name)
+    Genre.all.find{|artist| artist.slug == artist_name}
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,12 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
 
+def slug
+  self.name.downcase.gsub!(" ", "-")
+end
 
+def self.find_by_slug(artist_name)
+  Song.all.find{|artist| artist.slug == artist_name}
+end
 
 end
