618c40945124e2d71acd98ba3dac2bb0a2dd1fe1
esimonian <eli.simonian@gmail.com>
2016-03-17 17:43:47 -0700

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,9 @@
+class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :artists, through: :songs
+  has_many :songs, through: :song_genres
+
+  extend Slugs::ClassMethods
+  include Slugs::InstanceMethods
+
+end
