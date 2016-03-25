4c252af20f4f9f52ae9f4e26a022d487d708d24c
James Novak <jamesnovak90@gmail.com>
2016-03-25 12:30:01 -0400

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,7 @@
+class Genre < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+  has_many :artists, through: :songs
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+Artist.create(name: "50 Cent")
+
+Song.create(name: "Ghetto Quran")
+
+Genre.create(name: "Hip Hop")
+
+
