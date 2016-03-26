3b12287b23e1ec268d7bd02a089644bc61ff1d94
Lady Codington <myemailforcoding@gmail.com>
2016-03-25 21:02:27 -0400

    fail 05_song_form_spec.rb:17

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -4,9 +4,12 @@ class SongController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
-    binding.pry
     @song = Song.find_by_slug(params[:slug])
-    erb 'songs/show'
+    erb :'songs/show'
   end
 end
