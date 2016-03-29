847dcb76ad321da3c18092c209e4db2015d666e6
corya0687 <corya0687@gmail.com>
2016-03-29 15:10:08 -0400

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,14 @@
+module Slugifiable
+  module Instance
+    def slug
+      slug= self.name.downcase.split(" ").join("-")
+    end
+  end
+
+  module Class
+    def find_by_slug(slug)
+    unslug= slug.split("-").join(" ")
+    self.find_by('lower(name) = ?', unslug.downcase)
+    end
+  end
+end
