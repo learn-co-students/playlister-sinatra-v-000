b6e141dd06d8661963701d5427bca98eff91c7a6
rocketNeck <homestarflyer@gmail.com>
2016-03-31 20:53:43 -0400

    all done
    passed all tests
    played around with a stylesheet
    and formatting the lists templates

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,38 @@
+
 class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
-    erb :'/songs/index'
+    erb :'songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
   end
 
   post '/songs' do
-    erb :'songs/index'
+    song = Song.create(name: params[:name],
+                     artist: Artist.find_or_create_by(name: params[:artist]),
+                     genres: params[:genres])
+    song.save
+    erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do 
+    song = Song.find_by_slug(params[:slug])
+    song.update_all(name: params[:name],
+                  artist: params[:artist],
+                  genres: params[:genres])
+    erb :'songs/show'
   end
 end
