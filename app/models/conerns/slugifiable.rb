b05214dcd7ad34b8ca1e4c7f47069222402b23e8
glee38 <glee3388@gmail.com>
2016-04-05 22:31:47 -0400

    passing all model specs

--- app/models/conerns/slugifiable.rb
+++ app/models/conerns/slugifiable.rb
@@ -1,2 +1,14 @@
-class Slugify
+module Slugify
+  def slug
+    self.name.downcase.strip.gsub(" ", "-")
+  end
+end
+
+module FindBySlug
+  include Slugify
+  def find_by_slug(s)
+    self.all.detect do |obj|
+      obj if obj.slug == s
+    end
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,7 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  include Slugify
+  extend FindBySlug
 end
