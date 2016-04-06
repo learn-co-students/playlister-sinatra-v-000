42ca34000c7254ea660c0e831a71f7c9179aa420
glee38 <glee3388@gmail.com>
2016-04-06 15:09:05 -0400

    working views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,12 @@
 class ArtistsController < ApplicationController
+  get '/artists' do
+    erb :'/artists/index'
+  end
 
+  get '/artists/:slug' do
+    # using dynamic attribute-based finder #find_by_attribute
+
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-@artist = Artist.create(name: "Taylor Swift")
-blank_space = Song.create(name: "Blank Space", artist: @artist)
-pop = Genre.create(name: "Pop")
-blank_space.genre_ids = pop.id
+LibraryParser.parse
