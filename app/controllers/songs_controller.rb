e8e1761c1ffe86d648f0d5020bbd93267e3ef247
glee38 <glee3388@gmail.com>
2016-04-07 13:10:36 -0400

    refactor and add alt solutions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,12 @@ class SongsController < ApplicationController
       genres: params[:genres]
       )
     #@song.add_genres(params)
+
+    # solution on learn
+    #   @song = Song.create(:name => params["Name"])
+    #   @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    #   @song.genre_ids = params[:genres]
+    #   @song.save
     erb :'songs/show', locals: {message: "Successfully created song."}
   end
 
@@ -28,7 +34,7 @@ class SongsController < ApplicationController
     erb :'songs/edit'
   end
 
-  post '/songs/:slug' do
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(
       artist: Artist.find_or_create_by(name: params[:song][:artist]),
