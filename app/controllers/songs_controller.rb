d9c478f916d50dce46a54dbf0e4f98f1817c46a5
Enoch Griffith <enoch2k2@hotmail.com>
2016-04-08 15:10:41 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,15 +17,41 @@ class SongsController < ApplicationController
       @artist.songs << @song
       @artist.save
     end
+    if !params["genre"]["name"].empty?
+      @genre = Genre.find_or_create_by(name: params["genre"]["name"])
+      @song.genres << @genre
+      @genre.songs << @song
+      @genre.save
+    end
     @song.save
-
-    redirect "/songs/#{@song.slug}"
+    erb :"/songs/show", locals:{message: "Successfully created song."}
+    # redirect '/songs'
   end
 
   get '/songs/:slug' do
-    binding.pry    
     @song = Song.find_by_slug(params[:slug])
     erb :"/songs/show"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"/songs/edit"
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    if !params[:song][:name].empty?
+      @song.update(params[:song])
+    end
+    if !params[:song][:artist].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:song][:artist])
+    end
+    @genre = Genre.find_by_id(params[:song][:genre_ids])
+    if !@song.genres.include?(@genre)
+      @song.genre_ids = params[:song][:genre_ids]
+    end
+    @song.save
+    erb :'/songs/show', locals:{message: "Song successfully updated."}
   end
 
 end
