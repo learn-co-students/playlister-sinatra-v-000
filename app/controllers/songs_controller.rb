a7bd0812670a63e2bdd0475b1122ec6b28e06163
viparthasarathy <viparthasarathy@gmail.com>
2016-04-08 19:08:38 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,4 +24,20 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @artist = Artist.find_or_create_by(params[:artist])
+    @artist.songs << @song
+    SongGenre.where(song_id: @song.id).destroy_all
+    params[:genre_ids].each do |genre_id|
+      SongGenre.find_or_create_by({genre_id: genre_id, song_id: @song.id})
+    end
+    erb :'/songs/show', locals: {message: "Song successfully updated."}
+  end
 end
