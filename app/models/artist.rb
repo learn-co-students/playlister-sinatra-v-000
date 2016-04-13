a5edf7d6bc910b556fa153aa61d418a0516ae94a
e-baker <eric@ericbaker.me>
2016-04-13 13:38:04 -0400

    tests through /songs/:slug passing

---
+++ app/models/artist.rb
@@ -0,0 +1,13 @@
+class Artist < ActiveRecord::Base
+  has_many :genres, through: :songs
+  has_many :songs
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find{|artist| artist.slug == slug}
+  end
+
+end
