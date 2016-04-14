615dedf7b2db9d1a046d753abfee37df8be91f09
Mark <mystycs@gmail.com>
2016-04-14 11:03:26 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.find{|song| song.slug == slug}
+  end
+
+end
