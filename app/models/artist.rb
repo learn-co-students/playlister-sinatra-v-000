d1f17a7f7efb462e4d4d8b9d52362feee71fca04
gnfisher <me@gnfisher.com>
2016-04-15 13:35:41 -0300

    all relationships up, slugify module passing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,10 @@
 class Artist < ActiveRecord::Base
     has_many :songs
+    has_many :genres, through: :songs
+
+    include Concerns::Slugifiable
+
+    def self.find_by_slug(slug)
+      self.find_by(name: slug.split('-').map(&:capitalize).join(" "))
+    end
 end
