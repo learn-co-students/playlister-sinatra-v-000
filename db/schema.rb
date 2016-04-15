68912fe7277295b6f9de5ab354d6943a4c1f548e
gnfisher <me@gnfisher.com>
2016-04-15 13:02:44 -0300

    genres model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415155553) do
+ActiveRecord::Schema.define(version: 20160415160129) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
