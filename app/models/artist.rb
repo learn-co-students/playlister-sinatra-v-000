ae1631abd6cd0d999d62009f96cd09a3ba36226f
gnfisher <me@gnfisher.com>
2016-04-15 13:00:29 -0300

    relationship between song and artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,3 @@
 class Artist < ActiveRecord::Base
-  
+    has_many :songs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415155215) do
+ActiveRecord::Schema.define(version: 20160415155553) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "artist_id"
   end
 
 end
