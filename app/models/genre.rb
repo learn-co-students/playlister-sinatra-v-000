6a9fe9fe93577800393b99e7466dc1ceffdd369e
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-18 22:07:35 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,6 @@
 class Genre < ActiveRecord::Base
   has_many :artists
-  has_many :songs
   has_many :song_genres
-  has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+  has_many :songs, through: :song_genres
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ActiveRecord::Base
-  has_one :artists
+  belongs_to :artist
   has_many :song_genres
-  has_many :genres
   has_many :genres, through: :song_genres
 end
