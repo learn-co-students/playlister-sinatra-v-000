3cc65fb072e7b5ad458bd8160e5e59766d2cd6ec
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-18 21:43:45 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,10 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres
+  has_many :genres, through: :songs
 
-  def self.find_by_slug(arg)
-    test = arg.split('-').join(' ')
-    self.all.detect {|artist| artist = test} 
-  end
+  #def self.find_by_slug(arg)
+  #  test = arg.split('-').join(' ')
+  #  self.all.detect {|artist| artist = test} 
+  #end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
-   has_one :artists
-   has_many :song_genres
-   has_many :genres
-   has_many :genres, through: :song_genres
+  has_one :artists
+  has_many :song_genres
+  has_many :genres
+  has_many :genres, through: :song_genres
 end
