de13be93d7c891f3b3caee9c33f500ccf02e0946
mirandalily <mirandaraymond@me.com>
2016-04-21 14:21:54 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,12 +2,39 @@ class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
-    erb :'songs/index'
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
-    @songs = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genres = params[:genres]
+    @song.save
+    erb :'/songs/show', locals: {message: "Successfully created song."}
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    if !(params[:artist][:name]).empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    end
+    @song.save
+    erb :'songs/show', locals: {message: "Song successfully updated."}
   end
 
 end
