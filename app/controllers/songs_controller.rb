667844034655261641eb1f0ddddcdc5e22306a76
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-23 19:14:26 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,45 +17,39 @@ class SongsController < ApplicationController
     session[:referrer] = request.referrer
     
     @redirect = session[:referrer]
-    @artist = Artist.all.find_or_create_by(name: params["Artist Name"])
+    @artist = Artist.find_or_create_by(name: params["Artist Name"])
     @artist.name ? nil : @artist.update(name: params["Artist Name"])
     
-    @song = Song.all.find_or_create_by(artist_id: @artist.id)
+    @song = Song.find_or_create_by(artist_id: @artist.id)
 
     erb :'/songs/show'
   end
 
   post '/songs' do
     #binding.pry
-    #@song.artist = Artist.all.find_or_create_by(name: params["Artist Name"])
-
     session[:referrer] = request.referrer
     
     @redirect = session[:referrer]
     #binding.pry
-    @song = Song.all.find_or_create_by(name: params["Name"])
+    @song = Song.find_or_create_by(name: params["Name"])
+    @artist = Artist.find_or_create_by(name: params["Artist Name"])
+    
+    @song.artist_id = @artist.id
     #@song.artist_id ? nil : @song.update(artist_id: @artist.id)
     @song.save
-    
-    @song_genre = SongGenre.find_or_create_by(genre_id: params["genres"])
+    @artist.save
+    @song_genre = SongGenre.find_or_create_by(genre_id: params["genres"].first)
     @song_genre.song_id ? nil : @song_genre.update(song_id: @song.id)
 
-    if @artist == Artist.all.last
-      redirect to "/songs/#{@song.slug}"
-    else
-      redirect to "/songs"
-    end
+    erb :"/songs"
   end
 
   get '/songs/:slug' do
-    session[:referrer] = request.referrer
-    #binding.pry
-
     @song = Song.find_by_slug(params[:slug])
-    @song_artist = Artist.all.find_by(:id => @song.artist_id)
+    @song_artist = Artist.find_by(:id => @song.artist_id)
     #binding.pry
-    genre_id = SongGenre.all.find_or_create_by(:song_id => @song.id).genre_id
-    @song_genre = Genre.all.find_by(:id => @song.id)
+    genre_id = SongGenre.find_or_create_by(:song_id => @song.id).genre_id
+    @song_genre = Genre.find_by(:id => @song.id)
 
     erb :'/songs/show'
   end
@@ -63,7 +57,8 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
 
     session[:referrer] = request.referrer
-    
+    @redirect = session[:referrer]
+
     @song = Song.find_by_slug(params[:slug])
     #binding.pry
     erb :'/songs/edit'
