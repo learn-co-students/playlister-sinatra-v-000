332516ec0afe70ae04b3e1d5f761ede1097b0b26
Charles Pletcher <cpletcher2@gmail.com>
2016-04-28 12:22:49 -0400

    Make spec more idiomatic

--- spec/features/05_song_form_spec.rb
+++ spec/features/05_song_form_spec.rb
@@ -49,13 +49,13 @@ describe 'Song Forms' do
         artist = Artist.create(name: artist_name)
       end
 
-      it 'does not create a new artist' do
-        expect do
-          fill_in 'Name', with: song_name
-          check 'Hippity Hop'
-          fill_in 'Artist Name', with: artist_name
-          click_on 'Create'
-        end.not_to change(Artist, :count)
+      it "does not create a new artist" do
+        expect {
+          fill_in "Name", with: song_name
+          check "Hippity Hop"
+          fill_in "Artist Name", with: artist_name
+          click_on "Create"
+        }.not_to change(Artist, :count)
       end
 
       it 'creates a new song and associates it with an existing artist' do
