a99797ab96527c773ec13834543faee0792b2c7f
Chris Haugen <haugenc@gmail.com>
2016-05-04 11:53:46 -0500

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,12 @@
+module Slugifiable
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find{ |x| x.slug == slug}
+    end
+  end
+  module InstanceMethods
+    def slug
+      self.name.gsub(" ","-").downcase
+    end
+  end
+end
