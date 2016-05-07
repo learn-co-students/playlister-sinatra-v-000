063b837c573abe79744657c7ca2960b64d09c3f5
adriandave13 <adrian@paradoxlogic.net>
2016-05-06 22:28:57 -0400

    Done.

---
+++ db/migrate/20160506195655_create_song_genres.rb
@@ -0,0 +1,12 @@
+class CreateSongGenres < ActiveRecord::Migration
+  def up
+    create_table :song_genres do |t|
+      t.integer :song_id
+      t.integer :genre_id
+    end
+  end
+
+  def down
+    drop_table :song_genres
+  end
+end
