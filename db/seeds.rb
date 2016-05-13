31c4b2d7394e1ea8eb46d43fe1b87c607f96b9a0
thebrianemory <the@brianemory.com>
2016-05-13 13:10:08 -0700

    Complete basic view specs for songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,34 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+# modestmouse = Artist.create(name: "Modest Mouse")
+# floaton = Song.create(name: "Float On", artist: modestmouse)
+# alternative = Genre.create(name: "Alternative")
+# floaton.genre_ids = alternative.id
+
+# etherwood = Artist.create(name: "Etherwood")
+# lightmywayhome = Song.create(name: "Light My Way Home", artist: etherwood)
+# dnb = Genre.create(name: "Drum n Bass")
+# lightmywayhome.genre_ids = dnb.id
+#
+# rawtekk = Artist.create(name: "Rawtekk")
+# aftermath = Song.create(name: "Aftermath", artist: rawtekk)
+# snowflakes = Song.create(name: "Snowflakes", artist: rawtekk)
+# aftermath.genre_ids = dnb.id
+# snowflakes.genre_ids = dnb.id
+#
+# fredvgrafix = Artist.create(name: "Fred V & Grafix")
+# herewithyou = Song.create(name: "Here With You", artist: fredvgrafix)
+# catchmybreath = Song.create(name: "Catch My Breath", artist: fredvgrafix)
+# herewithyou.genre_ids = dnb.id
+# catchmybreath.genre_ids = dnb.id
+#
+# deadmau5 = Artist.create(name: "Deadmau5")
+# ghostsnstuff = Song.create(name: "Ghosts n Stuff", artist: deadmau5)
+# electronic = Genre.create(name: "Electronic")
+# ghostsnstuff.genre_ids = electronic.id
+#
+# pendulum = Artist.create(name: "Pendulum")
+# thetempest = Song.create(name: "The Tempest", artist: pendulum)
+# theislandpt1 = Song.create(name: "The Island Pt I (Dawn)", artist: pendulum)
+# thetempest.genre_ids = dnb.id
+# theislandpt1.genre_ids = dnb.id
