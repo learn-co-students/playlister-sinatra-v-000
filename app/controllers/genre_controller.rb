2f74cb20ca60ae225c4bb35ac6beb4e56548103c
danielriley06 <driley3@gmail.com>
2016-05-23 16:26:34 -0400

    Done.

---
+++ app/controllers/genre_controller.rb
@@ -0,0 +1,15 @@
+class GenreController < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "../views/genres") }
+
+  get "/genres" do
+    @genres = Genre.all
+    erb :index
+  end
+
+  get "/genres/:slug" do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :show
+  end
+
+end
--- config.ru
+++ config.ru
@@ -4,5 +4,9 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+use ArtistController
+use GenreController
+use SongController
+
 use Rack::MethodOverride
 run ApplicationController
