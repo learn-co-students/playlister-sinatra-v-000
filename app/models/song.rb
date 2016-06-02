2639efa87977ae08daafd8f94435c4c0e869f756
Michael Chen <michael.chen.1120@gmail.com>
2016-06-02 19:07:01 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+end
