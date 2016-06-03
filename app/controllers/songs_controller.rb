822e5b4fc54fd46d3db40ba3a4131e1af2162a42
Rachel Stratemeier <m.rachel.stratemeier@gmail.com>
2016-06-02 19:06:21 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,13 +8,8 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
-  post '/songs' do
-    @song = Song.create(:name => params[:Name])
-    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-    @song.genre_ids = params[:genres]
-    @song.save
-    flash[:message] = "Successfully created song."
-    redirect "/songs/#{@song.slug}"
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
@@ -26,11 +21,15 @@ class SongsController < ApplicationController
     else
       erb :'/songs/new'
     end
-
   end
 
-  get '/songs/new' do
-    erb :'/songs/new'
+  post '/songs' do
+    @song = Song.create(:name => params[:Name])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
@@ -43,6 +42,7 @@ class SongsController < ApplicationController
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
+
     flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
--- config.ru
+++ config.ru
@@ -4,11 +4,10 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-Dir[File.join(File.dirname(__FILE__), "app/controllers", "*.rb")].collect {|file| File.basename(file).split(".")[0] }.reject {|file| file == "application_controller" }.each do |file|
-  string_class_name = file.split('_').collect { |w| w.capitalize }.join
-  class_name = Object.const_get(string_class_name)
-  use class_name
-end
 
 use Rack::MethodOverride
+
+use GenresController
+use ArtistsController
+use SongsController
 run ApplicationController
