4d026d994241c372be0d0376f2fb919385be5f7b
akizzles <aykim002@gmail.com>
2016-06-09 12:18:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,11 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
-
+  
   get '/' do
     erb :index
   end
 
-  get '/songs' do
-    @songs = Song.all
-
-  end
-
-  get '/genres' do
-    @genres = Genre.all
-
-  end
-
-  get '/artists' do
-    @artists = Artist.all
-  end
 end
--- db/migrate/20160607180654_create_song_genres.rb
+++ db/migrate/20160607180654_create_song_genres.rb
@@ -1,4 +1,4 @@
-class CreateAddGenreToSongs < ActiveRecord::Migration
+class CreateSongGenres < ActiveRecord::Migration
   def change
     create_table :song_genres do |t|
       t.integer :song_id
