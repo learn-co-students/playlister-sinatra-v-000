2810e69543ac09bdf7055a25915db9b675278cd0
aback1227 <aback1227@gmail.com>
2016-06-15 16:31:49 -0400

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,7 +7,7 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      self.all.find {|instance| instance.slug == slug}
+      self.all.find{|instance| instance.slug == slug}
     end
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,4 +4,6 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
   has_many :song_genres
   has_many :songs, through: :song_genres
+
+  
 end
