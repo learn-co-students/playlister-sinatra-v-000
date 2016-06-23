84cda70d9b4bee122695d63d92cf9ae103e29d8d
miclp <michaelpeimani@gmail.com>
2016-06-22 23:16:17 -0700

    Start work on song/new form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,25 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  post '/songs' do
+    binding.pry
+    @artist = Artist.find_or_create_by(name: params[:song][:artist])
+    @song = Song.create(name: params[:song][:name])
+    @song.artist = @artist
+
+    # iterate through genre ids and add these to the song_genres join table
+    params[:song][:genre_ids].each do |id|
+      genre = Genre.find(id)
+      # trying to build out the join table relationship
+      # @song.song_genres.build(genre: genre)
+
+      SongGenre.create(song_id: @song.id, genre_id: genre.id) #didn't seem to work
+    end
+
+    @song.save
+    redirect "/songs/#{@song.slug}"
+  end
+
   get '/songs/new' do
     #new song!
     erb :'songs/new'
