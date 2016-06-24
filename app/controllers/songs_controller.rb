86e6664e99cb9a02e7e43955c86c89cfb1f72763
miclp <michaelpeimani@gmail.com>
2016-06-24 16:50:19 -0700

    Add edit song features

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -6,7 +10,7 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    binding.pry
+    # binding.pry
     @artist = Artist.find_or_create_by(name: params[:song][:artist])
     @song = Song.create(name: params[:song][:name])
     @song.artist = @artist
@@ -22,6 +26,10 @@ class SongsController < ApplicationController
     end
 
     @song.save
+
+    # flash message
+    flash[:message] = "Successfully created song."
+
     redirect "/songs/#{@song.slug}"
   end
 
@@ -33,6 +41,45 @@ class SongsController < ApplicationController
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug/edit' do
+    # update song based on the params
+
+    @song = Song.find_by_slug(params[:slug])
+
+    # update artist
+    if !params[:song][:artist].empty?
+      artist = Artist.find_or_create_by(name: params[:song][:artist])
+      @song.artist = artist
+    end
+
+    # update the song's genres
+    genres = []
+    if !params[:song][:genre_ids].empty?
+      params[:song][:genre_ids].each do |genre_id|
+        genres << Genre.find(genre_id)
+      end
+    end
+
+    if !params[:genre][:name].empty?
+      genre = Genre.find_or_create_by(name: params[:genre][:name])
+      if !genres.include?(genre)
+        genres << genre
+      end
+    end
+    @song.genres = genres
+
+    @song.save
+    # flash message
+    flash[:message] = "Successfully updated song."
+
+    redirect "/songs/#{@song.slug}"
   end
 
 end
