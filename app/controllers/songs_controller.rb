771a3a50b9e4e21bc1af08d962340b908d037d6a
kaileeagray <kaileeagray@gmail.com>
2016-07-01 18:55:15 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,16 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  post '/songs' do
+
   end
 end
