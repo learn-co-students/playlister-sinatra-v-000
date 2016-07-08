2788b7c4b042bec707ed280f50849452a008dd2c
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-08 18:06:10 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+LibraryParser.parse
+
+@artist = Artist.create(:name => "Taylor Swift")
+blank_space =  Song.create(:name => "Blank Space", :artist => @artist)
+pop = Genre.create(:name => "Pop")
+blank_space.genre_ids = pop.id
