31845a1d1293669f408883d346f05c0a2f7916fb
jwolfe890 <jwolfe890@gmail.com>
2016-07-12 21:51:08 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,37 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
 
-  get '/songs' do
-    "hello"
-    # @songs = Song.all
-    # erb :'/songs/index'
-  end
-   
+  use Rack::Flash
 
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do 
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'  
+  end  
+
+  post '/songs' do
+    @song = Song.create(:name => params["Name"]) 
+
+    @artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.artist = @artist
+
+    @song.genre_id = params[:genres]
+
+    @song.save 
+
+    flash[:message] = "Successfully created song."
+
+    redirect("/songs/#{@song.slug}")
+
+  end 
+   
 end 
