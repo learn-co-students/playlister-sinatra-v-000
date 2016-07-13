005d50c8e88e62bc98f3436a730eafcbff6f7347
sarond <saron.desta@gmail.com>
2016-07-13 00:25:35 -0400

    passed model specs

---
+++ app/models/genre.rb
@@ -0,0 +1,10 @@
+class Genre < ActiveRecord::Base
+
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+  include Slugifiable::Slug 
+  extend Slugifiable::FindBySlug
+
+end
