00570661ba2b43fc23820de2c7b283db1536491e
ndaniele <n.daniele@gmail.com>
2016-07-13 16:52:15 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,7 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(name: params["name"])
+    @song = Song.create(name: params["Name"])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params["genres"]
     @song.save
