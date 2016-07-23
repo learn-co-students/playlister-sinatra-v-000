ee6415b71355383de0e0de0fc59297065c08e257
steveafrost <ffrostt@gmail.com>
2016-07-23 14:57:02 -0400

    completed basic views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,23 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs/new' do
+    @song = Song.create(params["Name"])
+    @song.genre_ids = params["genres"]
+
+    artist = Artist.find_or_create_by(params["Artist Name"])
+    @song.artist = artist
+    @song.save
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
+
+
 end
