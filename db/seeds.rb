0da07818c5f674e6de061e03dcab460cc065e9b6
steveafrost <ffrostt@gmail.com>
2016-07-23 12:37:23 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
-Artist.create(:name => "Jay-Z")
-Artist.create(:name => "Kanye")
-Artist.create(:name => "Robert Ellis")
-
-Song.create(:name => "99 Problems")
-Song.create(:name => "Gold Digger")
-Song.create(:name => "Elephant")
-
-Genre.create(:name => "Rap")
-Genre.create(:name => "Folk")
+LibraryParser.parse
