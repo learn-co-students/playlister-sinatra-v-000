b48f13b9f40fbee5b2eeffee60b9f567615b63fb
Han Lee <hanlee12@gmail.com>
2016-07-27 21:27:10 +0000

    stuck at 05 3-11

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,29 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post "/songs" do
+     @song = Song.create(name: params["Name"])
+     @song.artist = Artist.create(name: params["Artist Name"])
+#     @song.genres << Genre.find(params[:genres][])
+     params[:genres][].each do |g|
+       @song.genres << Genre.find(g)
+     end
+
+    # if !params[:pet][:name].empty?
+    #   @owner.pets << Pet.create(name: params[:pet][:name])
+    # end
+    @song.save
+
+    redirect "songs/#{@song.slug}"
+  end
+
   get "/songs/:slug" do
     @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/new'
+    erb :'/songs/show'
   end
 
 end
