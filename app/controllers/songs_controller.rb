9878261c5aa1366a464d00741e3fbb4599a9f2fc
Yechiel Kalmenson <chilikkal@gmail.com>
2016-08-03 04:23:53 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/songs' do
+    erb :'songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    puts params
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    params["genres"].each {|genre_id| @song.genre = genre_id}
+    @song.save
+    redirect '/songs/#{@song.slug}'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+end
