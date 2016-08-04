6599bd3d7c84894c9e7ba6dbc392fa78c97926d8
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-08-04 14:04:15 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
+gem 'rack-flash3'
 
 group :test do
   gem 'rspec'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+
+  enable :sessions
+  use Rack::Flash
 
   get '/' do
     erb :index
@@ -23,9 +29,42 @@ class ApplicationController < Sinatra::Base
     erb :setup_new
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :setup_edit
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :songs_slug
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params["Song Name"]
+    @song.artist = params["Artist Name"]
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params[:Name])
+
+    @song.genre_ids = params[:genres]
+
+    if !(Artist.all.find_by_name(params["Artist Name"]))
+      @song.artist = Artist.create(name: params["Artist Name"])
+    else
+      @song.artist = Artist.all.find_by_name(params["Artist Name"])
+    end
+
+    @song.save
+    flash[:message] = "Successfully created song."
+
+    redirect "/songs/#{@song.slug}"
   end
 
   get '/artists/:slug' do
