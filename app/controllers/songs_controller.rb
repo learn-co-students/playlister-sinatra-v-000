4bc64ee3039e70df7860f60979bb494584c65715
Bob Habig <rfhab2@gmail.com>
2016-08-10 22:35:05 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,9 +21,11 @@ class SongsController < ApplicationController
   post '/songs' do
     @song = Song.create(name: params["Name"])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-    @song.genre_ids = params[:genres]
+    @song.genre = params[:genres]
     @song.save
-    redirect "/songs/#{@song.slug}"
+    flash[:message] = "Successfully created song."
+
+    redirect to("/songs/#{@song.slug}")
   end
 
 end
