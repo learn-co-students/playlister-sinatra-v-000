9fc6d1bf75c38f458a6474fa0ab5878eeeb30093
kromoser <kevinromoser@gmail.com>
2016-08-13 17:50:04 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,17 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+
+def slug
+    self.name.gsub(/\W/, '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |song|
+      song.slug == slug
+    end
+    
+  end
+
+end
