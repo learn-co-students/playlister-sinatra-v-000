8d51bc6b9d4a9f8f041eb5a6c2a71c192af58a3d
Sandy Hogan <svern99@aol.com>
2016-08-16 21:32:46 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,11 +4,6 @@ class SongsController < ApplicationController
 
   use Rack::Flash
 
-  get '/songs' do
-   @songs = Song.all
-   erb :'/songs/index'
-  end
-
   get '/songs/new' do
     erb :'/songs/new'
   end
@@ -23,12 +18,15 @@ class SongsController < ApplicationController
     redirect to("/songs/#{@song.slug}")
   end
 
+  get '/songs' do
+   @songs = Song.all
+   erb :'/songs/index'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_or_create_by(params[:artist])
     erb :'/songs/show'
   end
-
-
 
  get '/songs/:slug/edit' do
    @song = Song.find_by_slug(params[:slug])
@@ -38,7 +36,7 @@ class SongsController < ApplicationController
  patch '/songs/:slug' do
    @song = Song.find_by_slug(params[:slug])
    @song.update(params[:song])
-   @song.artist = Artist.find_or_create_by(params[:name])
+   @song.artist = Artist.find_or_create_by(params[:artist])
    @song.genre_ids = params[:genres]
    @song.save
 
