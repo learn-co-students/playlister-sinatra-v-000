74a52fc38ba4e9cfa5b86f4bb637400236b854f8
KerryAlsace <kerrydougherty@gmail.com>
2016-08-30 20:50:54 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,30 @@
 class Genre < ActiveRecord::Base
-  has_many :songs_genres
+  has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    # "Taylor Swift"
+    # => "taylor-swift"
+    name_array = self.name.split
+    downcase_array = []
+    name_array.each do |name|
+      downcase_array << name.downcase
+    end
+    slug = downcase_array.join("-")
+    slug
+  end
+
+  def self.find_by_slug(slug)
+    name_array = slug.split("-")
+    capital_array = []
+    name_array.each do |name|
+      capital_array << name.capitalize
+    end
+    name = capital_array.join(" ")
+    name
+    self.find_by(name: name)
+  end
 end
 
 # Genre.create(name: "Pop")
