51446b69bb93f8465cbc254f968b5a19ecbc0ac4
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-03 22:52:27 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,12 @@
 require 'pry'
 class SongsController < ApplicationController
-
+  def slug
+    self.name.downcase.gsub(" ","-")
+  end
+def find_by_slug(slug)
+   name = slug.split("-").map { |word| word.capitalize }.join(" ")
+   self.find_by_name(name)
+  end
   # songs route controller
   get '/songs' do
     @songs = Song.all
@@ -26,5 +32,7 @@ class SongsController < ApplicationController
     flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
+
+
 
 end
