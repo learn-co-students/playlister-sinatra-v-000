41c9fb94427341a2539a5d6d378269bb6d8dc4db
Marcel Galang <marcelgalang@gmail.com>
2016-09-09 01:17:32 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+
   enable :sessions
   set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, "app/views"
 
   get '/' do
     erb :index
