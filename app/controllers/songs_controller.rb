13a8f11f19fb26ef190f63ee713c208e7659d40c
bmolina-nyc <bruce.d.molina@gmail.com>
2016-09-14 21:36:07 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,35 +1,39 @@
 require 'pry'
+require 'rack-flash'
+
 
 class SongsController < ApplicationController
-
+use Rack::Flash
   get '/songs' do
     @songs = Song.all 
     erb :'/songs/index' 
   end
 
+  get '/songs/new' do 
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
-  
     @song = Song.find_by_slug(params[:slug])
 
     erb :'/songs/show'
   end
 
+  post '/songs' do 
 
+   @song = Song.create(name: params[:name]) 
+ 
+    if !params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
+    end
+      
+    params["genres"].each do |id|
+        @song.genres << Genre.find(id)
+    end
+
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}" 
+  end
+  
 end
-
-
-
-  # # get '/owners' do
-  # #   @owners = Owner.all
-  # #   erb :'/owners/index' 
-  # # end
-
-  # # get '/owners/new' do 
-
-  # #   erb :'/owners/new'
-  # # end
-
-  #  get '/pets' do
-  #   @pets = Pet.all
-  #   erb :'/pets/index' 
-  # end
