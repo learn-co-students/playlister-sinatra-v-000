0312ca5a7662e16174acea1ede485c9e4a1a12f0
bmolina-nyc <bruce.d.molina@gmail.com>
2016-09-15 01:27:13 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -41,8 +41,32 @@ use Rack::Flash
     erb :'/songs/edit'
   end
 
+
+#need to figure out how to get the genre to properly update
+
   post '/songs/:slug' do 
-    binding.pry 
+
+    @song = Song.find_by_slug(params[:slug])  # finds your original artist 
+    
+    #update the song name
+    if !params[:name].empty? # if this is not empty - we're renaming the song
+      @song.name = params[:name]
+    end
+    
+    @song.genres.clear # wipe the old genres, update below with whatever is in params
+    params["genres"].each do |id|
+        @song.genres << Genre.find(id)
+    end  # adding all the genres selected to the songs genres
+
+    if !params[:artist][:name].empty? # if the create a new artist is not empty and the checkbox is unchecekd we're renaming the song to the brand new artist
+      @song.artist = Artist.create(name: params[:artist][:name])
+    else
+      @song.artist = Artist.find(params[:artists]).first
+    end
+    @song.save 
+
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}" 
   end
 
 
