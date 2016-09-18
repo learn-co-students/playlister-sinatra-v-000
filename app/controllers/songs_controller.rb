6238f8245c9169949ab7590838e233ed00ca46ab
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-18 21:37:06 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,35 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    artist = Artist.find_or_create_by(name: params[:artist])
+    @song = Song.create(name: params[:Name], artist: artist, genre_ids: params[:genre])
+    @song.save
+    @message = "Successfully created song."
+    redirect to "songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  post '/songs/:slug/edit' do
+    artist = Artist.find_or_create_by(name: params[:artist])
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(artist: artist, genre_ids: params[:genre])
+    @song.save
+    @message = "Successfully updated song."
+    redirect to "songs/#{@song.slug}"
+  end
 
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,13 +2,13 @@ module Slugify
 
   module ClassMethod
     def find_by_slug(slugged)
-      slug_array = slugged.scan(/[a-z0-9]+/)
-      reverse_slug_array = []
-      slug_array.each do |word|
-        reverse_slug_array << word.capitalize
+      found = []
+      self.all.each do |thing|
+        if slugged == thing.slug
+          found << thing
+        end
       end
-      reverse_slug = reverse_slug_array.join(" ")
-      self.find_by(name: reverse_slug)
+      found[0]
     end
 
   end
@@ -16,7 +16,7 @@ module Slugify
   module InstanceMethod
     def slug
       strip_chars_array = self.name.downcase.scan(/[a-z0-9]+/)
-      final_slug = strip_chars_array.join("-")
+      slug = strip_chars_array.join("-")
     end
   end
 
