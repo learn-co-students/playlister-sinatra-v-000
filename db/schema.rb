933a6e891fdff4d16d29031dff5620571c90edbb
auranbuckles <auran59@gmail.com>
2016-09-20 01:05:54 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920034517) do
+ActiveRecord::Schema.define(version: 20160920040254) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -21,9 +21,15 @@ ActiveRecord::Schema.define(version: 20160920034517) do
     t.string "name"
   end
 
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string  "name"
     t.integer "song_id"
+    t.integer "artist_id"
   end
 
 end
