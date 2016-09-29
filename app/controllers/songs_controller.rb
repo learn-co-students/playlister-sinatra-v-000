a4a31e977d13a73987ebd5ba722689eaf014ef45
Bob Habig <rfhab2@gmail.com>
2016-09-29 03:03:07 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -33,15 +33,14 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
-  post '/songs/:slug' do
-    binding.pry
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
 
     flash[:message] = "Successfully updated song."
-  
+
     redirect :"songs/#{@song.slug}"
   end
 end
