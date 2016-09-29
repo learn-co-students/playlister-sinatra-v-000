312b3b4bf49c3b9d308ed7bc42b6d995cf999bf6
Brad Watson <brad.watson.orlando@gmail.com>
2016-09-29 16:45:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get "/songs" do
-    @songs = Song.all
-    erb :'/songs/index'
-  end
-
   get "/artists" do
     @artists = Artist.all
     erb :'/artists/index'
@@ -22,11 +17,6 @@ class ApplicationController < Sinatra::Base
     erb :'/genres/index'
   end
 
-  get "/songs/:slug" do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
-  end
-
   get "/artists/:slug" do
     @artist = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
@@ -35,18 +25,6 @@ class ApplicationController < Sinatra::Base
   get "/genres/:slug" do
     @genre = Genre.find_by_slug(params[:slug])
     erb :'genres/show'
-  end
-
-  get '/songs/new' do
-    erb :'/songs/new'
-  end
-
-  post "/songs" do
-    @song = Song.create(params[:Name])
-    @song.artist = Artist.find_or_create_by(name: params[:'Artist Name'])
-    @song.genre_ids = params[:genres]
-    @song.save
-    redirect to '/songs/#{@song.slug}'
   end
 
 end
