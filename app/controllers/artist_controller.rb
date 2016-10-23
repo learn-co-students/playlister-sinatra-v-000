8093cf79533733334f6111a326b24aaa99f1ca89
David Kennell <davidkennell0@gmail.com>
2016-10-23 01:30:48 +0000

    Done.

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,3 +1,12 @@
 class ArtistController < ApplicationController
     
+    get '/artists' do 
+        erb :'artist/index'
+    end
+    
+    get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artist/show'
+    end
+    
 end
--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,3 +1,20 @@
 class SongController < ApplicationController
     
+    get '/songs' do 
+        erb :'song/index'
+    end
+    
+    get '/songs/new' do 
+        erb :'song/new'
+    end
+
+    post '/songs/new' do 
+        binding.pry
+    end
+    
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :'song/show'
+    end
+    
 end
