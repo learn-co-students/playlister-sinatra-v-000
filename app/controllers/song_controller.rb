d489745c09c9790fe0293dc26803ae679dd77008
David Kennell <davidkennell0@gmail.com>
2016-10-23 15:49:53 +0000

    Done.

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -8,10 +8,33 @@ class SongController < ApplicationController
         erb :'song/new'
     end
 
-    post '/songs/new' do 
-        binding.pry
+    post '/songs' do
+        @song = Song.create(:name => params["Name"], :genre_ids => params[:song][:genre_ids],)
+        artists = []
+        Artist.all.each do |artist|
+            artists << artist.name
+        end
+        if artists.include?(params["Artist Name"])
+            @song.artist = Artist.find_by(:name => params["Artist Name"])
+        else
+          if !params["Artist Name"].empty?
+              @song.artist = Artist.create(:name => params["Artist Name"])
+          else
+              @song.artist = Artist.find(params[:song][:artist_id])
+          end
+        end
+          @song.save
+                  binding.pry
+
+          @song.slug = @song.slug
+         redirect '/songs/#{@song.slug}'
     end
     
+    get '/songs/:slug/edit' do 
+        @song = Song.find_by_slug(params[:slug])
+        erb :'song/edit'
+    end
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :'song/show'
