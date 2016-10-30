a2a5fad4f78584858cd44b38944f95f4ac5f2a84
Perla Yates <pianistps@yahoo.com>
2016-10-30 23:24:19 +0000

    completed slug methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,14 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
+
+  def slug
+    #strip special characters and replace with -
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(arg)
+    #binding.pry
+    Artist.all.find{|artist| artist.slug == arg}
+  end
 end
