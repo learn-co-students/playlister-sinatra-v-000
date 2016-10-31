2305cec77c6deefe933da937fcb5c56393290a3a
Perla Yates <pianistps@yahoo.com>
2016-10-31 20:33:01 +0000

    finished models, started songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,12 @@
 class SongsController < ApplicationController
 
+  get '/' do
+
+  end
+
+  get '/songs' do
+    #presents user list of all songs in lib.
+    #each song should be clickable link
+    
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, :through => :song_genres
+
+  def slug
+    #strip special characters and replace with -
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(arg)
+    #binding.pry
+    Song.all.find{|song| song.slug == arg}
+  end
 end
