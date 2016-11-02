d9d10ee0526dd81f2f22ee810476d3c0cac0a097
Perla Yates <pianistps@yahoo.com>
2016-11-02 21:09:52 +0000

    added /genres and index

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,9 @@
 class GenresController < ApplicationController
 
+  get '/genres' do
+    #presents user list of all songs in lib.
+    #each song should be clickable link
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
 end
