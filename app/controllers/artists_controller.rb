f72789e329dba50679d357747ababfe7e3415b37
Perla Yates <pianistps@yahoo.com>
2016-11-02 21:07:25 +0000

    added /artists and index

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,10 @@
 class ArtistsController < ApplicationController
 
+  get '/artists' do
+    #presents user list of all songs in lib.
+    #each song should be clickable link
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
 
 end
