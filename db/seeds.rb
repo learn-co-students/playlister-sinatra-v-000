b2aad49721383da031d885c051e93a5bff61fa20
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-11-07 17:43:36 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Artist.create(name: "Avrohom Freid")
+Song.create(name: "napolians march")
+Song.create(name: "Kol dodi")
+Genre.create(name: "Niggunim")
+Genre.create(name: "Jewish")
+Artist.create(name: "Pesach malovstovker")
 
+parser = LibraryParser.new.call
