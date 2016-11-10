62f382665d60a8945d56ae7705c4fdf5942f947b
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-11-10 02:27:28 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,25 @@ require "pry"
 class SongsController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
+  get "/songs/new" do
+    erb :"songs/new"
+  end
+
+  post "/songs/new" do
+    #binding.pry
+    @song = Song.find_or_create_by(name: params[:name])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    params[:genres].each do |id|
+      @song.genres << Genre.find(id)
+    end
+    @song.save
+    #binding.pry
+    redirect "/songs/#{@song.slug}"
+
+  end
+
+
+
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
@@ -14,6 +33,17 @@ class SongsController < Sinatra::Base
     erb :"/songs/show"
   end
 
+  get "/songs/:slug/edit" do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/edit"
+  end
 
-
+  post "/songs/edit" do
+    @song = Song.find_by(name: params[:name])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    #binding.pry
+    params[:genres].each { |genre_id| @song.genres << Genre.find(genre_id.to_i) }
+    @song.save
+    redirect "/songs/#{@song.slug}"
+  end
 end
