2fdad399abd8784278c88dadadddd400db785c84
andresduarte <andresduarte010@gmail.com>
2016-11-27 18:38:18 -0500

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,19 @@
+class Slugifiable
+
+  module InstanceMethods
+
+    def slug
+      self.name.gsub(" ", "-").downcase
+    end
+
+  end
+
+  module ClassMethods
+
+    def find_by_slug(slug)
+      self.all.detect{|instance| instance.slug == slug}
+    end
+    
+  end
+
+end
