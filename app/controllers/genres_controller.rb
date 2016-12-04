d324579cf6907dc12fa1a5df46f376e2504919fc
William Story <wstory@villanova.edu>
2016-12-04 03:33:57 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,7 @@
+class GenresController < ApplicationController
+
+  get '/genres' do
+
+  end
+
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,7 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+
+  end
+
+end
