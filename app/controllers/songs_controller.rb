fdf589b841673ca16d382f2a5e1ad364e9e44575
Ray Mulligan <rmullig2@optonline.net>
2016-12-06 00:38:57 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,20 +10,48 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    #binding.pry
     erb :'/songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
   end
 
   post '/songs' do
     @song = Song.find_or_create_by(name: params[:Name])
     @artist = Artist.find_or_create_by(name: params[:"Artist Name"])
     @song.artist = @artist
+    #binding.pry
     params[:genre].each do |genre|
       @genre = Genre.find_or_create_by(name: genre)
-      @song.artist.genres << @genre
+      #@artist.genres << @genre
       @songgenre = SongGenre.create(:song_id => @song.id, :genre_id => @genre.id)
     end
-    binding.pry
-    erb :'/songs/:slug'
+    @song.save
+    @message = params[:message]
+    #binding.pry
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  patch '/songs' do
+    #binding.pry
+    @song = Song.find_or_create_by(name: params[:Name])
+    @artist = Artist.find_or_create_by(name: params[:"Artist Name"])
+    @song.artist = @artist
+    #binding.pry
+    if params[:genre] != nil
+      params[:genre].each do |genre|
+        @genre = Genre.find_or_create_by(name: genre)
+        #@artist.genres << @genre
+        @songgenre = SongGenre.create(:song_id => @song.id, :genre_id => @genre.id)
+      end
+    end
+    @song.save
+    @message = params[:message]
+    #binding.pry
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
