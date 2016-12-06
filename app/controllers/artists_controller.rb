577ca0b515a65606e31f5f58430e46734b236cdc
William Story <wstory@villanova.edu>
2016-12-06 05:17:49 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,14 +2,11 @@ class ArtistsController < ApplicationController
 
   get '/artists' do
     @artists = Artist.all
-
     erb :'/artists/index'
   end
 
   get '/artists/:slug' do
-    @song = params[:slug]
-    @artist = Artist.find_by_slug(@song)[0].name
-    @genre = Genre.find_by_slug(@song)[0].name
+    @artist = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
   end
 
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,8 +2,12 @@ class GenresController < ApplicationController
 
   get '/genres' do
     @genres = Genre.all
-
     erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genres = Artist.find_by_slug(params[:slug])[0]
+    erb :'genres/show'
   end
 
 end
