95910c570f56ccfbd2b0c92546097e4296d88764
William Story <wstory@villanova.edu>
2016-12-06 01:45:21 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,16 @@
 class ArtistsController < ApplicationController
 
   get '/artists' do
+    @artists = Artist.all
 
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @song = params[:slug]
+    @artist = Artist.find_by_slug(@song)[0].name
+    @genre = Genre.find_by_slug(@song)[0].name
+    erb :'artists/show'
   end
 
 end
