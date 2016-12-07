a1700af8779aadf1058848eb492868dce41ff89a
christinechoi <smi77y@gmail.com>
2016-12-07 16:20:13 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
-   # binding.pry
+    binding.pry
     @song = Song.find_by_slug(params[:slug])
     
     erb :'/songs/show'
