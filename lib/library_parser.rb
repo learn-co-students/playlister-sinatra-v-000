11149f2bf30b69ede5db7ac975ed8a5e8d7e9687
Adam Friedl <ajfriedl@gmail.com>
2016-12-08 09:30:11 -0500

    Done.

--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -32,9 +32,10 @@ class LibraryParser
     genre = Genre.find_or_create_by(name: genre_name)
     artist = Artist.find_or_create_by(name: artist_name)
 
-    song.song_genres.build(genre: genre)
     song.artist = artist
-    
+    song.genres = genre
+
+
     song.save
   end
 end
