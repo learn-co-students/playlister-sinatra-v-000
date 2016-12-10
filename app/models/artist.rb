6cd2b48dfd74626820d0db9cc72c51af97ade569
Adam Friedl <ajfriedl@gmail.com>
2016-12-10 10:14:58 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,6 +7,6 @@ class Artist < ActiveRecord::Base
   end
 
   def self.find_by_slug(slug)
-    self.all.find {|object| object.slug == slug}
+    self.all.find {|artist| artist.slug == slug}
   end
 end
