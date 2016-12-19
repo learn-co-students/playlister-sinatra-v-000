9e0272addb03d1a43e042821288c478f380fcdb0
Howard DeVennish <howarddevennish@gmail.com>
2016-12-19 21:38:06 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,6 +3,13 @@ class Artist < ActiveRecord::Base
   has_many :genres, :through => :songs
 
   def slug
+    self.name.downcase.gsub(" ", "-")
   end
-  
+
+  def self.find_by_slug(slug)
+    Artist.all.find do |artist|
+      artist.slug == slug
+    end
+  end
+
 end
