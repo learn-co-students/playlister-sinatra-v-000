7823bc3b89cb0aa5c4e3a75c8f77a81d6cf50a8f
github username <github email address>
2017-01-01 14:21:52 -0500

    Done.

--- db/migrate/20161230223234_create_songs.rb
+++ db/migrate/20161230223234_create_songs.rb
@@ -3,6 +3,7 @@ class CreateSongs < ActiveRecord::Migration
     create_table :songs do |t|
       t.string :name
       t.integer :artist_id
+      t.integer :genre_ids
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230214312) do
+ActiveRecord::Schema.define(version: 20161231192845) do
 
   create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20161230214312) do
   create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
+    t.integer  "genre_ids"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
