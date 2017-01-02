bed7a1fa63bf838ea26343dce6225e082483c448
github username <github email address>
2017-01-02 15:18:01 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,16 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.each do |s|
+      if s.slug == slug
+        return s
+      end
+    end
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,16 @@ class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
   has_many :song_genres
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.each do |s|
+      if s.slug == slug
+        return s
+      end
+    end
+  end
 end
