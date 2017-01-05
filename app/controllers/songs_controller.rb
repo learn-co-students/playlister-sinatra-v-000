ef1d506f3248e41c156278ad7d779247a1997f74
Jon Fougner <jon.fougner@gmail.com>
2017-01-05 20:28:41 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,37 +7,13 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(name: params["Name"])
-    if !params[:"Artist Name"].empty?
-      @song.artist = Artist.create(:name => params[:"Artist Name"])
-    end
-    if !params["genres[]"].empty?
-      @song.genres << Genre.create(name: params["genres[]"])
-      # the line above seems wrong for multiple genres
+    @song.artist = Artist.find_or_create_by(:name => params[:"Artist Name"])
+    params["genres"].each do |g|
+      @song.genres << Genre.find_by_id(g.name)
     end
     @song.save
-    binding.pry
     redirect "songs/#{@song.slug}"
   end
-  # the instantiations above may not capture all the relevant data
-
-  # post '/owners/:id' do
-  #   @owner = Owner.find(params[:id])
-  #   @owner.update(params["owner"])
-  #   if !params["pet"]["name"].empty?
-  #     @owner.pets << Pet.create(name: params["pet"]["name"])
-  #   end
-  #   redirect "owners/#{@owner.id}"
-  # end
-
-  # post '/pets' do
-  #   @pet = Pet.create(params[:pet])
-  #   if !params["owner"]["name"].empty?
-  #     @pet.owner = Owner.create(name: params["owner"]["name"])
-  #   end
-  #   @pet.save
-  #   redirect "pets/#{@pet.id}"
-  # end
-  # the above is a precedent from a prior lab
 
   get '/songs/new' do
     erb :'/songs/new'
