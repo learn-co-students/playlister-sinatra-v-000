1bba630f6d82541f602dbdd88f1fba0afb051c17
github username <github email address>
2017-01-13 14:58:17 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,17 +28,22 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
+
     @song = Song.find_by_slug(params[:slug])
+
     erb :"/songs/show"
   end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+
     erb :"/songs/edit"
   end
 
   post '/songs/:slug' do
+    flash[:message] = "Successfully updated song."
 
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
