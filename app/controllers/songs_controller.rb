778a6dac3d6de76e9cc9c9362537eecbd3ac25a8
Christina L. Cole <christinalcole@gmail.com>
2017-01-14 01:07:03 -0500

    initial new-song form, route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,16 +6,27 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+    @genres = Genre.all
 
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
+
+  post '/songs' do
+    #binding.pry
+    @song = Song.create(name: params["Name"])
+    if !Artist.find_by(name: params["Artist Name"])
+      @song.artist = Artist.create(name: params["Artist Name"])
+    else
+      @song.artist = Artist.find_by(params["Artist Name"])
+    end
+      @song.genres << Genre.find_by_id(params["genres"])
+      @song.save
+      #binding.pry
+    redirect to "/songs/#{@song.slug}"
+  end
 end
-
-
-# Any given song's show page should have links to that song's artist and the each genre associated with the song.
-# Pay attention to the order of /songs/new and /songs/:slug. The route /songs/new could interpret new as a slug if
-#  that controller action isn't defined first.
