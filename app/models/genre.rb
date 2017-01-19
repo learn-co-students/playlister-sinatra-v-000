cc2e89cf02d9f336f165894933e689bfd75b4b15
Mary Umoh <umohm12@gmail.com>
2017-01-19 15:18:43 +0000

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,6 @@
+class Genre < ActiveRecord::Base
+
+  has_many :artists
+  has_many :songs
+
+end   
---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ActiveRecord::Base
+
+  belongs_to :artist
+  has_many :genres
+  
+end
