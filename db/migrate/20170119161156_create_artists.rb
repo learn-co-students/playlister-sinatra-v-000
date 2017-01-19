5669028e13e268e52c01ba9396097470484fed5f
Mary Umoh <umohm12@gmail.com>
2017-01-19 16:22:56 +0000

    Done.

--- db/migrate/20170119161156_create_artists.rb
+++ db/migrate/20170119161156_create_artists.rb
@@ -1,4 +1,11 @@
 class CreateArtists < ActiveRecord::Migration
   def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :songs
+      t.string :genres
+    end
   end
 end
+
+#Unsure if attributes should be in table or model or both 
