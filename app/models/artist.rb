8273441e9fbc4033c9dacd056e90f30e5aeafe0c
Mary Umoh <umohm12@gmail.com>
2017-01-21 16:36:32 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,16 @@
 class Artist < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+  
     attr_accessor :name
-    
+
     has_many :songs
     has_many :genres, through: :songs
     #has_many => <#Artist>.songs || <#artist>.genres
-    
-    def slug
-    end 
-    
-    def find_by_slug
-    end 
-end   
+
+    # def slug
+    # end
+    #
+    # def find_by_slug
+    # end
+end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,13 +1,15 @@
 class Genre < ActiveRecord::Base
-  attr_accessor :name
-  
-  has_many :artists, through: :songs
-  has_many :songs, through: :song_genre 
-  
-  def slug
-  end 
-  
-  def find_by_slug
-  end 
-end   
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 
+  attr_accessor :name
+
+  has_many :artists, through: :songs
+  has_many :songs, through: :song_genre
+
+  # def slug
+  # end
+  #
+  # def find_by_slug
+  # end
+end
