2c816fb7ac4b615a8901fc3a614cf661c5668fe8
Mary Umoh <umohm12@gmail.com>
2017-01-23 20:31:41 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,7 +2,7 @@ module Slugifiable
   module InstanceMethods
     def slug
       #strip out any special characters, and replace all spaces with `-`. For a song, artist and genre
-      slug = song.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+      self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     end
   end
   #<#Artist>.slug
@@ -10,6 +10,7 @@ module Slugifiable
   module ClassMethods
     def find_by_slug(slug)
       #uses the `slug` method to retrieve a song/artist/genre from the database and return that entry
+      self.all.select {|element| element.slug == slug } #find a specific instance that contains our slug
     end
   end
   #Artist.find_by_slug(:slug)
