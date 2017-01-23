cff9918dd0d7139f1c946106dd0367667e15d448
Mary Umoh <umohm12@gmail.com>
2017-01-23 18:13:06 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,11 @@
 class Artist < ActiveRecord::Base
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
-  
+
     attr_accessor :name
 
     has_many :songs
     has_many :genres, through: :songs
     #has_many => <#Artist>.songs || <#artist>.genres
-
-    # def slug
-    # end
-    #
-    # def find_by_slug
-    # end
+    
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,11 +1,15 @@
-module Slugifiable 
+module Slugifiable
   module InstanceMethods
     def slug
+      #strip out any special characters, and replace all spaces with `-`. For a song, artist and genre
+      slug = song.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     end
   end
   #<#Artist>.slug
+
   module ClassMethods
     def find_by_slug(slug)
+      #uses the `slug` method to retrieve a song/artist/genre from the database and return that entry
     end
   end
   #Artist.find_by_slug(:slug)
