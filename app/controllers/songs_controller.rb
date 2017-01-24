ac788b3c92677fb631623c2b7c04b5a2ffcce2b9
Umohm1 <umohm12@gmail.com>
2017-01-24 15:26:21 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,6 +38,7 @@ class SongsController < ApplicationController
 
 
     patch '/songs/:slug' do
+      # binding.pry
       @song = Song.find_by_slug(params[:slug])
       @song.update(params[:song])
       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
