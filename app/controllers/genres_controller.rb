fa05b94f331f70eb9177249948cd9a7a986ec06f
Scott Young <young.scottw@gmail.com>
2017-01-27 23:14:05 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,12 @@
+class GenresController < ApplicationController
+  #Genres controller section, to be refactored
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
+end
