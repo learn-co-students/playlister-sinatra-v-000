37288a308dffffbe4eca3342b59dd7ed567d7758
Chirag Dudhat <cdudhat@gmail.com>
2017-01-28 12:53:40 +0530

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ActiveRecord::Base
+  include Concerns::Slugifiable
+  extend Concerns::Findable
+  
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+#binding.pry
+LibraryParser.parse
