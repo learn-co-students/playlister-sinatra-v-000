dceb09063a5df0b934d4a46b16f5904b7f7e9ac4
Liah Wallace <liahwallace1@gmail.com>
2017-02-14 19:19:25 +0000

    Done.

---
+++ app/controllers/genres_controllers.rb
@@ -0,0 +1,13 @@
+class GenresControllers < ApplicationController
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
+
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,57 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params["song_name"])
+    @song.artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.genre_ids = params[:genres]
+    if !params[:genre_name].empty?
+      @song.genres << Genre.create(name: params[:genre_name])
+    end
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params["song_name"]
+    @song.artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.genre_ids = params[:genres]
+    if !params[:genre_name].empty?
+      @song.genres << Genre.create(name: params[:genre_name])
+    end
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  post '/songs/:slug/delete' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.delete
+    redirect to '/songs'
+  end
+
+end
