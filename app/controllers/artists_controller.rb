89a130fabb2b569fc94386016de3b673da74d414
bentonwong <bentonwong@gmail.com>
2017-03-22 16:10:02 -0700

    restore controllers from backup

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,18 @@
+require_relative '../../config/environment'
+require 'pry'
+
+class ArtistsController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/artists' do
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+  end
+
+end
