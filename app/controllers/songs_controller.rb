617942e2ba5507f6cfec32d21b56d3722d47b673
bentonwong <bentonwong@gmail.com>
2017-03-22 19:58:29 -0700

    add feature to save edits

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,4 +40,14 @@ class SongsController < Sinatra::Base
     erb :'/songs/edit'
   end
 
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    if !params["artist"]["name"].empty?
+      @song.artist = Artist.find_or_create_by(params["artist"])
+    end
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
 end
