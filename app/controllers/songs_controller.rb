448ca537923f2af11f2980133cd86726ee6e9ca2
walwoodr <walwoodr@gmail.com>
2017-04-03 20:50:54 -0500

    Add flash message and logic to prevent artist duplication

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,7 +17,11 @@ class SongsController < ApplicationController
     song = Song.new
     song.name = params["Name"]
     if params["Artist Name"] != ""
-      song.build_artist(name: params["Artist Name"])
+      if Artist.exists?(:name => params["Artist Name"])
+        song.artist = Artist.find_by(name: params["Artist Name"])
+      else
+        song.build_artist(name: params["Artist Name"])
+      end
     else
       song.artist = Artist.find(params["Artist"])
     end
@@ -29,6 +33,7 @@ class SongsController < ApplicationController
       end
     end
     song.save
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{song.slug}"
   end
 
