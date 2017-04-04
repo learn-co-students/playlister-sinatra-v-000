db59c9f3d985d0362a692b7b1a30b4e6486ca812
walwoodr <walwoodr@gmail.com>
2017-04-03 20:36:35 -0500

    Add controller logic for new song post route, minor tweaks to view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,32 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
+  end
+
+  post '/songs' do
+    song = Song.new
+    song.name = params["Name"]
+    if params["Artist Name"] != ""
+      song.build_artist(name: params["Artist Name"])
+    else
+      song.artist = Artist.find(params["Artist"])
+    end
+    params.each do |key, value|
+      Genre.all.each do |genre|
+        if genre.name == key
+          song.genres << Genre.find(value)
+        end
+      end
+    end
+    song.save
+    redirect "/songs/#{song.slug}"
   end
 
 end
