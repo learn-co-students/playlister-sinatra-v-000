21d5c5e58446ca100570e01829d4e4ae970306d6
Jenna Francis <jef399@nyu.edu>
2017-04-04 14:49:20 +0000

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,13 @@
+module Slugifiable
+  module InstanceMethods
+    def slug
+      self.name.downcase.gsub(" ","-")
+    end
+  end
+
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.detect{|item| item.slug == slug}
+    end
+  end
+end
