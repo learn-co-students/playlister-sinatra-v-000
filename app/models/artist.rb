9746880d56e1e69fbf25d74378d9f0b587140df0
bortch89 <bortch@gmail.com>
2017-04-07 20:17:58 +0200

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethdods
+
   has_many :songs
   has_many :genres, through: :songs
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethdods
+  
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
