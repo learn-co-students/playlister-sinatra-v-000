f9d8ed2eca498b2c2d836796fb0afdf67892751b
Michael Ries <michael.ries@gmail.com>
2017-04-20 21:03:26 +0000

    add initial db migration

--- db/migrate/20170420204844_create_database_tables.rb
+++ db/migrate/20170420204844_create_database_tables.rb
@@ -10,9 +10,12 @@ class CreateDatabaseTables < ActiveRecord::Migration
     end
 
     create_table :genres do |t|
+      t.string :name
     end
 
     create_table :song_genres do |t|
+      t.integer :genre_id
+      t.integer :song_id
     end
   end
 end
