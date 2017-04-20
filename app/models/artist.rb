6563909b64a1a522c8c663eb3a51a6dfd5ec759e
Michael Ries <michael.ries@gmail.com>
2017-04-20 22:16:14 +0000

    passing artist specs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
   has_many :songs
-  has_many :genres through: :songs
+  has_many :genres, through: :songs
 end
