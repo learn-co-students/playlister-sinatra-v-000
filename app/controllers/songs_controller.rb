00be4fc891ae6caa50833bce0117ecc389579f32
Michael Ries <michael.ries@gmail.com>
2017-04-20 22:44:54 +0000

    added empty views and new controllers for song, artist, genre

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,9 @@
+class SongsController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/' do
+    erb :index
+  end
+end
