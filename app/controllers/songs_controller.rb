139a7fe439e02b20c48a975009db2e4530060f45
Michael Ries <michael.ries@gmail.com>
2017-04-20 23:42:47 +0000

    fixed links in index pages to use slugs; added songs/show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,4 +7,9 @@ class SongsController < Sinatra::Base
     @songs = Song.all
     erb :'songs/index'
   end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb 'songs/show'
+  end
 end
