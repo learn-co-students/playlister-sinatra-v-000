7d83a2663361e3fd6425d396cbd6530411982d20
Michael Ries <michael.ries@gmail.com>
2017-04-21 02:07:16 +0000

    add songs/edit.  all tests pass

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,8 +26,23 @@ class SongsController < Sinatra::Base
     redirect to "songs/#{song.slug}"
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
+  end
+
+  post '/songs/:slug' do
+    song = Song.find_or_create_by({name: params[:name]})
+    song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    song.genres << params[:genres].collect {|g| Genre.find(g)}
+    song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to "songs/#{song.slug}"
   end
 end
