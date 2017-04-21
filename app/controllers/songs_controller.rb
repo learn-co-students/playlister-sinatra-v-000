da57e14ef6facc53c4c7d8408cadd169275a4f4f
Michael Ries <michael.ries@gmail.com>
2017-04-21 01:24:19 +0000

    added seed data

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,18 @@ class SongsController < Sinatra::Base
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    song = Song.create({name: params[:name]})
+    song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    song.genres << params[:genres].collect {|g| Genre.find(g)}
+    song.save
+    redirect to "songs/#{song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
