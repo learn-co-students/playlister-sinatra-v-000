e32f13d32375075cea7c51cf5fbb8e9c6191c0ba
Erika Hughes <ehughes@knights.ucf.edu>
2017-05-01 15:15:19 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,9 +7,31 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    # binding.pry
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    binding.pry
+    @song = Song.create(name: params[:Name])
+    genres = params[:genres]
+    if !params["Artist Name"].empty?
+      @song.artist = Artist.create(name: params["Artist Name"])
+    end
+    genres.each do |id|
+      @song.genres << Genre.find_by_id(id)
+    end
+    @song.save
+    redirect "songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     # binding.pry
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
+
+
 end
