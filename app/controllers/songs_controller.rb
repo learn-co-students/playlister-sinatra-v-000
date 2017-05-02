97ac6721864162894f070ec28ce8b92492dbbf8e
Scott Atkins <satkins16@gmail.com>
2017-05-02 13:09:23 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -15,11 +19,29 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(name: params[:Name])
-    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["artist_name"])
     @song.genre_ids = params[:genres]
     @song.save
 
+    flash[:message] = "Successfully created song."
+
+    redirect("/songs/#{@song.slug}")
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  post '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    
     redirect("/songs/#{@song.slug}")
   end
 
