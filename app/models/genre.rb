89815857499cba4425f79d8054804126c556dc01
Feiran Wang <wangfeiranpku@163.com>
2017-05-05 00:20:40 +0000

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,15 @@
+class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :artists, :through => :songs
+  has_many :songs, :through => :song_genres
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|t| t.slug == slug}
+  end
+
+
+end
