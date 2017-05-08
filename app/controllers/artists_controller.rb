cde12ec8188c7b6eec8efcf6fc0be58de4c8454c
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-08 18:50:09 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,23 @@
+class ArtistsController < ApplicationController
+
+  get '/artists' do 
+    @artists = Artist.all
+    erb :'artists/index'
+  end
+
+  get '/artists/new' do 
+
+    erb :'artists/new'
+  end
+
+  post '/artists/new' do 
+
+    redirect "/artists/#{slug}"
+  end
+
+  get '/artists/:slug' do 
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
+  end
+
+end
---
+++ app/models/song_genre.rb
@@ -0,0 +1,6 @@
+class SongGenre < ActiveRecord::Base
+  belongs_to :genre
+  belongs_to :song
+
+
+end
