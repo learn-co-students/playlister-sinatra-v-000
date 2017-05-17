dc01f087e6a6f17a3db7ed97a29816dd36bb9535
Jdville03 <jarrel.delottinville@gmail.com>
2017-05-17 12:17:07 -0400

    Refactored.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,5 @@
 class SongsController < ApplicationController
 
-  enable :sessions
   use Rack::Flash
 
   get '/songs' do
@@ -19,15 +18,8 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    song = Song.create(name: params[:name])
-    params[:genres].each do |id|
-      song.genres << Genre.find(id)
-    end
-    if artist = Artist.find_by(name: params[:artist])
-      song.artist = artist
-    else
-      song.create_artist(name: params[:artist])
-    end
+    song = Song.create(params[:song])
+    song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     song.save
     flash[:message] = "Successfully created song."
     redirect to "/songs/#{song.slug}"
@@ -41,12 +33,8 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     song = Song.find_by_slug(params[:slug])
-    song.genres = params[:genres].collect{|id| Genre.find(id)}
-    if artist = Artist.find_by(name: params[:artist])
-      song.artist = artist
-    else
-      song.create_artist(name: params[:artist])
-    end
+    song.update(params[:song])
+    song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     song.save
     flash[:message] = "Successfully updated song."
     redirect to "/songs/#{song.slug}"
