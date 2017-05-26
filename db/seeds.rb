4ce48f45aaadea84ad1e55e2b7cd3b27cedd9320
Camille Jackson <cj@mail.co.de>
2017-05-26 23:21:09 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+sohn = Artist.create(name: "SOHN")
+Song.create(name: "Artifice", artist: sohn)
+Song.create(name: "The Wheel", artist: sohn)
