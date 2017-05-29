66e576d1e7099b48c2086257cf6a49b988a4e42d
machen2 <victoriamachen@gmail.com>
2017-05-29 05:33:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-#  get '/' do
-#    erb :index
-#  end
+  get '/' do
+    erb :index
+  end
 end
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,8 @@
 class ArtistsController < ApplicationController
-
-
+  
   get '/artists' do
-  #  @artists = Artist.all
-  #  erb :'/artists/index'
-    "Hello World"
+    @artists = Artist.all
+    erb :'/artists/index'
   end
 
   get '/artists/:slug' do
