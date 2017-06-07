3c77e0f5d8e2ff4e8ef59d966ea70fb9b40cde77
WilliamBarela <william.barela.dev@gmail.com>
2017-06-08 00:04:51 +0900

    updated models to include join table and parsing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ActiveRecord::Base
+  extend Parser::ClassMethods
+  include Parser::InstanceMethods
+
   has_many :songs, dependent: :destroy
   has_many :genres, through: :songs
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,8 @@
 class Genre < ActiveRecord::Base
-  has_many :songs
+  extend Parser::ClassMethods
+  include Parser::InstanceMethods
+
+  has_many :song_genres
+  has_many :songs, through: :song_genres
   has_many :artists, through: :songs
 end
