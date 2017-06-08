8d13c6c88887ef1a344922909b604a2bd9fdb3dd
Joseph Kim <josephkim0224@gmail.com>
2017-06-08 19:09:35 +0000

    Done.

---
+++ app/models/artist.rb
@@ -0,0 +1,13 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def self.find_by_slug(slug)
+    Artist.all.find { |artist| artist.slug == slug }
+  end
+
+  def slug
+    name.downcase.gsub(' ', '-')
+  end
+
+end
