5d5b0dbd9e14c1f71fd440d39fea99e5f5b8d4d4
WilliamBarela <william.barela.dev@gmail.com>
2017-06-08 18:22:00 +0900

    lacking only flash and edit specs

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -4,4 +4,11 @@ class ArtistController < ApplicationController
     @artists = Artist.all
     erb :'/artists/index'
   end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    @songs = @artist.songs
+    @genres = @artist.genres
+    erb :'/artists/show'
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,27 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'songs/index'
   end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.find_or_create_by(:name => params[:Name])
+    @artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.artist = @artist
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    #flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @artist = @song.artist
+    @genres = @song.genres
+    erb :'/songs/show'
+  end
 end
