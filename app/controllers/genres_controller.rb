ab62bc49ca0d6ab663724b8871c5ba8365ca83da
Villy Siu <villysiu@gmail.com>
2017-06-16 19:20:44 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    @songs = Song.find(@genre.song_genres.collect{|g| g.song_id})
+
+    erb :'/genres/show'
+  end
+
+end
