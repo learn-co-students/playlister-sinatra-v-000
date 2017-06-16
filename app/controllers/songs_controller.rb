d341fef7212450abfc41b3b08ca1d42652c3a2b5
Niki Lee <ew1415048nk@gmail.com>
2017-06-16 20:41:27 +0900

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
+require 'rack-flash'
 class SongsController <ApplicationController
+use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -16,31 +18,29 @@ class SongsController <ApplicationController
     @song.genre_ids = params[:genres]
     # binding.pry
     @song.save
-    redirect to "/songs/#{@song.slug}"
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
   end
+
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/edit'
   end
 
-  post '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    @song.name = params["Name"]
-    @song.artist = Artist.find_or_create_by(name:params["Artist Name"])
-    @song.genre_ids = params[:genres]
-    @song.save
-    redirect to "/songs/#{@song.slug}"
-  end
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    # binding.pry
     erb :'/songs/show'
   end
 
-
-
-
-
+  patch '/songs/:slug' do
+    binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name:params["Artist Name"])
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
 
 end
--- config.ru
+++ config.ru
@@ -8,4 +8,5 @@ use Rack::MethodOverride
 use SongsController
 use ArtistsController
 use GenresController
+
 run ApplicationController
