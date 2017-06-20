d5aa7bc6ee31158173dce0f27e03605e40e8090e
Terry Blue <terryblue@comcast.net>
2017-06-20 01:48:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < Sinatra::Base
 
+  require 'rack-flash'
+
+  enable :sessions
+  use Rack::Flash
+
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,22 +6,22 @@ class SongsController < ApplicationController
 	end
 
 	get '/songs/new' do
-
+		binding.pry
 		erb :'/songs/new'
-	end
-
-	get '/songs/:id' do
-
-		erb :'/songs/show'
 	end
 
 	get '/songs/:slug' do
 		binding.pry
-		Song.slug
+
+	end
+
+	get '/songs/:id' do
+		binding.pry
+		erb :'/songs/show'
 	end
 
 	get '/songs/:slug/edit' do
-
+		binding.pry
 	end
 
 end
