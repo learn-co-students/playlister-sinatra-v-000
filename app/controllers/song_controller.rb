b040a672d7a2c9453b8dc52a6f6cf79f9aba0adc
Will-Sauer <will@heueso.com>
2017-06-23 11:07:05 -0700

    finished

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -16,27 +16,24 @@ end
 
 
 post '/songs' do
-      binding.pry
-     @song = Song.create(:name => params[:song][:name])
-     
-
-     @genres = []
-    #  binding.pry
-     params["song"]["genres"].each {|genre| 
-                                   binding.pry
-                                   @genre = Genre.find_or_create_by(:name => genre[:name])
-                                   @genres << @genre
-     }
-     @song.genres = @genres
-
-     
-     @song.artist = Artist.find_or_create_by(:name => params["song"]["artist"])
-     @song.save
-
-
+      @song = Song.create(:name => params[:song][:name])
+      
+ 
+      @genres = []
+      params["song"]["genres"].each {|genre| 
+                                    @genre = Genre.find_or_create_by(:name => genre[:name])
+                                    @genres << @genre
+      }
+      @song.genres = @genres
+ 
+      
+      @song.artist = Artist.find_or_create_by(:name => params["song"]["artist"])
+      @song.save
 
 redirect "/songs/#{@song.slug}"
 end
+
+
 
 #new form
 
@@ -50,15 +47,28 @@ end
 
 
 get '/songs/:slug/edit' do
-    
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
 end
 
 
 
 
-# #update
+#update
 post '/songs/:slug' do
-    
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params["song"]["name"]
+    @song.artist = Artist.find_or_create_by(:name => params["song"]["artist"])
+
+     @genres = []
+     params["song"]["genres"].each {|genre| 
+                                   @genre = Genre.find_or_create_by(:name => genre[:name])
+                                   @genres << @genre
+     }
+     @song.genres = @genres
+     @song.save
+
+     redirect "/songs/#{@song.slug}"
 end
 
 
