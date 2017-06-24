90bcd3e944c2c569365c66cde60489ad9bd07a3f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-23 18:28:58 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
-    @song = Song.create(name: "Dirty Paws")
-    @genre = Genre.create(name: "Alt")
-    @artist = Artist.create(name: "Of Monsters And Men")
-
-    @song.song_genres.create(genre: @genre)
-    @song.artist = @artist
-  
-    @song.save
+@parser = LibraryParser.new
+@parser.call
