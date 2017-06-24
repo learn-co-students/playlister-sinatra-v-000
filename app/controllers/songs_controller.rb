9fe7054db3dfe2b74e3cd158a5b87afd853b82db
Terry Blue <terryblue@comcast.net>
2017-06-24 19:29:37 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,15 +6,18 @@ class SongsController < ApplicationController
 	end
 
 	get '/songs/new' do
-		binding.pry
+		@genres = Genre.all
 		erb :'/songs/new'
 	end
 
 	get '/songs/:slug' do
 		@song = Song.find_by_slug(params[:slug])
+		# @song.artist = Artist.find_by_id(@song.artist_id)
+		# @song.genre = Genre.find_by_id(@song.genre_id)
+		# @song.save
 		@song_name = @song.name
-		@song_artist = Artist.find_by_id(@song.artist_id)
-		@song_genre = Genre.find_by_id(@song.genre_id)
+		@song_artist = @song.artist
+		@song_genre = @song.genre
 		erb :'/songs/show'
 	end
 
@@ -25,6 +28,11 @@ class SongsController < ApplicationController
 
 	get '/songs/:slug/edit' do
 		binding.pry
+	end
+
+	post '/songs' do
+		binding.pry
+		redirect to 'songs/:slug'
 	end
 
 end
