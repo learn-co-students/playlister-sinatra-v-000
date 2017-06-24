a9c221e0670f38eb62e47f2111b226c498ad969c
Amanda George <github email address>
2017-06-24 18:39:52 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,8 @@
+require 'pry'
 class SongsController < ApplicationController
 
   get '/songs' do
-    erb :'songs/index'
+    erb :"songs/index"
   end
 
   get '/songs/new' do
@@ -13,5 +14,33 @@ class SongsController < ApplicationController
     erb :'songs/show'
   end
 
+  post '/songs' do
+    song = Song.new
+    song.name = param["Name"]
+    if params["Artist Name"] != ""
+      if Artist.exists?(name: params["Artist Name"])
+        song.artist = Artist.find_by(name: params["Artist Name"])
+      else
+        song.build_artist(name: params["Artist Name"])
+      end
+    else
+      song.artist = Artist.find(params["Artist"])
+    end
+    song.save
+
+    flash[:message] = "Successfully created song."
+    redirect to ("songs/#{song.slug}")
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
 
 end
