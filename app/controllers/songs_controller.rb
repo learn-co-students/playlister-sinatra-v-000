e2d135362d0ab2e0ac8d6dabd1b8c2d0e7889b9a
Terry Blue <terryblue@comcast.net>
2017-06-27 21:52:43 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,6 +36,7 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
+    binding.pry
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
