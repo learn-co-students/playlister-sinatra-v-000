382b0bc138bc60c8b1df64abd2bc7b419649c98f
Samson Yuwono <samson.yuwono@gmail.com>
2017-06-30 18:11:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,11 +21,10 @@ use Rack::Flash
   end
 
   post '/songs' do
-    @song= Song.create(name: params["Name"])
-    @song.artist= Artist.find_or_create_by(name: params["Artist Name"])
-    @song.genre_ids= params[:genre]
+    @song= Song.create(name: params["song_name"])
+    @song.artist= Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids= params[:genres]
     @song.save
-
     flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
@@ -35,7 +34,7 @@ use Rack::Flash
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/edit'
   end
-  #
+
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.name= params[:song_name]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+hip_hop = Genre.create(name: "Hip Hop")
+pop = Genre.create(name: "Pop")
+rock = Genre.create(name: "Rock")
