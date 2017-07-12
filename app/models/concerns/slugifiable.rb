8ac4f4c24d2d444a4324d7e4398df84acdf80901
denisedearman <dearmandenise@gmail.com>
2017-07-12 14:30:10 -0400

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,17 @@
+module Slugifiable
+
+  module InstanceMethods
+    def slug
+      name.downcase.gsub(" ", "-")
+    end
+  end
+
+  module ClassMethods
+
+    def find_by_slug(slug)
+      self.all.find{|x| x.slug == slug}
+    end
+
+  end
+
+end
