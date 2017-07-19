e59667420448e6b8d537c19621efb572d28dc671
Susanna Klosko <sdklos@gmail.com>
2017-07-19 06:55:43 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,16 @@
+class GenresController < ApplicationController
+
+  get '/genres' do
+
+      erb :'/genres/index'
+  end
+
+  get '/genres/new' do
+    @genre = Genre.create(params)
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
+end
