c6aa673c7a2627af729a7df9bcec8d8defb3ae57
gb23 <gregbenj@hotmail.com>
2017-07-21 13:36:04 -0700

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,27 +7,7 @@ module Slugifiable
 
     module ClassMethods
         def find_by_slug(slug)
-            instance = self.new
-            if instance.is_a?(Artist)
-                Artist.all.collect { |artist|
-                    if artist.slug == slug
-                        artist
-                    end
-                }.compact.first
-          
-            elsif instance.is_a?(Genre)
-                Genre.all.collect { |genre|
-                    genre if genre.slug == slug
-                }.compact.first
-
-            elsif instance.is_a?(Song)
-                Song.all.collect { |song|
-                    song if song.slug == slug
-                }.compact.first
-
-            else
-                raise instance.inspect
-            end            
+            self.all.find{ |instance| instance.slug == slug }
         end
     end
 
