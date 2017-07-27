08884290c8375b18c903658033e428e8910237e4
github username <github email address>
2017-07-27 15:34:44 -0400

    Done.

---
+++ app/controllers/artist_controller.rb
@@ -0,0 +1,16 @@
+class ArtistController < Sinatra::Base
+set :views, Proc.new { File.join(root, "../views/") }
+
+
+  get "/artists" do
+    @artists = Artist.all
+    erb :"artists/index"
+  end
+
+  get "/artists/:slug" do
+
+    @artist = Artist.find_by_slug(params[:slug])
+    #binding.pry
+    erb :"artists/show"
+  end
+end
