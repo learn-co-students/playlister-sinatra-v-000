0874d1c0340de71b7c93927d4fa83d52a56506d4
Scott Wakeman <swakeman@ca.rr.com>
2017-08-15 06:19:28 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,12 @@ class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
 
+  def slug
+    self.name.downcase.split(/\W/).reject { |w| w.empty? }.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    # use FIND method to find genre from Genre.all whos slug == slug argument provided
+    Genre.all.find{|genre| genre.slug == slug}
+  end
 end
