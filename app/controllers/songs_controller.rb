321661a699dff9b7de2d95dce9bffbd848c6b3d3
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-15 15:59:59 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,20 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    Song.all.each do |song|
+      song
+      binding.pry
+    end
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    binding.pry
+    @song = Song.create(params[:song])
+    redirect "songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
