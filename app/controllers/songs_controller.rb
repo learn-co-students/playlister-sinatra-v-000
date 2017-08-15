2eafd5eebc6b177410128c85f3fd71ae6d98974c
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-15 22:01:18 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,17 +9,20 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
   post '/songs' do
     @song = Song.create(name: params['Name'])
     @song.artist = Artist.find_or_create_by(name: params['Artist Name'])
     @song.genre_id = params[:genre]
     @song.save
-    redirect("/songs/#{@song.slug}")
+    #binding.pry
+    redirect "/songs/#{@song.slug}"
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
-  end
+
 
 end
