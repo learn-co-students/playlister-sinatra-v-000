6bfd438e0c3a02120f7afc2a39d35ae7ca0305a7
Justin Acker <justin@justinacker.com>
2017-08-17 04:01:10 +0000

    all but one test passing

--- app.rb
+++ app.rb
@@ -1,37 +1,37 @@
-require_relative 'config/environment'
-
-class App < Sinatra::Base
-
-  get '/' do
-    erb :index
-  end
-
-  get '/songs' do
-    erb :'songs/index'
-  end
-
-  get '/genres' do
-    erb :'genres/index'
-  end
-
-  get '/artists' do
-    erb :'artists/index'
-  end
-
-  get '/songs/new' do
-
-  end
-
-  get '/songs/:slug' do
-
-  end
-
-  get '/artists/:slug' do
-
-  end
-
-  get '/genres/:slug' do
-
-  end
-
-end
+# require_relative 'config/environment'
+#
+# class App < Sinatra::Base
+#
+#   get '/' do
+#     erb :index
+#   end
+#
+#   get '/songs' do
+#     erb :'songs/index'
+#   end
+#
+#   get '/genres' do
+#     erb :'genres/index'
+#   end
+#
+#   get '/artists' do
+#     erb :'artists/index'
+#   end
+#
+#   get '/songs/new' do
+#
+#   end
+#
+#   get '/songs/:slug' do
+#
+#   end
+#
+#   get '/artists/:slug' do
+#
+#   end
+#
+#   get '/genres/:slug' do
+#
+#   end
+#
+# end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,11 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
-  end
-
-  get '/songs' do
-    erb :'songs/index'
-  end
-
-  get '/genres' do
-    erb :'genres/index'
-  end
-
-  get '/artists' do
-    erb :'artists/index'
-  end
-
-  get '/songs/new' do
-
-  end
-
-  get '/songs/:slug' do
-
-  end
-
-  get '/artists/:slug' do
-
-  end
-
-  get '/genres/:slug' do
-
   end
 
 end
