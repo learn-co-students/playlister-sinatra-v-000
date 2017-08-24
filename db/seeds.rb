0c4c7f52769b08d4e16675abfb40094fc7e7a38f
zoebisch <zoebisch@gmail.com>
2017-08-23 21:08:55 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
 # Add seed data here. Seed your database with `rake db:seed`
+# @artist = Artist.create(name: "Pink Floyd")
+# @g1 = Genre.create(name: "Rock")
+# @song = Song.create(name: "Eclipse", artist: @artist)
+# @song.genres << @g1
+# Genre.create(name: "Acid Rock")
+# Genre.create(name: "Electronic")
 
+Artist.create(name: "Pink Floyd")
+Genre.create(name: "Acid Rock")
+Genre.create(name: "Electronic")
