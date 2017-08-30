28aa827ce32fb5d9921e0e16e48240038a37cfbf
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-30 15:25:04 -0400

    Views and Controllers for Models added. Spec for 04_basic_view_spec all passing for views and controllers

--- app/models/modules/slugify.rb
+++ app/models/modules/slugify.rb
@@ -1,14 +1,15 @@
 module Slugify
 	module ClassMethods
 		def find_by_slug(slug)
-			@slug = slug.split("-").map { |n| n.capitalize }.join(" ")
-			self.find_by(name: @slug)
+			# @slug = slug.split("-").map { |n| n.capitalize }.join(" ")
+			# self.find_by(name: @slug)
+			self.all.find { |name| name.slug == slug}
 		end
 	end
 
 	module InstanceMethods
 		def slug
-				self.name.gsub(" ", "-").downcase
+				self.name.downcase.gsub(" ", "-")
 	  	end
 
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+beyonce = Artist.new(name: "Beyonce")
+halo = Song.create(name: "Halo")
+beyonce.songs << halo
+beyonce.save
