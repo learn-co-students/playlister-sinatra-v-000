cc41fbdf87d5153035ca0cc8c361aaa09c2bab57
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-30 20:44:21 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,45 @@ class SongsController < ApplicationController
 		erb :'songs/index'
 	end
 
-	get '/songs/:slug' do	
+	get	'/songs/new' do
+		@genres = Genre.all
+		erb :'songs/new'
+	end
+
+	get '/songs/:slug' do
 		@song = Song.find_by_slug(params[:slug])
 		erb :'songs/show'
+	end
+
+	get '/songs/:slug/edit' do
+		@song = Song.find_by_slug(params[:slug])
+
+		erb :'songs/edit'
+	end
+
+	post '/songs' do
+		@song = Song.create(params["song"])
+		@artist = Artist.all.find {|a| a.name == params["artist"]["name"]}
+		 if !params["artist"].empty? && !@artist
+		 	@song.artist = Artist.create(name: params["artist"]["name"])
+		 else
+		 	@song.artist = @artist
+		 end
+		@song.save
+		# flash[:message] = "Successfully created song."
+		redirect to "/songs/#{@song.slug}"
+	end
+
+	patch '/songs/:slug' do
+		
+		@song = Song.find_by_slug(params[:slug])
+		@song.update(params["song"])
+		if !params["artist"].empty? && !@artist
+		 	@song.artist = Artist.create(name: params["artist"]["name"])
+		 end
+		flash[:message] = "Successfully updated song."
+		binding.pry
+		redirect to "songs/#{@song.slug}"
 	end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 # Add seed data here. Seed your database with `rake db:seed`
-beyonce = Artist.new(name: "Beyonce")
+beyonce = Artist.create(name: "Beyonce")
+Artist.create(name: "Sam Hunt")
+Artist.create(name: "Jayz")
+Song.create(name: "In a Small Town")
+Song.create(name: "99 problems")
 halo = Song.create(name: "Halo")
 beyonce.songs << halo
 beyonce.save
+Genre.create(name: "Hip Hop")
+Genre.create(name: "Rock")
+Genre.create(name: "Country")
