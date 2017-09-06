de369228aaf80b3f6f72ea6ea5b74e494adb08f5
github username <github email address>
2017-09-05 22:04:19 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,21 +1,24 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     erb :'/songs/index'
   end
 
-  post '/songs' do
-    # binding.pry
-    @song = Song.create(name: params[:name])
-    @song.artist = Artist.find_or_create_by(params[:artist])
-    @song.genre_ids = params[:genres]
-    @song.save
-
-    redirect to "/songs/#{@song.slug}"
-  end
-
   get '/songs/new' do
     erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params["name"])
+    @song.artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.genre_ids = params["genres"]
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
@@ -26,11 +29,19 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
+    binding.pry
+    @song.artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.genre_ids = params["genres"]
+    @song.save
 
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
 
+    erb :'/songs/edit'
   end
 
 end
