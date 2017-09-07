53cb1d3ebf8a73ac679ac9558648b81c409deaa6
github username <github email address>
2017-09-06 20:48:49 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,22 +28,22 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
-  post '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    if !params[:artist_name].empty?
-      @song.artist = Artist.find_or_create_by(name: params[:artist_name])
-    end
-    @song.genre_ids = params[:genres]
-    @song.save
-
-    flash[:message] = "Successfully updated song."
-    redirect to "/songs/#{@song.slug}"
-  end
-
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
 
     erb :'/songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.genre_ids = params[:genres]
+    if !params[:artist_name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    end
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
