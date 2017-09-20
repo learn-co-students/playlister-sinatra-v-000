9bd2e9a8e5a11035fae4bcea37a6e97b978ff282
alexisadorn <alexisadorn@gmail.com>
2017-09-20 18:54:21 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,9 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class SongsController < Sinatra::Base
+  enable :sessions
+  use Rack::Flash
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
@@ -9,7 +14,31 @@ class SongsController < Sinatra::Base
   end
 
   get '/songs/new' do
+    @genres = Genre.all
     erb :"songs/new"
+  end
+
+  post '/songs' do
+    @song = Song.create(:name => params[:song][:name])
+
+    artist_entry = params[:song][:artist]
+    if Artist.find_by(:name => artist_entry)
+      artist = Artist.find_by(:name => artist_entry)
+    else
+      artist = Artist.create(:name => artist_entry)
+    end
+    @song.artist = artist
+
+    genre_selections = params[:song][:genres]
+    genre_selections.each do |genre|
+      @song.genres << Genre.find(genre)
+    end
+
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect to "songs/#{@song.slug}"
+
   end
 
   get '/songs/:slug' do
