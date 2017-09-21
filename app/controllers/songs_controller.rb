c623f7831b520f954c51117a9cd72499bad62051
alexisadorn <alexisadorn@gmail.com>
2017-09-20 21:04:49 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,35 @@ class SongsController < Sinatra::Base
     erb :"songs/show"
   end
 
+  patch '/songs/:slug' do
+    song = Song.find_by_slug(params[:slug])
+    song.name = params[:song][:name]
+
+    artist_name = params[:song][:artist]
+    if Artist.find_by(:name => artist_name)
+      if song.artist.name != artist_name
+        song.artist = Artist.find_by(:name => artist_name)
+      end
+    else
+      song.artist = Artist.create(:name => artist_name)
+    end
+
+    if song.genres
+      song.genres.clear
+    end
+    genres = params[:song][:genres]
+    genres.each do |genre|
+      song.genres << Genre.find(genre)
+    end
+
+    song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "songs/#{song.slug}"
+  end
+
   get '/songs/:slug/edit' do
+    slug = params[:slug]
+    @song = Song.find_by_slug(slug)
     erb :"songs/edit"
   end
 end
