c9d5ca74b582d380df02ce2a9e377e4917272f35
Amelie Oller <amelieoller@gmail.com>
2017-09-21 19:11:11 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,42 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    session[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genres = params[:genres]
+    @song.save
+    session[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @message = session[:message]
+    erb :'/songs/show'
+  end
+
+end
