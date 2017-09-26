feb6fb0dab9252fb1f299f447966a723f5de48ff
CodeDad70 <eric.strickler@gmail.com>
2017-09-26 16:48:07 -0600

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,18 @@
+require 'rack-flash'
+
+class ArtistsController < ApplicationController
+  use Rack::Flash
+  
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do 
+    @artist = Artist.find_by_slug(params[:slug])
+
+    erb :'artists/show'
+  
+  end
+end
+
