3dbb022254c3af5e93e565ad9375974b863e8742
Leigh Passamano <leighayna.josele@gmail.com>
2017-09-26 02:32:21 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,12 +15,10 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    #binding.pry
-    song = Song.create(name: params[:song_name])
-    artist = Artist.find_or_create_by(name: params[:artist_name])
-    song.artist = artist
+    song = Song.create(name: params[:song][:name])
+    song.artist = Artist.find_or_create_by(name: params[:song][:artist])
 
-    params[:genres].each do |genre_id|
+    params[:song][:genre_ids].each do |genre_id|
       song.genres << Genre.find(genre_id)
     end
     song.save
@@ -39,12 +37,18 @@ class SongsController < ApplicationController
     erb :'songs/edit'
   end
 
-  post '/songs/:id' do
-    binding.pry
-    #song = Song.find(params[:id])
-    #song.update(params)
-    #flash[:message] = "Successfully updated song."
-    #redirect "/songs/#{song.slug}"
+  post '/songs/:slug' do
+    song = Song.find_by_slug(params[:slug])
+    song.name = params[:song][:name]
+    song.artist = Artist.find_or_create_by(name: params[:song][:artist])
+    song.genres.clear
+
+    params[:song][:genre_ids].each do |genre_id|
+      song.genres << Genre.find(genre_id)
+    end
+    song.save
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{song.slug}"
   end
 
 end
