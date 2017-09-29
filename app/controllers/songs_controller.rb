b74c9499457aea0d4102c8daf44d877f425c8318
Robert Peckham <peckham.rob@gmail.com>
2017-09-29 14:39:36 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,11 +35,11 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
-  patch '/songs/:slug/' do
-    binding.pry
+  patch '/songs/:slug' do
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Artist.find_or_create_by(params[name: params["Artist Name"]])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.save
 
     flash[:message] = "Successfully updated song."
