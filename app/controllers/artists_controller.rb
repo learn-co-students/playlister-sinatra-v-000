edd269944fb77a0524565b706a5094c3a77baada
Tina Hang <mstinahang@gmail.com>
2017-10-05 06:02:57 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
+  end
+
+end
