71d1c787f16d2c3e6b91ee07dd496cf04ebd95c7
Annette Maza <nettymaza@gmail.com>
2017-10-14 20:05:12 +0000

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,18 @@
+require 'pry'
+module Slugifiable
+  
+  module InstanceMethods
+    def slug
+      name.downcase.gsub(" ","-")
+    end
+  end
+
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find do |artist|
+        artist.slug == slug
+      end
+    end
+  end
+
+end
