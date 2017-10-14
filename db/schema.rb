ff8723ee12a20914c1d9a51e0c5aef7ea6acb256
Annette Maza <nettymaza@gmail.com>
2017-10-14 16:06:38 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014013203) do
+ActiveRecord::Schema.define(version: 20171014154944) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
