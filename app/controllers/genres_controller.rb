c413d6b9878ffd8a812d699101c1cf4e6ff0568c
daniellecroxton <danikrist@gmail.com>
2017-10-15 11:16:04 -0600

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,6 +5,7 @@ class GenresController < ApplicationController
   end
 
   get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
     erb :'genres/show'
   end
 
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,20 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+    get '/songs/new' do
+      erb :'songs/new'
+    end
+
   get '/songs/:slug' do
-    # @song = Song.find_by_slug(params[:slug])
+    @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.new(params[:song])
+    @song.artist = Artist.find_or_create_by(params[:artist])
+    @song.save
+    redirect "/songs/#{@song.slug}"
   end
 
 end
