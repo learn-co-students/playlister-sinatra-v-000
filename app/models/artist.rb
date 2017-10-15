803b77521b3d6ae60b7a8e1a67e5a1b6d4bc75a2
daniellecroxton <danikrist@gmail.com>
2017-10-14 22:53:53 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,30 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def slug
+    slug = self.name.strip.downcase
+
+    #blow away apostrophes
+    slug.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    slug.gsub! /\s*@\s*/, " at "
+    slug.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with hyphen
+     slug.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '-'
+
+     #convert double hyphens to single
+     slug.gsub! /-+/,"-"
+
+     #strip off leading/trailing hyphens
+     slug.gsub! /\A[-\.]+|[-\.]+\z/,""
+
+     slug
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |artist| artist.slug == slug}
+  end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,29 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
 
+  def slug
+    slug = self.name.strip.downcase
+
+    #blow away apostrophes
+    slug.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    slug.gsub! /\s*@\s*/, " at "
+    slug.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with hyphen
+     slug.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '-'
+
+     #convert double hyphens to single
+     slug.gsub! /-+/,"-"
+
+     #strip off leading/trailing hyphens
+     slug.gsub! /\A[-\.]+|[-\.]+\z/,""
+
+     slug
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |song| song.slug == slug}
+  end
 end
