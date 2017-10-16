91e965904478393364fadadf1342269fbb637804
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-15 22:17:29 -0600

    Added edit song route.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,17 +32,19 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
     erb :'/songs/edit'
   end
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.update(params["song"])
-    @song.artist = Artist.create(name: params[:artist][:name])
+    @song.name = params['Name']
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
     @song.save
 
+    flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
-
   end
 
 
