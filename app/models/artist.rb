42e9c8938ed36dc3763f51be6d54862b5f3a169b
Derek  Pelt <derekpelt@gmail.com>
2017-10-18 09:04:59 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres, through: :songs 
+  has_many :genres, through: :songs
+
+  def slug
+    slug = self.name.downcase.strip.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    unslug = slug.gsub("-", " ").split.map(&:capitalize).join(" ")
+    Artist.find_by(name: unslug) 
+  end
 end
