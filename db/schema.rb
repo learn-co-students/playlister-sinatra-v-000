67eeeb87c282f0a2a623bd64957dd5aa99bed7c2
Derek  Pelt <derekpelt@gmail.com>
2017-10-18 08:50:02 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018083903) do
+ActiveRecord::Schema.define(version: 20171018084828) do
 
   create_table "artists", force: :cascade do |t|
-    t.string  "name"
-    t.integer "genre_id"
+    t.string "name"
   end
 
   create_table "genres", force: :cascade do |t|
