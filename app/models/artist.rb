b7e23c83ef694fb56c8d4b0b88779b053e2fead5
Seb Gertz <sebgertz@gmail.com>
2017-10-20 07:50:13 +0000

    passing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres, through: :songs
+  has_many :genres, through: :songs #:songs is the intermediary that connects artist to genre
+
+  def slug
+    @slug = self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    @artist = self.all.find{|artist| artist.slug == slug}
+  end
+
 end
