f075c956aa1d04ddd77f8cbc3e3ea4b2bb138389
Christine Tran <christinetran825@gmail.com>
2017-10-20 18:18:07 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,8 +37,7 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Artist.find_by(name: params[:artist][:name])
-    @song.genre_ids = params[:genres]
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
 
     flash[:message] = "Successfully updated song."
