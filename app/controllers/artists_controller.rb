1cf998013cb02d603469caa8287adb0e19ec4f7a
Derek  Pelt <derekpelt@gmail.com>
2017-10-22 05:52:01 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,8 @@
 class ArtistsController < ApplicationController
 
+  get '/artists' do
+    @artists =Artist.all
+    erb :'/artists/show'
+  end
+
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,10 @@
 class GenresController < ApplicationController
 
+  get '/genres' do
+    @genress = Genre.all
+    erb :'/genres/show'
+  end
+ 
+
+
 end
