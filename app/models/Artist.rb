11dfc72ffe06f55562085874db5b50b042a22702
Mary Benitez <meebenitez@gmail.com>
2017-10-23 17:41:15 +0000

    Done.

--- app/models/Artist.rb
+++ app/models/Artist.rb
@@ -1,4 +1,20 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+
+    self.all.each do |artist|
+      if artist.slug == slug
+        match = artist
+      end
+    end
+  end
+
+
+
 end
--- app/models/Song.rb
+++ app/models/Song.rb
@@ -2,4 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+
+    self.all.each do |song|
+      if song.slug == slug
+        match = song
+      end
+    end
+  end
+
+
 end
