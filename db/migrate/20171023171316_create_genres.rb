e146458794b26a1d6800e7e61227b34e7bbbddce
Mary Benitez <meebenitez@gmail.com>
2017-10-23 17:18:58 +0000

    Done.

--- db/migrate/20171023171316_create_genres.rb
+++ db/migrate/20171023171316_create_genres.rb
@@ -1,4 +1,6 @@
 class CreateGenres < ActiveRecord::Migration
   def change
+    create_table :genres do |t|
+      t.string :name
   end
 end
--- db/migrate/20171023171333_create_song_genres.rb
+++ db/migrate/20171023171333_create_song_genres.rb
@@ -1,4 +1,7 @@
 class CreateSongGenres < ActiveRecord::Migration
   def change
+    create_table :song_genres do |t|
+      t.integer :song_id
+      t.integer :genre_id
   end
 end
