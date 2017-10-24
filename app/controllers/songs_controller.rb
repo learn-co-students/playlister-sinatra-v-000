839d78305408cd9763b66648a4b4ec8911f00708
Seb Gertz <sebgertz@gmail.com>
2017-10-24 13:36:39 +0000

    Tests

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,14 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
+
   get '/songs' do
-    erb :'songs/index'
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
@@ -9,6 +17,48 @@ class SongsController < ApplicationController
     erb :'songs/show'
   end
 
+  post '/songs' do
+  #  binding.pry
+    @song = Song.create(name: params[:song][:name])
+    if !params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    end
 
+    if !params[:genre][:name].empty?
+      @song.genres << Genre.create(name: params[:genre][:name])
+    else
+      @song.genre_ids = params[:genres]
+    end
+    @song.save
+
+    flash[:message] = "Successfully created song."
+
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'/songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+
+    if !params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    end
+
+    if !params[:genre][:name].empty?
+      @song.genres << Genre.create(name: params[:genre][:name])
+    else
+      @song.genre_ids = params[:genres]
+    end
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
 end
