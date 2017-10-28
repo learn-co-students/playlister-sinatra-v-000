bf1995fae42291e8a85f1f18662dd55eb7ba2641
Eclecticdynastye <bklyngal1994@gmail.com>
2017-10-27 20:58:26 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,18 @@
+class ArtistsController < ApplicationController
+  
+  get '/artists' do 
+
+    @artists = Artist.all 
+
+    erb :'artists/index'
+  end
+
+  get '/artists/:slug' do 
+    @artist = Artist.find_by_slug(params[:slug])
+    # @artist_songs = @artist.songs
+    # @artist_genres = @artist.genres
+
+    # erb :'/artists/show'
+  end
+
+end
