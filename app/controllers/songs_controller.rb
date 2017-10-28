ec1361e2476d0f0ddda4f62dcfa649203f712eef
johanesteves <jsesteves91@gmail.com>
2017-10-27 22:25:03 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,9 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     erb :'songs/index'
@@ -15,21 +20,42 @@ class SongsController < ApplicationController
     else
       @artist = Artist.find_by_name(params[:song][:artist])
     end
-    @new_song = Song.new(name: params[:song][:name], artist: @artist)
 
-    params[:song][:genre_ids].each do |genre_id|
-      @new_song.genres << Genre.find_by_id(genre_id)
-    end
-
+    @new_song = Song.new(name: params[:song][:name], artist: @artist, genre_ids: params[:song][:genre_ids])
     @new_song.save
+
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@new_song.slug}"
-
   end
-  get '/songs/:slug' do
 
+  get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
 
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @updated_song = Song.find_by_slug(params[:slug])
+
+    if !params[:song][:artist].empty?
+      if !Artist.find_by_name(params[:song][:artist])
+        artist = Artist.create(name: params[:song][:artist])
+      else
+        artist = Artist.find_by_name(params[:song][:artist])
+      end
+    else
+      artist = @updated_song.artist
+    end
+
+    @updated_song.update(name: params[:song][:name], artist: artist, genre_ids: params[:song][:genre_ids])
+
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@updated_song.slug}"
   end
 
 end
