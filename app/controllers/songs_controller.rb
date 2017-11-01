5534519befd06d1bae9d314e80ed773af86082a3
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-01 15:53:59 -0400

    finished editing a song and its artist. Need to complete genre updatiing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,5 @@
+# require 'rack-flash'
+
 class SongsController < ApplicationController
 
   get '/songs' do
@@ -7,19 +9,49 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
-    binding.pry
     erb :'./songs/new'
   end
 
   post '/songs/new' do
-    # redirect to "./songs/#{@song.slug}"
+    @artist = Artist.find_or_create_artist(params)
+    @song = Song.create(params["song"])
+    @song.artist = @artist
+    @song.genres << Genre.find(params["genre"]["id"])
+    # binding.pry
+    session[:notice] = "Successfully created song."
+    # binding.pry
+    @song.save
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
+    @notice = session[:notice]
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
     @genres = @song.genres
     # binding.pry
     erb :'./songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genres = @song.genres
+    # binding.pry
+    erb :'./songs/edit'
+  end
+
+  post '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    if !params["song"]["name"].empty?
+      binding.pry
+      @song.update(params["song"]["name"])
+    end
+    # binding.pry
+    @song.artist.update(name: Artist.find_or_create_artist(params).name)
+    binding.pry
+    session[:notice] = "Successfully updated song."
+    # binding.pry
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
 end
