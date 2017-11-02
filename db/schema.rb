91ac092948203efe915731b82acbd009320df975
GusVilla303 <gusvilla303@gmail.com>
2017-11-02 16:25:38 -0600

    Add tables for genres and song_genres, and associate songs, song_genres, and genres. Those specs are passing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102214812) do
+ActiveRecord::Schema.define(version: 20171102221106) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+    t.string "genres"
   end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
+    t.integer "genre_ids"
   end
 
 end
