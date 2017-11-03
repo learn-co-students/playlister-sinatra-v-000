01b390b209cec68b130883b6292e7c4346dfacb6
GusVilla303 <gusvilla303@gmail.com>
2017-11-02 21:56:50 -0600

    Add #slug and .find_by_slug to Song class. All model specs passing.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    name.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split("-").map(&:capitalize).join(' ')
+    self.all.detect { |a| a.name == name }
+  end
 end
