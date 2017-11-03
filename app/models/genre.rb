608438728a075b617e6c96ec6e7ad0ca8aaf51e2
GusVilla303 <gusvilla303@gmail.com>
2017-11-02 22:03:43 -0600

    Fix Genre associatation with songs through song_genres. Add #slug and .find_by_slug. Genre model specs are passing.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,14 @@
 class Genre < ActiveRecord::Base
-  has_many :songs
+  has_many :song_genres
+  has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    name.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split("-").map(&:capitalize).join(' ')
+    self.all.detect { |a| a.name == name }
+  end
 end
