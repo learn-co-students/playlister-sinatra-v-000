4001ffc16f73c6c0e56c1347d69e979fc553c19b
GusVilla303 <gusvilla303@gmail.com>
2017-11-02 21:53:52 -0600

    Add #slug and .find_by_slug in Artist. Respective specs are passing.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    name.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split("-").map(&:capitalize).join(' ')
+    self.all.detect { |a| a.name == name }
+  end
 end
