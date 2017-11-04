9cac24b749538eb6761c4dca162790f3f599ad06
GusVilla303 <gusvilla303@gmail.com>
2017-11-03 20:18:43 -0600

    Add ArtistsController and Artist view page. Clean up SongsController.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,16 +6,8 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
-    #binding.pry
     @song = Song.find_by_slug(params[:slug])
 
-    # I expect to use the find_by_slug class method
     erb :'songs/show'
   end
-  #
-  # get '/songs/:id' do
-  #   binding.pry
-  #   @song = Song.find(params[:id])
-  #   erb :'songs/show'
-  # end
 end
