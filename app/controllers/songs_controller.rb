1ce88c9cc551c604dbf433ac3b55e49c931ac206
GusVilla303 <gusvilla303@gmail.com>
2017-11-06 13:31:14 -0700

    Add functionality to create a new artist on '/songs/new' form. Spec is passing.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,27 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
 
     erb :'songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.new
+    @song.name = params[:song][:name]
+    if !params[:song][:artist_name].empty?
+      @song.artist = Artist.create(name: params[:song][:artist_name])
+    end
+    @song.genre_ids = params[:song][:genre_ids]
+    @song.save
+
+
+    redirect "songs/#{@song.slug}"
   end
 end
