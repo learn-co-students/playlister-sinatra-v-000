c2f4ef3c0a522a2eb8af25f1d6c25469bff05013
GusVilla303 <gusvilla303@gmail.com>
2017-11-06 21:01:02 -0700

    Add '/songs/:slug/edit' action, edit.erb view, and patch action.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,17 +18,34 @@ class SongsController < ApplicationController
     erb :'songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    #binding.pry
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :"/songs/edit"
+  end
+
   post '/songs' do
     @song = Song.new
     @song.name = params[:song][:name]
     if !params[:song][:artist_name].empty?
-      @song.artist = Artist.create(name: params[:song][:artist_name])
+      @song.artist = Artist.find_or_create_by(name: params[:song][:artist_name])
     end
     @song.genre_ids = params[:genres]
     @song.save
 
     flash[:message] = "Successfully created song."
-    #binding.pry
     redirect "/songs/#{@song.slug}"
+  end
+
+  patch '/songs' do
+    @song = Song.find_or_create_by(name: params[:song][:name])
+    if !params[:song][:artist_name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:song][:artist_name])
+    end
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect redirect "/songs/#{@song.slug}"
   end
 end
