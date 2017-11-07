0a9bce0e89e92929e0ab5ff7c79b280bf90b9647
Sam Marshall <samuel.r.marshall@gmail.com>
2017-11-06 19:15:26 -0500

    finished ruby classes

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, :through => :song_genres
+
+  def slug
+    self.name.downcase.split(' ').join('-')
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split('-').join(' ').titleize
+    self.find_by(name: name)
+  end
+
 end
