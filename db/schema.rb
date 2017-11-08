bd8b71fcb892d386fff81898f53a0eb7f4be24c5
Denis Bondin <dsbondin@gmail.com>
2017-11-08 01:15:33 +0000

    2

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108010248) do
+ActiveRecord::Schema.define(version: 20171108010741) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
