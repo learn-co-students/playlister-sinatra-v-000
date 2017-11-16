5234d117c380bb6a7dfcf95c7e373b54bb0e8146
alpbourne <alpbourne@gmail.com>
2017-11-16 18:19:26 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb:'/songs/show'
+    erb :'/songs/show'
   end
 
   post '/songs' do
@@ -24,6 +24,21 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(:name params[:artist][:name])
+    @song.save
+
+    flash[:mesage] = "Successfully updated song."
     redirect to "/songs/#{@song.slug}"
   end
 
