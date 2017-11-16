c17f598a7a9ff15d7339791df587a263de7f76b8
alpbourne <alpbourne@gmail.com>
2017-11-16 16:45:39 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveRecord::Base
   has_many :song_genres
-  has many :songs, :though => :song_genres
-  has many :artists, :through => :songs
+  has_many :songs, :through => :song_genres
+  has_many :artists, :through => :songs
 
 end
