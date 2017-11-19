a01e830702a338df56e575971ebaba27e1e52535
Elvin <Dimix181@Gmail.com>
2017-11-19 11:24:17 -0500

    song model spec 02 passing

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,13 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
 
+  def self.find_by_slug(slug)
+  normalize =  slug.gsub("-"," ").split.select{|x|x.capitalize!}.join(" ")
+  Song.all.find_by({name: normalize})
+  end
+
+  def slug
+    self.name.gsub(/\W/,"-").downcase
+  end
+
 end
