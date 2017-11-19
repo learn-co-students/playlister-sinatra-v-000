e2f7d795860ea50bef8f4e42a57212480d43b165
Elvin <Dimix181@Gmail.com>
2017-11-19 13:49:18 -0500

    genre model spec 03 passing

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,16 @@
 
 class Genre < ActiveRecord::Base
   has_many :song_genres
-  has_many :artists, through: :songs
   has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+  def self.find_by_slug(slug)
+  normalize =  slug.gsub("-"," ").split.select{|x|x.capitalize!}.join(" ")
+  Genre.all.find_by({name: normalize})
+  end
+
+  def slug
+    self.name.gsub(/\W/,"-").downcase
+  end
+
 end
