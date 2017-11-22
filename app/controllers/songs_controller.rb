4f74602fbd505a9c8cde15a72a61a7579598d741
Justin Tooley <jstooley823@gmail.com>
2017-11-22 00:49:25 +0000

    started edit song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,14 +9,30 @@ class SongController < Sinatra::Base
   end
 
   post '/songs' do
-  
-    genre = Genre.find_by(name: params['genre']['name'])
-    song_genre = SongGenre.find_or_create_by(genre_id: genre.id)
-    artist = Artist.find_or_create_by(name: params['artist']['name'])
-    song = Song.find_or_create_by(name: params["Name"], artist_id: artist.id)
-    song_genre.song_id = song.id
-    binding.pry
-    erb :'songs/index'
+    @genre = Genre.find_by(name: params['genre']['name'])
+    @song_genre = SongGenre.find_or_create_by(genre_id: @genre.id)
+    @artist = Artist.find_or_create_by(name: params['artist_name'])
+    @song = Song.find_or_create_by(name: params["Name"], artist_id: @artist.id)
+    @song_genre.song_id = @song.id
+    @song.save
+    @artist.save
+    @song_genre.save
+    redirect to "songs/#{@song.slug}"
+  end
+
+  post '/songs/:slug' do
+
+    @song = Song.find_by_slug(params[:slug])
+    @artist = Artist.find_or_create_by(name: params['artist'])
+    @genre = Genre.find_by(id: params['genre'])
+    @song.artist = @artist
+    @song_genre = SongGenre.find_by(song_id: @song.id)
+    @song_genre.genre = @genre
+
+    @song_genre.save
+    @song.save
+    @artist.save
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/new' do
@@ -26,6 +42,11 @@ class SongController < Sinatra::Base
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
   end
 
 end
