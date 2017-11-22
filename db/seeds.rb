308614e303ca033d0a4563843c2d48ce7d7db2a0
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-22 15:32:41 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,17 @@
 # Add seed data here. Seed your database with `rake db:seed`
+require_relative '../config/environment.rb'
 
+song_array = LibraryParser.new.files
+
+
+i=0
+  while i<song_array.count
+    x=LibraryParser.new.parse_filename(song_array[i])
+    LibraryParser.new.build_objects(x[0],x[1],x[2])
+  i+=1
+  end
+
+#LibraryParser.new.parse_filename(array[i])
+#this gives me the ith element as a three bonger array - artist, song, genre
+
+# next up is build_obects, but I need to include the models in the require_relative
