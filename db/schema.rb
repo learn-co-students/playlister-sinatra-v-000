c1405150b0436bd19422a657595afacb4fdd1e5c
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-23 19:00:05 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20171123171646) do
     t.string "name"
   end
 
-  create_table "song_genre", force: :cascade do |t|
+  create_table "song_genres", force: :cascade do |t|
     t.integer "song_id"
     t.integer "genre_id"
   end
