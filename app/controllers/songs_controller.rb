ee744e05c79876d58310175d87f8a4c21b547756
Adam Tropp <adt6261@gmail.com>
2017-11-29 18:33:13 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,6 +11,30 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+    @artists = Artist.all
     erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    if !params["artist"]["name"].empty?
+      @song.artist = Artist.create(name: params["artist"]["name"])
+    end
+    @song.save
+    redirect to "owners/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(:slug)
+    erb :'/songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(:slug)
+    @song.update(params["song"])
+    if !params["artist"]["name"].empty?
+      @song.artist = Artist.create(name: params["artist"]["name"])
+    end
+    redirect to "song/#{@song.slug}"
   end
 end
