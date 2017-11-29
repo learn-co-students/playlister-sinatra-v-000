cead277466b61983e84a0e76c3e2e636c48c758e
Adam Tropp <adt6261@gmail.com>
2017-11-29 07:09:15 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,12 @@
-class ArtistsController < Sinatra::Base
+class ArtistsController < ApplicationController
 
   get '/artists' do
     @artists = Artist.all
     erb :'/artists/index'
+  end
+
+  get "/artists/:slug" do
+    @artist = Artist.find_by_slug(:slug)
+    erb :'/artists/show'
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,16 @@
 class SongsController < ApplicationController
-  set :views, Proc.new { File.join(root, "../views/songs/") }
 
   get '/songs' do
     @Songs = Song.all
-    erb :index
+    erb :'/songs/index'
+  end
+
+  get "/songs/:slug" do
+    @song = Song.find_by_slug(:slug)
+    erb :'/songs/show'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 end
