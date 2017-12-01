1037b40a324b12351a5852df9391ad904fb6c460
miriam <miriam5000@gmail.com>
2017-12-01 10:29:59 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,11 +1,13 @@
-class Genre <ActiveRecord::Base
-	extend Slugifiable::ClassMethods
+class Genre < ActiveRecord::Base
+	
 	include Slugifiable::InstanceMethods
+	extend Slugifiable::ClassMethods
 	
 	has_many :song_genres
 	has_many :songs, :through => :song_genres
-	has_many :artists, :through => :song_genres
+	has_many :artists, :through => :songs
 	
-	
-
+	def self.find_by_slug(slug)
+	    Genre.all.find{|genre| genre.slug == slug}
+	end
 end 
