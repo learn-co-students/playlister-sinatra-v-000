7bf156ab622af634ce87ab3e7abb42f1e68b3925
Adam Fudge <coolfudge@gmail.com>
2017-12-02 23:45:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,9 +13,7 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
-  post '/songs' do
-# puts "params= #{params}"
-# puts "params[:genre_ids]= #{params[:genre_ids]}"
+  post '/songs' do #i dont think this can handle blank fields properly
     artist = Artist.find_or_create_by(name: params[:artist][:name])
     artist.save
 
@@ -36,6 +34,32 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug/edit' do
+    @genres = Genre.all
+    @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
+  end
+
+  post '/songs/:slug' do
+# puts "params= #{params}"
+
+    @song = Song.find_by_slug(params[:slug])
+    if params[:song][:name] != ""
+      @song.name = (params[:name])
+    end
+
+    if params[:artist][:name] != ""
+      artist = Artist.find_or_create_by(name: params[:artist][:name])
+      artist.save
+      @song.artist = artist
+    end
+
+    if params.key?(:genre_ids)
+      @song.genres = params[:genre_ids].collect { |id| Genre.find(id)}
+    end
+
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to "songs/#{@song.slug}"
   end
 end
