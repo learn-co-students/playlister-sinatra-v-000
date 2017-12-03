649a066bd1a06900f5171fd2cb875376a9a98e10
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-03 16:56:22 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,10 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+
   get '/' do
     erb :index
   end
 
-  patch "/songs/:slug" do
-
-    @song = Song.find_by_slug(params[:slug])
-      @song.update(params[:song])
-      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-      @song.save
-
-      flash[:message] = "Successfully updated song."
-      redirect("/songs/#{@song.slug}")
-  end
+  
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,6 +39,15 @@ class SongsController < ApplicationController
    erb :'songs/edit'
  end
 
- 
+ post "/songs/:slug" do
+
+   @song = Song.find_by_slug(params[:slug])
+     @song.update(params[:song])
+     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+     @song.save
+
+     flash[:message] = "Successfully updated song."
+     redirect("/songs/#{@song.slug}")
+ end
 
 end
