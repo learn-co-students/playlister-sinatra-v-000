d2139cd4c5ddadd4b2506fd7e45fefb6efb3ef8d
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-03 15:48:10 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
    @song.save
    flash[:notice] = "Successfully created song."
 
-   redirect to "/songs/#{@song.slug}"
+   redirect("/songs/#{@song.slug}")
  end
 
  get '/songs/:slug/edit' do
