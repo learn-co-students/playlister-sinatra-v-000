4d5a6fd57e9b348d7db9c8c7f023580411221b4b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-04 17:25:47 -0500

    all tests passing. No flash working - still weird.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,7 @@
 class SongsController < ApplicationController
 
+  enable :sessions
+
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
@@ -8,10 +10,13 @@ class SongsController < ApplicationController
   post '/songs' do
     @song = Song.create(params[:song])
     if !params[:artist_name].empty?
-      @song.artist = Artist.create(name: params[:artist_name])
+      @song.artist = Artist.find_or_create_by(name: params[:artist_name])
     end
-
     @song.save
+
+    # Taking this route b/c flash is not working
+    session[:created_song] = "Successfully created song."
+
     redirect to "/songs/#{@song.slug}"
   end
 
@@ -21,12 +26,40 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
-  get '/songs/:slug' do
 
+  get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :song_show
 
     # erb :'songs/show' -- This just will not work for some reason and keeps throwing 500 errors on this route.
   end
+
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/edit"
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    if !params[:artist_name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    end
+    @song.save
+
+    # Taking this route b/c flash is not working
+    session[:updated_song] = "Successfully updated song."
+    redirect to "songs/#{@song.slug}"
+  end
+
+#TO FIX:
+  # patch '/posts/:id' do #edit action
+  #   @post = Post.find_by_id(params[:id])
+  #   @post.title = params[:title]
+  #   @post.content = params[:content]
+  #   @post.save
+  #   redirect to "/posts/#{@post.id}"
+  # end
 
 end
