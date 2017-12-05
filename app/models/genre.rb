f03a4c75ec6d6e684931b81db6dee20b6180bd20
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-12-05 14:07:09 -0500

    Artits, Song, Genre & Song_genre tests completed

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,17 @@
 class Genre <ActiveRecord::Base
   has_many :song_genres
   has_many :songs, :through => :song_genres
-  has_many :artists, :through => :Songs
+  has_many :artists, :through => :songs
 
+
+  def slug
+  name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    Genre.all.find do |genre|
+      genre.slug == slug
+    end
+  end
 
 end
