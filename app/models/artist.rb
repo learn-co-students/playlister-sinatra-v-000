8f934bf7df13365b20d9a24097e25a768a85d710
Emma Feng <ff18qd@gmail.com>
2017-12-06 03:29:36 +0000

    Done.

---
+++ app/models/artist.rb
@@ -0,0 +1,9 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+@artist = Artist.create(:name => "Taylor Swift")
+blank_space =  Song.create(:name => "Blank Space", :artist => @artist)
+pop = Genre.create(:name => "Pop")
