8ba56f9344620c03cc2f4eb145b7ae3c06eed84a
Emma Feng <ff18qd@gmail.com>
2017-12-08 00:47:41 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,17 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    name = self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slugname)
+    # binding.pry
+    name1 = slugname.split("-").collect do |item|
+      size = item.size-1
+      item[0].upcase+item[1,size]
+    end
+    Artist.find_by_name(name1.join(" "))
+  end
 end
