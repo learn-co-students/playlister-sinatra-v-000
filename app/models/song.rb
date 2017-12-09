00e91ed5e9a51e772b33b8c3db36e327dd554151
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-12-09 18:28:26 -0500

    7 tests left

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   has_many :genres, :through => :song_genres
 
   def slug
-    self.name.downcase.gsub(' ', '-')
+    self.name.to_s.downcase.gsub(' ', '-')
   end
 
   def self.find_by_slug(slug)
