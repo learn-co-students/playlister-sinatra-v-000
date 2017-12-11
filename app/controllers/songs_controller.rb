0c41b861b0e0c66328afba000dfb8344bd7163c0
Jason Vance <jbvance@gmail.com>
2017-12-11 21:37:14 +0000

    update views and add code to seed db

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,16 @@ class SongsController < ApplicationController
         erb :'/songs/index'
     end
     
+    get '/songs/new' do
+       erb :'/songs/new' 
+    end
+    
     get '/songs/:slug' do
        @song = Song.find_by_slug(params[:slug])
        @genres = @song.genres
        erb :'/songs/show'
     end
+    
+    
     
 end
