1aed7f4aab3a5c375c0040bd51180f5924a7583c
Jason Vance <jbvance@gmail.com>
2017-12-12 01:59:18 +0000

    complete add song functionality

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,5 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
     
     get '/songs' do
@@ -13,6 +15,21 @@ class SongsController < ApplicationController
        @song = Song.find_by_slug(params[:slug])
        @genres = @song.genres
        erb :'/songs/show'
+    end
+    
+    post '/songs' do
+       @artist_name = params["Artist Name"]
+       @name = params["Name"]
+       @genres = params["genres"]
+       @song = Song.create(name: @name)
+       
+       @song.artist = Artist.find_or_create_by(name: @artist_name)
+       @genres.each do |genre|
+          song_genre = SongGenre.create(song_id: @song.id, genre_id: genre.to_i)
+       end
+       @song.save
+       flash[:message] = "Successfully created song."
+       redirect to "/songs/#{@song.slug}"
     end
     
     
