ae51f98d670c35beef292d20b2bfbce69510f45a
americanmouths <shiverr@gmail.com>
2017-12-11 21:01:48 -0800

    Finish add song page and controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,34 @@
+require 'rack/flash'
+
 class SongController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
-
+    LibraryParser.parse
     erb :'songs/index'
   end
+
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params["slug"])
+
+    erb :'songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(:name => params[:Name])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to "songs/#{@song.slug}"
+end
 
 end
