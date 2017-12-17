bd6292f2c17a14968bd71c34a66180e8ee740ff3
Nicholas Halloran <nmhalloran@hotmail.com>
2017-12-17 15:47:54 +0000

    Done.

--- db/migrate/20171214214711_create_song.rb
+++ db/migrate/20171214214711_create_song.rb
@@ -2,7 +2,7 @@ class CreateSong < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :name
-      t.index :artist_id
+      t.integer :artist_id
     end
   end
 end
