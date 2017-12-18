a03e336d1fc88448857b27bf53b5924b053d12fe
Alexander Friedland <friedlandaj@gmail.com>
2017-12-18 19:18:31 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,9 +1,19 @@
 module Slugifiable
 
-  def slug
+#instance method - @artist.slug
+#include Slugifiable::Instance
+  module Instance
+    def slug
+      self.name.to_s.gsub(" ", "-").downcase
+    end
   end
 
-  def find_by_slug
+#class method - 'undefined method fbs for Artist'
+#extend Slugifiable::Class
+  module Class
+    def find_by_slug(slug)
+      self.all.find {|x| x == slug}
+    end
   end
 
 end
