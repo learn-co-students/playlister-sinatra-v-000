075e5d13524a250ba55073bf2a715681ce298237
Mel Rudick <melissarudick@MacBook-Air.local>
2017-12-19 14:19:58 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,14 @@ class SongsController < ApplicationController
 
   get "/songs/:slug" do
     @song = Song.find_by_slug(params["slug"])
-    
     erb :'songs/show'
+  end
+
+  post "/songs" do
+    @song = Song.create(name: params[:name])
+    @song.artist = Artist.find_or_create_by(name: params[:artist])
+    @song.genre = Genre.find(name: params[:genre])
+    flash[:message] = "Successfully updated"
+    redirect "/songs/#{@song.slug}"
   end
 end
--- lib/concerns/slugify.rb
+++ lib/concerns/slugify.rb
@@ -1,16 +1,20 @@
 module Slugify
   module ClassMethods
     def find_by_slug(slug)
-      name = slug.split("-").each {|word|
-      word.capitalize!}.join(" ").strip
-      self.find_by_name(name)
+      # name = slug.split("-").each {|word|
+      # word.capitalize!}.join(" ").strip
+      # self.find_by_name(name)
+      # binding.pry
+      self.all.find {|e| e.slug == slug}
     end
   end
 
   module InstanceMethods
     def slug
-      self.name.split(" ").map {|word|
-      word.downcase}.join("-")
+      slugged = self.name
+      slugged.downcase.gsub(" ","-")
+      # self.name.split(" ").map {|word|
+      # word.downcase}.join("-")
     end
   end
 end
