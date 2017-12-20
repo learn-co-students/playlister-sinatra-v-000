658d22a511178481eff26b4d36ed34808121ba9f
Nicholas Halloran <nmhalloran@hotmail.com>
2017-12-20 19:10:52 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
 # Add seed data here. Seed your database with `rake db:seed`
-biggie = Artist.create(name: "Biggie")
-dead_wrong = Song.create(name: "Dead Wrong")
-rap = Genre.create(name: "Rap")
-
-biggie.songs << dead_wrong
-dead_wrong.genres << rap
+LibraryParser.parse
