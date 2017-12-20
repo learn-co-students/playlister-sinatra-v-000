1b04bd5936553677eb3eeaf3d3414e0107d90df2
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-20 07:18:19 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,9 @@
 class Artist < ActiveRecord::Base
-  include Slug::InstanceMethods
-  extend Slug::ClassMethods
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
 
   has_many :songs
   has_many :genres, through: :songs
   has_many :song_genres
 
-  def slug
-    self.name.gsub(" ", "-").downcase
-  end
-
-  def self.find_by_slug(slug)
-    self.all.detect{|item| item.slug == slug}
-  end
 end
