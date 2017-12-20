8ca7b72c83e992afa27b476179d37baeab07ddbe
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-20 16:23:12 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,8 +4,20 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
+
+
 end
