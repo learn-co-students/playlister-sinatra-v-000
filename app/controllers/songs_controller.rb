c86dff16dd17e0b32794171f00b0521b2e820520
Alexander Friedland <friedlandaj@gmail.com>
2017-12-23 20:44:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,10 @@
 class SongsController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_Songs_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
-    erb :index
+  get '/songs' do
+    erb :'/songs/index'
+  end
+
+  get '/songs' do
+
   end
 end
