3e6476d304316fec399b695a18350c362c86c450
Alexander Friedland <friedlandaj@gmail.com>
2017-12-23 21:21:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < Sinatra::Base
 
   register Sinatra::ActiveRecordExtension
-  
+
+  enable :sessons
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,7 @@ class SongsController < ApplicationController
   use Rack::Flash
 
   get '/songs' do
+    @songs = Song.all
     erb :'/songs/index'
   end
 
