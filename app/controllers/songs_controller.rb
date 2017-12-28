e081a08f6abecf88f698a9bc7cdb370d0d28b663
Alexander Friedland <friedlandaj@gmail.com>
2017-12-28 03:29:05 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,7 +44,7 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully updated song."
-    redirect('/songs/#{@song.slug}')
+    redirect("/songs/#{@song.slug}")
   end
 
 
