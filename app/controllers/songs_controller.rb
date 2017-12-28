db6c1e5d0ad0e486d62c40923b8220c32162b405
Alexander Friedland <friedlandaj@gmail.com>
2017-12-28 01:04:47 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 require 'rack-flash'
+require 'pry'
 
 class SongsController < ApplicationController
 
@@ -11,6 +12,12 @@ class SongsController < ApplicationController
 
   get '/songs/new' do
     erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/show'
   end
 
   post '/songs' do
