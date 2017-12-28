3d4512069e3fd1cc3b7502362448a3f9be3a23af
Alexander Friedland <friedlandaj@gmail.com>
2017-12-28 01:49:18 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -13,9 +13,15 @@ module Slugifiable
 #class method - 'undefined method fbs for Artist'
 #extend Slugifiable::Class
   module Class
+=begin
     def find_by_slug(slug)
       slug = slug.split("-").map(&:capitalize)*' '
       self.all.find_by(name: slug)
+    end
+=end
+
+    def find_by_slug(slug)
+      self.all.find{ |instance| instance.slug == slug }
     end
   end
 end
