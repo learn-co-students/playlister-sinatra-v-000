e4eecf8a43c5487a21c485fe2a0b8a9eaef5b2c7
Veronica Miller <vmcmiller@gmail.com>
2018-01-01 07:01:32 +0000

    songs edit and new paths

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,6 +36,11 @@ class SongsController < ApplicationController
   end
 
   post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params["song"]["name"])
+    @song.artist = Artist.find_or_create_by(name: params["song"]["artist"])
+
+    @song.save
     flash[:message] = "Successfully updated song."
     redirect to "songs/#{@song.slug}"
   end
