7a1d19bd9d54d5a7966c9260265e7ae7013291d6
Hunter Kleindl <hkleindl@gmail.com>
2018-01-01 16:49:26 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,14 @@
+class ArtistsController < ApplicationController
+
+  get '/artists' do
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+
+    # binding.pry
+    erb :'/artists/show'
+  end
+
+end
