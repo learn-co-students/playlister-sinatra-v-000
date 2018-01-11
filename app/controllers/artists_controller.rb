9e9a940b51c647ed1d9770b561e249607bdb182e
Jacqueline Lee <anginblu@gmail.com>
2018-01-11 03:03:35 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,12 +2,17 @@ class ArtistsController < ApplicationController
 
   get '/artists' do
     @artists = Artist.all
-    erb :'artists/index'
+    erb :'/artists/index'
   end
 
   get '/artists/:id' do
-    erb :'artists/show'
+    erb :'/artists/show'
 
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
   end
 
 
