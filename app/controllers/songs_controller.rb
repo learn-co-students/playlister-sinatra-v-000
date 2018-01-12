8436b61acfcfdfd31d7690badc08025f014f410d
Lara Sanders <larasanders@gmail.com>
2018-01-12 01:04:18 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,13 @@
+
 class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
   end
 
   get '/songs/:slug' do
@@ -12,12 +17,32 @@ end
 
 post '/songs' do
   @song = Song.create(:name => params["Name"])
-  @song.artist_ids = params[:artists]
-  # @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+  if !params["song"]["artist_name"].empty?
+  @song.artists << Artist.find_or_create_by(:name => params["Artist Name"])
+end
   @song.genre_ids = params[:genres]
   @song_genres = song.genres
   @song.save
-  erb :'/songs/show'
+  flash[:message] = "Successfully created song."
+redirect to("/songs/#{@song.slug}")
 end
 
+get 'songs/:slug/edit' do
+    @songs = Song.find(params[:id])
+    erb :'/songs/edit'
+  end
+
+  get '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :'/songs/show'
+  end
+
+  post '/songs/:id' do
+    @song = Song.find(params[:id])
+    @song.update(params["song"])
+    if !params["song"]["name"].empty?
+      @song.artists << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
+end
 end
