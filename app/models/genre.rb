09d3ba07b9b80450c5291c00e4132285a788c8ec
Phoebe Lau <phoebelau7@gmail.com>
2018-01-16 18:15:31 +0000

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,7 @@
+class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+end
