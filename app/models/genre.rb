9a86d660588531baf83f313ee0d6901039be7d97
Hannah Carnes <hecarnes@gmail.com>
2018-01-15 21:33:19 -0500

    models done

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,13 @@
 class Genre < ActiveRecord::Base
-  has_many :artists
   has_many :song_genres
   has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.where("LOWER(name) = ?", slug.gsub("-", " ")).first
+  end
 end
