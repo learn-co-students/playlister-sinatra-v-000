ed6c1fc1c5840be4e9c5e51954daa46594518277
Jess Mitchell <jessicamitch42@gmail.com>
2018-01-18 19:37:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,10 @@
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    # @song = Song.create(name: "Person with a Face")
-    # @genre = Genre.create(name: "New Age Garbage")
-    # @artist = Artist.create(name: "That One with the Guitar")
-    #
-    # @song.song_genres.create(genre: @genre)
-    # @song.artist = @artist
-    #
-    # @song.save
     erb :index
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     # This should present the user with a list of all songs in the library.
@@ -9,7 +13,30 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+    @genres = Genre.all
     erb :'songs/new'
+  end
+
+  post '/songs' do
+    # binding.pry
+    s = Song.create(name: params["song_name"])
+    a = Artist.find_or_create_by(name: params["artist_name"])
+    s.artist_id = a.id
+    s.save
+
+    params[:genres].each do |g|
+      sg = SongGenre.create(song_id: s.id, genre_id: g.to_i)
+    end
+
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{s.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @artist = @song.artist
+    @genres = Genre.all
+    erb :'/songs/edit'
   end
 
   get '/songs/:slug' do
@@ -23,6 +50,19 @@ class SongsController < ApplicationController
 
     # binding.pry
     erb :'songs/show'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @artist = Artist.find_by_id(@song.artist_id)
+
+    if params[:artist][:name] != @artist.name
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+      @song.save
+    end
+
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
   end
 
 
