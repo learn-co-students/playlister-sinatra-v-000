dce7cf95274d1a9604531eb98e971005dbd1fa0b
Terry-Thompson <terry.lyn.thompson@gmail.com>
2018-01-18 14:39:53 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,20 @@
+require 'pry'
 class Artist < ActiveRecord::Base
+		has_many :genres, through: :songs
 		has_many :songs
-		has_many :genres
+
+
+	def slug
+		self.name.downcase.gsub(" ","-")
+	end
+
+	
+	def self.find_by_slug(slug)
+		name = slug.split("-").collect {
+		|name| name.capitalize}.join(" ")
+		
+		Artist.find_by_name(name)	
+	end
+
+
 end 
