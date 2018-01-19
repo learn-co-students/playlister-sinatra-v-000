0abdff817cc4c5200f9c1d11c32fd2bcbe4b11c1
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-19 23:04:13 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,9 @@
+require 'rack-flash'
+
 class SongsController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
+  use Rack::Flash
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
   
@@ -27,12 +31,41 @@ class SongsController < Sinatra::Base
       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     end
     
-    if !params[:genres].empty?
-      params[:genres].each{ |genre| @song.genres << Genre.find(genre) }
+    if !params[:genre][:name].empty?
+      @song.genres << Genre.find_or_create_by(params[:genre])
     end
 
     @song.save
+    
+    flash[:message] = "Successfully created song."
 
+    redirect "/songs/#{@song.slug}"
+  end
+  
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @artists = Artist.all.sort_by{ |artist| artist.name }
+    @genres = Genre.all.sort_by{ |genre| genre.name }
+    
+    erb :'/songs/edit'
+  end
+  
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    
+    if !params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    end
+    
+    if !params[:genre][:name].empty?
+      @song.genres << Genre.find_or_create_by(params[:genre])
+    end
+    
+    @song.save
+    
+    flash[:message] = "Successfully updated song."
+    
     redirect "/songs/#{@song.slug}"
   end
 end
