17bd4c71882393e7143836b482496cc02783f569
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-19 03:02:02 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,15 @@
+class SongsController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+  
+  get '/songs' do
+    @songs = Song.all.sort_by{ |song| song.name }
+    erb :'/songs/index'
+  end
+  
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+end
