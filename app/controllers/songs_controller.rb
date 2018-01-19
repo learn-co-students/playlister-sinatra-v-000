118ca5078941731f510be0682db6270d7dfce9c2
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-19 04:35:39 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,8 +8,31 @@ class SongsController < Sinatra::Base
     erb :'/songs/index'
   end
   
+  get '/songs/new' do
+    @artists = Artist.all.sort_by{ |artist| artist.name }
+    @genres = Genre.all.sort_by{ |genre| genre.name }
+    
+    erb :'/songs/new'
+  end
+  
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+  
+  post '/songs' do
+    @song = Song.create(params[:song])
+    
+    if !params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    end
+    
+    if !params[:genres].empty?
+      params[:genres].each{ |genre| @song.genres << Genre.find(genre) }
+    end
+
+    @song.save
+
+    redirect "/songs/#{@song.slug}"
   end
 end
