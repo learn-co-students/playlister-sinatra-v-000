d20ef2cf3e23c8ea9cd0a8a8c1b3d94ccd89a62d
Terry-Thompson <terry.lyn.thompson@gmail.com>
2018-01-21 14:20:02 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,5 +9,16 @@ class SongsController < ApplicationController
 		erb :'/songs/new'
 	end
 
+  post '/songs' do 
+		@song = Song.find_or_create_by(name: params[:song][:name])
+		@artist = Artist.find_or_create_by(name: params[:artist][:name])
+	end
+
+	get '/songs/:slug' do 
+		slug = params[:slug]
+		@song = Song.find_by_slug(slug)
+		erb :"/songs/show"
+	end
+
 
 end
