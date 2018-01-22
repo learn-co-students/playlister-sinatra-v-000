8b5b192368abfd17d48b117c2dced8994be3df56
Sherry El Garhy <sherryptk@gmail.com>
2018-01-22 23:58:00 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,9 @@ class Song < ActiveRecord::Base
   has_many :Song
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+  slug = @name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  slug
+  end
 end
