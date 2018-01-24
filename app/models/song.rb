881c88e5162e000a5c85214030322b9b14fd210c
Sherry El Garhy <sherryptk@gmail.com>
2018-01-24 00:35:45 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,15 @@ class Song < ActiveRecord::Base
   has_many :genres, through: :song_genres
 
   def slug
-  slug = @name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  slug = name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
   slug
+  end
+
+  def find_by_slug(slug)
+    @@all.each do |song|
+      if song.name.slug == slug
+        return song
+      end
+    end
   end
 end
