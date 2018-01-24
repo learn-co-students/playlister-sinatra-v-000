2f523d5c09e4160d8ca58f3c9cd29f0c26a3affe
Sherry El Garhy <sherryptk@gmail.com>
2018-01-24 00:50:55 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,17 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres
+
+  def slug
+  slug = name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  slug
+  end
+
+  def find_by_slug(slug)
+    @@all.each do |song|
+      if artist.name.slug == slug
+        return song
+      end
+    end
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,17 @@ class Genre < ActiveRecord::Base
   has_many :Song
   has_many :song_genres
   has_many :songs, through: :song_genres
+
+  def slug
+  slug = name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  slug
+  end
+
+  def find_by_slug(slug)
+    @@all.each do |song|
+      if song.name.slug == slug
+        return song
+      end
+    end
+  end
 end
