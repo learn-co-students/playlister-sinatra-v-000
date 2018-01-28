da8d680bf786473ccca016bb0cc6bf51a083faf2
Sherry El Garhy <sherryptk@gmail.com>
2018-01-28 15:12:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  use Rack::Flash
-  use Rack::MethodOverride
   
   get '/' do
     erb :index
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,26 +2,7 @@ require 'sinatra/base'
 require 'rack-flash'
 
 class SongsController < ApplicationController
-
-  post '/songs/:slug' do
-    binding.pry
-    @song = Song.find_by_slug(params[:slug])
-    slug = @song.slug
-
-    new_artist = Artist.find_or_create_by(name: params[:artist_name])
-    @song.artist = new_artist
-
-    updated_genres = []
-
-    params["genres"].each do |genre_id|
-      genre = Genre.find(genre_id.to_i)
-       updated_genres << genre
-    end
-
-    @song.genres = updated_genres
-    flash[:message] = "Successfully updated song."
-    redirect to "/songs/#{slug}"
-  end
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -53,6 +34,27 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @genres = Genre.all
     erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    slug = @song.slug
+
+    @song.update(params[:song])
+
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+
+    updated_genres = []
+    params["genres"].each do |genre_id|
+      genre = Genre.find(genre_id.to_i)
+       updated_genres << genre
+    end
+
+    @song.genres = updated_genres
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{slug}"
   end
 
   get '/songs/:slug' do
