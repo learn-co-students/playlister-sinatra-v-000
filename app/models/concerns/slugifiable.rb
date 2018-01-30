60ffcd3cb72804861b98ec66b68fb0483625ec8c
brittmmendez <33286529+brittmmendez@users.noreply.github.com>
2018-01-30 10:12:44 -0500

    Update slugifiable.rb

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,7 +7,7 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      Artist.all.find{|artist| artist.slug == slug}
+      self.all.find{|artist| artist.slug == slug}
     end
   end
 
