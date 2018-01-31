447613577b17ba096a81bb5b0183684cabdb5659
Brad Hambrice <hambrice@uab.edu>
2018-01-31 05:18:43 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,61 @@
+require 'pry'
+require 'rack-flash'
 class SongsController < ApplicationController
+use Rack::Flash
 
   get '/songs' do
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    #binding.pry
+    @song = Song.create(name: params["Name"])
+   params["genres"].each do |genre_id|
+      @song.genres << Genre.find(genre_id)
+    end
+    if Artist.all.any? {|a| a.name == params["Artist Name"]}
+      @song.artist = Artist.find_by(name: params["Artist Name"])
+    else
+      @song.artist = Artist.create(name: params["Artist Name"])
+    end
+    @song.save
+
+    #binding.pry
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    #binding.pry
     erb :'/songs/show'
   end
 
-  get '/songs/new'
-    erb :'/songs/new'
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    #@song.update =
+    erb :'songs/edit'
   end
-  
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params["Name"]) if !params["Name"].empty?
+    params["genres"].each do |genre_id|
+       @song.genres << Genre.find(genre_id)
+     end
+      @song.genres.uniq!
+    if Artist.all.any? {|a| a.name == params["Artist Name"]}
+      @song.artist = Artist.find_by(name: params["Artist Name"])
+    else
+      @song.artist = Artist.create(name: params["Artist Name"])
+    end
+    @song.save
+    binding.pry
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
 end
--- config.ru
+++ config.ru
@@ -7,6 +7,7 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+
 use Rack::MethodOverride
 use SongsController
 use ArtistsController
