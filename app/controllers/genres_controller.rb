c2d8efb48ba3b2d25384f05119534c045260a3af
lizbur10 <liz.burton147@gmail.com>
2018-01-30 19:23:41 -0500

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,7 +3,6 @@ class GenresController < ApplicationController
     get '/genres' do
         @genres = Genre.all
         erb :'/genres/index'
-
     end
 
     get '/genres/:slug' do
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,10 +20,7 @@ class SongsController < ApplicationController
     post '/songs' do
         @song = Song.create(:name => params["song"]["name"])
         @song.artist = find_or_create_artist(params)
-
-        params["song"]["genre_ids"].each do |genre_id|
-            @song.genres << Genre.find(genre_id)
-        end
+        find_or_add_genres(params)
         @song.save
         flash[:message] = "Successfully created song."
         redirect to "/songs/#{@song.slug}"
@@ -39,11 +36,7 @@ class SongsController < ApplicationController
         @song = Song.find_by_slug(params[:slug])
         @song.name = params["song"]["name"]
         @song.artist = find_or_create_artist(params)
-        params["song"]["genre_ids"].each do |genre_id|
-            if !@song.genres.include?(Genre.find(genre_id))
-                @song.genres << Genre.find(genre_id)
-            end
-        end
+        find_or_add_genres(params)
         @song.save
         flash[:message] = "Successfully updated song."
         redirect to "/songs/#{@song.slug}"
@@ -52,6 +45,15 @@ class SongsController < ApplicationController
     helpers do 
         def find_or_create_artist(params)
             Artist.find_by(:name => params["song"]["artist"]) || Artist.create(:name => params["song"]["artist"])
+        end
+
+        def find_or_add_genres(params)
+            params["song"]["genre_ids"].each do |genre_id|
+                if !@song.genres.include?(Genre.find(genre_id))
+                    @song.genres << Genre.find(genre_id)
+                end
+            end
+    
         end
     end
 
