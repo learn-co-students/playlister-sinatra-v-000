cbc06ff56c3ef8fd3a9487d63c61bf0533b6c4d5
PJ Frias <peejfrias@gmail.com>
2018-02-07 19:33:43 +0000

    pass song and genre test specs

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,7 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,7 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
   belongs_to :artist
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
