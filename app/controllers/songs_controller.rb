15a4b0212865996c941d4a4916bccfa7016f45ab
PJ Frias <peejfrias@gmail.com>
2018-02-08 19:14:18 +0000

    save

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,10 +8,38 @@ class SongsController < Sinatra::Base
     erb :"songs/index"
   end
 
+  get '/songs/new' do
+    erb :"songs/new"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
 
     erb :"songs/show"
+  end
+
+
+
+  post '/songs' do
+    @song = Song.new(:name => params["name"])
+    @artist = Artist.find_or_create_by(:name => params["artist_name"])
+    @song.artist = @artist
+    params["genre_ids"].each do |id|
+      @song.genres << Genre.find(id)
+    end
+    @song.save
+    flash[:message] = "Successfully created a song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :edit
+  end
+
+  post '/songs/:slug' do
+    "EDITED SONG COMIN AT YA"
   end
 
 end
