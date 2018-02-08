55923c67dd68910b3243bcee9c965c617ad3f9a0
PJ Frias <peejfrias@gmail.com>
2018-02-08 14:43:31 +0000

    complete file structure

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,11 @@
-class ApplicationController < Sinatra::Base
+class SongsController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
-    erb :index
+  get '/songs' do
+    @songs = Song.all
+    erb :"songs/index"
   end
 
 end
