65dd998b5eee7a4ce515c6ffb07d05b95a3dde37
churchwerks <theBoss@stevenlennis.com>
2018-02-09 19:13:52 -0600

    Song specs passing

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,17 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, :through => :song_genres
 
+  def slug
+    song = self.name
+    "#{song.gsub(/\W/, "-").squeeze("-")}".downcase
+  end
+
+  def self.find_by_slug(slug)
+    name_array = slug.split("-")
+    name_array.collect do |word|
+      word.capitalize!
+    end
+    slug = name_array.join("-")
+    @song = Song.find_by name: slug.sub("-", " ")
+  end
 end
