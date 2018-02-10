8388b058e8dd12aeff048785a07c512c664ea764
churchwerks <theBoss@stevenlennis.com>
2018-02-09 19:17:10 -0600

    Genre specs passing

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,18 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, :through => :song_genres
   has_many :artists, :through => :songs
+
+  def slug
+    genre = self.name
+    "#{genre.gsub(/\W/, "-").squeeze("-")}".downcase
+  end
+
+  def self.find_by_slug(slug)
+    name_array = slug.split("-")
+    name_array.collect do |word|
+      word.capitalize!
+    end
+    slug = name_array.join("-")
+    @genre = Genre.find_by name: slug.sub("-", " ")
+  end
 end
