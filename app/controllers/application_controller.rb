cd72b184c612744ce1d46734bf1d6a27fc5e22e9
churchwerks <theBoss@stevenlennis.com>
2018-02-09 19:57:07 -0600

    Basic View templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
 
   enable :sessions
   set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, 'app/views'
+  #set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < ApplicationController
 
   get '/artists' do
     @artists = Artist.all
+    binding.pry
     erb:'artists/index'
   end
 
