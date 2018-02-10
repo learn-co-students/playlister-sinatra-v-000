2ff9f80f0ced7c13274eff5d539e173b8b4889b1
churchwerks <theBoss@stevenlennis.com>
2018-02-09 19:08:06 -0600

    Artist Specs passing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,17 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
 
+  def slug
+    artist = self.name
+    "#{artist.gsub(/\W/, "-").squeeze("-")}".downcase
+  end
+
+  def self.find_by_slug(slug)
+    name_array = slug.split("-")
+    name_array.collect do |word|
+      word.capitalize!
+    end
+    slug = name_array.join("-")
+    @artist = Artist.find_by name: slug.sub("-", " ")
+  end
 end
