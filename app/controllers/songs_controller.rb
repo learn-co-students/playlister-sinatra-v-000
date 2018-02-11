e31d8a1fa161720f4caa1363690e85c5b6677658
github username <github email address>
2018-02-11 15:50:29 -0600

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,9 @@
+# require 'rack-flash'
+
+class SongsController < ApplicationController
+
+    get '/songs' do
+        @songs = Song.all
+    erb:'/songs/index'
+    end
+end
--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -4,7 +4,6 @@ class LibraryParser
   def files
     data_path = File.join(File.dirname(__FILE__), '..', 'db', 'data')
     Dir.entries(data_path)[2..-1]
-    binding.pry
   end
 
   def self.parse
@@ -34,10 +33,8 @@ class LibraryParser
     song = Song.create(name: song_name)
     genre = Genre.find_or_create_by(name: genre_name)
     artist = Artist.find_or_create_by(name: artist_name)
-
     song.song_genres.build(genre: genre)
     song.artist = artist
-    
     song.save
   end
 end
