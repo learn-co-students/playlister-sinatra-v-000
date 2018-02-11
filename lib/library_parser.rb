fbf089cd951a5d693252d443bfb9022a4ecff1ac
github username <github email address>
2018-02-11 14:40:05 -0600

    Done.

--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 class LibraryParser
   def files
     data_path = File.join(File.dirname(__FILE__), '..', 'db', 'data')
     Dir.entries(data_path)[2..-1]
+    binding.pry
   end
 
   def self.parse
