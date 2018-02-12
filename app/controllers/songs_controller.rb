77726bd4918f8c771f1c947b7e09153a20821009
churchwerks <theBoss@stevenlennis.com>
2018-02-12 09:13:04 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,15 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
-    erb:'songs/index'
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
@@ -17,15 +24,34 @@ class SongsController < ApplicationController
       redirect '/songs'
     end
   end
-end
 
-post '/signup' do
-  @user = User.new(username: params["username"], email: params["email"], password: params["password"])
-  if !!@user.username && !!@user.email && !!@user.password && @user.username != "" && @user.email != "" && @user.password != ""
-    @user.save
-    session[:user_id] = @user.id
-    redirect '/tweets'
-  else
-    redirect '/signup'
+
+
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully created song."
+
+    redirect("/songs/#{@song.slug}")
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
+  end
+
 end
