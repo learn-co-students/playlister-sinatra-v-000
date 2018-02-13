7ca512714913c459adad38736bf0cc499733e7c9
github username <github email address>
2018-02-13 14:13:38 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,6 +7,10 @@ class SongsController < ApplicationController
         erb:'/songs/index'
     end
 
+    get '/songs/new' do
+        erb:'/songs/new'
+    end
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb:'songs/show'
@@ -37,3 +41,4 @@ class SongsController < ApplicationController
       end
 end
 
+#WHY GENRE_IDS PLURAL? (SONG CONTROLLER AND SONG.SHOW)
