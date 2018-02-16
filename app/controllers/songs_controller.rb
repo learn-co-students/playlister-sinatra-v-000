4cacd3cfc62fe1c9ac6ebaf73022e4eb1efe5f61
Jordan Kline <jordan.kline@gmail.com>
2018-02-15 21:05:25 -0800

    All tests passing.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,4 +29,23 @@ class SongsController < ApplicationController
 
 	  redirect to "/songs/#{@song.slug}"
 	end
+
+	get '/songs/:slug/edit' do
+	  @song = Song.find_by_slug(params[:slug])
+	  
+	  erb :'/songs/edit'
+	end
+
+	patch '/songs/:slug' do
+	  @song = Song.find_by_slug(params[:slug])
+
+	  @song.update(params[:song])
+	  @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
+	  @song.save
+
+	  flash[:message] = "Successfully updated song."
+
+	  redirect to "/songs/#{@song.slug}"
+	end
+
 end
