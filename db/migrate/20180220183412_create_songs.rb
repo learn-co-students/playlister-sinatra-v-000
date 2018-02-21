95dc409c11b25131125ad8aad36c9de2ff807dfc
Eva Navarror <emanao@me.com>
2018-02-21 20:45:28 +0000

    initial working db and models setup

--- db/migrate/20180220183412_create_songs.rb
+++
@@ -1,8 +0,0 @@
-class CreateSongs < ActiveRecord::Migration
-  def change
-    create_table :songs do |t|
-      t.string :name
-      t.integer :artist_id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220183649) do
+ActiveRecord::Schema.define(version: 20180221200337) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
