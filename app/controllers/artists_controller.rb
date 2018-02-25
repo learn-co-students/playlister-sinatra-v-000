2dc7810849b113a8ae3bbfa955f7f17037ce7f25
funemployeddad <funemployeddad@gmail.com>
2018-02-24 19:07:39 -0700

    msg

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ArtistsController < ApplicationController
 
   get '/artists' do
@@ -11,22 +10,5 @@ class ArtistsController < ApplicationController
     @artist = Artist.find_by_slug(params[:slug])
     erb :"/artists/show"
   end
-=======
-require './app/models/concerns/slugifiable'
-
-class ArtistsController < ApplicationController
-  include Slugifiable
-
- get '/artists' do
-   @artists = Artist.all
-   erb :"/artists/index"
- end
-
- get '/artists/:slug' do
-   @artist = Artist.find_by_slug(params[:slug])
-
-   erb :"artists/show"
- end
->>>>>>> 1f9fad4566e7bea66933d1080285aea037651e86
 
 end
