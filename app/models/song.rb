0bab2eb9565847d1dc16f641502a7fa88c143567
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-26 22:29:13 +0000

    added #slug and ::find_by_slug to song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.parameterize
+  end
+
+  def self.find_by_slug(slug)
+    song_name = slug.titleize
+    self.all.detect{|song| song.name == song_name}
+  end
 end
