b33f1bba49e9a2a602d8d37beee3709d1b98e531
Sally Zasloff <szasloff@gmail.com>
2018-02-28 19:18:09 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,4 @@
+class Song < ActiveRecord::Base
+  belongs_to :artists
+  has_many :genres
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228190442) do
+ActiveRecord::Schema.define(version: 20180228191356) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20180228190442) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string  "name"
+    t.integer "artist_id"
   end
 
 end
