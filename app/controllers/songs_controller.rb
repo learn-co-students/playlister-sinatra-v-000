618cf049b21365c2b5e22bd70d4291c7ef0e066b
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-01 02:30:07 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,27 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params[:Name])
+    binding.pry
+    @genres = Genre.find_by(id: params[:genres])
+    @song.song_genres.build(genre: @genres)
+
+    @artist = Artist.find_or_create_by(name: params["Artist Name"])
+
+    @song.artist = @artist
+  end
+
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
+
 
 end
