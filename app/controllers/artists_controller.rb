4914ec3add96c1f29a1d4ec15cabd8446017fefc
Sally Zasloff <szasloff@gmail.com>
2018-03-01 22:31:45 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,12 @@ class ArtistsController <ApplicationController
       erb :'/artists/index'
     end
 
+    get '/artists/:slug' do
+      @artist = Artist.find_by_slug(params[:slug])
+
+      erb :'/artists/show'
+    end
+
+
+
 end
