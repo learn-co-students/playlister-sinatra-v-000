3c73dc8910416a482954a83fcb5a46718d93d314
Sally Zasloff <szasloff@gmail.com>
2018-03-01 00:02:22 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,19 +1,17 @@
-require 'pry'
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
 
 
-def slug
+  def slug
   	name.downcase.gsub(" ","-")
-end
+  end
 
 
-def self.find_by_slug(slug)
-
-end
-
+  def self.find_by_slug(slug)
+    self.all.detect{|artist|artist.slug == slug}
+  end
 
 
 end
