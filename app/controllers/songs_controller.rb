22d1029a6257eaeef6959f202b23d36dd0cb4d51
Sally Zasloff <szasloff@gmail.com>
2018-03-01 20:46:15 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,4 +5,15 @@ class SongsController <ApplicationController
 
     erb :'/songs/index'
   end
+
+
+  get '/songs/:slug' do
+    binding.pry
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'/songs/show'
+  end
+
+
+
 end
