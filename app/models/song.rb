4f0952f1207de1d331e891284e26fa5849b3204c
Alexander Urizar <urizaraj@gmail.com>
2018-03-02 20:54:23 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,12 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
   belongs_to :artist
+
+  def slug
+    self.name.downcase.scan(/[a-z]+/).join('-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |e| e.slug == slug }
+  end
 end
