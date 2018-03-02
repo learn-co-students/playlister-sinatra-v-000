f9d3ca8b80c1615baace99478d11a1ca35c5099c
Alexander Urizar <urizaraj@gmail.com>
2018-03-02 23:08:04 +0000

    Done.

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -8,12 +8,18 @@ class SongController < Sinatra::Base
     erb :'songs/index'
   end
 
-  get 'songs/new' do
+  post '/songs' do
+    song = Song.create(params[:song])
+    song.update(artist: Artist.find_or_create_by(params[:artist]))
+    redirect "/songs/#{song.slug}"
+  end
+
+  get '/songs/new' do
     erb :'songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb :'songs/detail'
+    erb :'songs/show'
   end
 end
