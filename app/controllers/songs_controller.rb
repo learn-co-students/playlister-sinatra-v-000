becffd66d920d16d2dcbf2c7d433afa14c46fcfc
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-02 22:03:16 +0000

    working post '/songs' action and init patch '/songs/:slug'

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
+
   get '/songs/new' do
     erb :'/songs/new'
   end
@@ -22,6 +26,23 @@ class SongsController < ApplicationController
       @song.genre_ids = params["genres"]
     end
     @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    # binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params)
+    @song.update(params[:song])
+
+    flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
 
