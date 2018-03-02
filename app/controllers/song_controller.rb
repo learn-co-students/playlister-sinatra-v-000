48d901dcf7dca0f072218cfbe59d4a9fcf1dd1dc
Alexander Urizar <urizaraj@gmail.com>
2018-03-02 21:42:34 +0000

    Done.

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -8,6 +8,10 @@ class SongController < Sinatra::Base
     erb :'songs/index'
   end
 
+  get 'songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/detail'
