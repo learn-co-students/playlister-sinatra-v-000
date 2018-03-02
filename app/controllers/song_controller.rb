b58c33fe1f2e64fad83592f3228d1cc794115264
Alexander Urizar <urizaraj@gmail.com>
2018-03-02 21:17:24 +0000

    Done.

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,9 +1,10 @@
-class ApplicationController < Sinatra::Base
+class SongController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views") }
 
   get '/songs' do
-    erb :songs
+    @songs = Song.all
+    erb :'songs/index'
   end
 end
--- config.ru
+++ config.ru
@@ -5,4 +5,5 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 use Rack::MethodOverride
+use SongController
 run ApplicationController
