38f9242d0e94dea0d19df96239f35ac051246d21
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-02 03:38:01 +0000

    added slug routes to artists and genres controllers

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,9 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
     erb :'/artists/index'
   end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+  end
 end
