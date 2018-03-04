f1be00b4fb67f118128b81850d026a5f1a63f351
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-03-04 20:49:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
+  register Sinatra::Flash
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
   end
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
 end
