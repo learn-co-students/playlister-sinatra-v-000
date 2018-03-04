717d7c999470f8f9f48f5c86a2062ac9728b6b81
Meredith Reece <mardebugger@gmail.com>
2018-03-04 05:01:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,16 +11,13 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    # binding.pry
     erb :'songs/show'
   end
 
   post '/songs' do
     @song = Song.create(params[:song])
-    # binding.pry
     if !params[:artist].empty?
       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-      # binding.pry
     end
     if !params[:genre][:name].empty?
       @song.genres << Genre.create(name: params[:genre][:name])
@@ -29,7 +26,30 @@ class SongsController < ApplicationController
     end
     @song.save
     flash[:message] = "Successfully created song."
-    # binding.pry
     redirect to("/songs/#{@song.slug}")
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genre = Genre.find_by(id: @song.genre_ids)
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    if !params[:song][:name].empty?
+      @song = Song.update(params[:song][:name])
+    elsif !params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    elsif !params[:genre][:name].empty?
+      @song.genres << Genre.find_or_create_by(name: params[:genre][:name])
+    else
+      @song.genre_ids = params[:genres]
+    end
+
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 end
