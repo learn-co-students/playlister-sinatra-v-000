41a9c05bc38a805a8a86c2e3672d2a0548448e75
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-07 14:48:06 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,23 @@
-# Add seed data here. Seed your database with `rake db:seed`
+public_access_tv = Artist.create(name: "Public Access TV")
+sufjan = Artist.create(name: "Sufjan Stevens")
+white_reaper = Artist.create(name: "White Reaper")
 
+mystery = Song.create(name: "Mystery of Love")
+monaco = Song.create(name: "Monaco")
+evil_disco = Song.create(name: "Evil Disco")
+
+indie = Genre.create(name: "Indie")
+garage_rock = Genre.create(name: "Rock")
+indie_rock = Genre.create(name: "Indie Rock")
+
+public_access_tv.songs << evil_disco
+evil_disco.genres << garage_rock
+public_access_tv.save
+
+sufjan.songs << mystery
+mystery.genres << indie
+sufjan.save
+
+white_reaper.songs << monaco
+monaco.genres << indie_rock
+white_reaper.save
