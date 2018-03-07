0dcf123a3dae189c5233b04f95e0b449b6331529
Katrina Brinson <kbrinso@gmail.com>
2018-03-07 21:08:22 +0000

    getting closer to solving new song controller and view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,13 +22,13 @@ class SongsController < ApplicationController
   post '/songs' do
     #not sure if the below is correct
     @song = Song.create(name: params[:song_name])
-    @artists = Artist.all
-    @genres = Genre.all
     if !params[:artist_name].empty?
       @artist = Artist.create(name: params[:artist_name])
       @artist.save
+      @song.artist = @artist
     else
       @artist = Artist.find_by_slug(params[:artist_slug])
+      @song.artist = @artist
     end
 
     if !params[:genre_name].empty?
@@ -38,7 +38,6 @@ class SongsController < ApplicationController
       @genre = Genre.find_by_slug(params[:genre_slug])
     end
 
-    @song.artist = @artist
     # @song.genre
     @song.save
     # @genres = params[:genres]
