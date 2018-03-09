d1f1ee38bc80e4723f7f3b90be260b0c63328ff7
ronald vilorio <soccer23415@gmail.com>
2018-03-09 01:00:33 +0000

    Done.

--- db/migrate/20180306205417_create_genres_table.rb
+++ db/migrate/20180306205417_create_genres_table.rb
@@ -2,7 +2,6 @@ class CreateGenresTable < ActiveRecord::Migration[5.1]
   def change
     create_table :genres do |t|
       t.string :name
-      t.integer :song_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20180307004141) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.integer "song_id"
   end
 
   create_table "song_genres", force: :cascade do |t|
