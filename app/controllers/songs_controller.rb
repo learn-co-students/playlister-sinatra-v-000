db91be2f8e718fd7908ea2000fd2122ab706c4b4
ingridwong0715 <ingrid830715@gmail.com>
2018-03-11 13:51:35 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,27 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'songs/index'
   end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+
+    @song = Song.new(params[:song])
+    if @artist =Artist.find_by(name: params[:artist][:name])
+      @song.artist = @artist
+    else
+      @song.build_artist(params[:artist])
+    end
+    @song.save
+    redirect "/songs/#{@song.slug}"
+
+
+
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,9 @@
 class Genre < ActiveRecord::Base
   require_relative '../../lib/slug_methods.rb'
   has_many :song_genres
-  has_many :artists, through: :song_genres, source: :song
   has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
 
   extend SlugMethods::ClassMethods
   include SlugMethods::InstanceMethods
