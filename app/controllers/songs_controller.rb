28b52d5b1d1ab2f43f65a53e8cc080f260b32b4a
Chely Ho <adept_inept@yahoo.com>
2018-03-20 00:10:10 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,14 +5,15 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+["blues", "greens", "reds", "rock"].each {|genre| Genre.create(name: genre)}
     @genres = Genre.all
     erb :'songs/new'
   end
 
   post '/songs' do
     artist = Artist.find_or_create_by(name: params[:artist])
-    #genres = params[:genres].map {|genre| Genre.find_or_create_by(name: genre)}
-    @song = Song.create(name: params[:name], artist: artist)
+    genres = params[:genres].map {|genre| Genre.find_or_create_by(name: genre)}
+    @song = Song.create(name: params[:name], artist: artist, genres: genres)
     flash[:message] = "Successfully created song."
     redirect "songs/#{@song.slug}"
   end
@@ -24,12 +25,20 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @genres = Genre.all || [{name=>"blah"}, {name=>"bloop"}, {name=>"bloop"}]
+    @genres = Genre.all
     erb :'songs/edit'
   end
 
-  post '/songs/:slug' do
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
+    puts params
+    binding.pry
+
+    @song.name = params[:name]
+    @song.artist = Artist.find_or_create_by(name: params[:artist])
+    @song.genres = params[:genres].map {|genre| Genre.find_or_create_by(name: genre)}
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect "songs/#{@song.slug}"
   end
 end
