7bdb740955738d3207a53235b0f43c193e6a72c2
github username <github email address>
2018-03-21 17:57:24 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,7 +39,7 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-    @song.genre_ids = params[:genres]
+    @song.genre_ids = params["genres[]"]
     @song.save
 
     flash[:message] = "Successfully updated song."
