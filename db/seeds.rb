4dc23f33274afb3478968244ead857a3f8033f0c
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-27 17:13:45 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+@artist = Artist.create(name: "Luda McDuda")
+@song = Song.create(name: "Ludalicious")
+@genre = Genre.create(name: "LudaMcD Soul")
+Genre.create(name: "What im talkin bout")
+
+@song.song_genres.create(genre: @genre)
+@song.artist = @artist
+
+@song.save
