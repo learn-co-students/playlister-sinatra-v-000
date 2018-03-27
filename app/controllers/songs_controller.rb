e502278aabc918752a7a03e96e20d1d29763c2f6
Kyle Horsch <krhorsch@yahoo.com>
2018-03-27 15:35:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,11 +4,13 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
-  post '/songs/new' do
-    artist = Artist.create(name: params[artist_name])
-    song = Song.create(name: params[song_name], genre: params[genres][])
-    artist.songs << song
-    redirect_to '/songs/:slug'
+  post '/songs' do
+    @artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song = Song.create(name: params["song_name"])
+    @song.genre_ids = params["genres"]
+    @song.artist = @artist
+    @song.save
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
@@ -16,15 +18,13 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
-  post '/songs/:slug/edit' do
+  patch '/songs/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @artist = Artist.find_or_create_by(name: params[artist_name])
-    params[genres][].each do |g|
-      @songs.genres << g
-    end
-    @artist.songs << @song
-    @artist.save
-    render '/songs/show'
+    @artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.update(genre_ids: params["genres"])
+    @song.artist = @artist
+    @song.save
+    render :'/songs/show'
   end
 
   get '/songs/:slug' do
