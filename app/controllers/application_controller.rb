3d50c5e3325303bab1f353e76601d58d44be517e
TRIP <lm3@protonmail.ch>
2018-03-26 20:58:24 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  enable :sessions
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+  
   get '/' do
     erb :index
   end
