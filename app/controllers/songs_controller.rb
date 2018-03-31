d4850fa6b2b190a7826515914b3762fdf0cc26d6
Kim Gonzales <Kimgo589@gmail.com>
2018-03-31 17:03:53 -0400

    add index page that links to songs,artist,genre indexes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,1 +1,4 @@
-# Pay attention to the order of /songs/new and /songs/:slug. The route /songs/new could interpret new as a slug if that controller action isn't defined first.
+class SongsController < ApplicationController
+    # Pay attention to the order of /songs/new and /songs/:slug. The route /songs/new could interpret new as a slug if that controller action isn't defined first.
+end 
+
