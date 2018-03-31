966ec39f72a002d334744d985747b07bd8e1c550
github username <github email address>
2018-03-30 19:14:28 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,16 +43,19 @@ class SongsController < ApplicationController
     end
 
     
-    get '/songs/:slug' do
+    get '/songs/:slug/edit' do
         @song = Song.find_by_slug(params[:slug])
-    end
+        erb:'/songs/edit' 
+        end
 
     patch '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         @song.update(params[:song])
-        @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
+        # @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
         @song.save
-        
+        @song.artist = Artist.find_by(:name => params[:artist][:name])
+        @song.artist.update(params[:artist][:name])
+        @song.artist.save
         # flash[:message] = "Successfully updated song."
         redirect("/songs/#{@song.slug}")
       end
