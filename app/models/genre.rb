25ffb3e6410675c7aee1c125f22f7d1a7eabe6c9
Kim Gonzales <Kimgo589@gmail.com>
2018-03-31 14:00:55 -0400

    migrate songs, artists, genres, song_genres tables. artist spec passing

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,14 +1,14 @@
 class Genre < ActiveRecord::Base 
-    has_many :artists through: :songs 
+    has_many :artists, through: :songs 
     has_many :song_genres
-    has_many :songs through: :song_genres 
+    has_many :songs, through: :song_genres 
 
     def slug
         self.downcase.gsub(" ","-")
     end 
 
     def self.find_by_slug(slug)
-        Genre.all.find{|a| a.slug == slug}
+        Genre.all.find{|genre| genre.slug == slug}
     end 
 
 end 
