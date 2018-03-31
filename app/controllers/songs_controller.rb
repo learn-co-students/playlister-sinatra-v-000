dd63eb4d1bd458ab9f94da8c0ed7427cbef5a8fc
Kim Gonzales <Kimgo589@gmail.com>
2018-03-31 17:25:08 -0400

    add artist, song, controller index, new and slug routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,15 @@
 class SongsController < ApplicationController
     # Pay attention to the order of /songs/new and /songs/:slug. The route /songs/new could interpret new as a slug if that controller action isn't defined first.
+    get '/songs' do
+        @songs = Song.all  
+        erb :'/songs/index'
+    end 
+    
+    get '/songs/new' do 
+    end 
+
+    get '/songs/:slug' do 
+    end 
+
 end 
 
