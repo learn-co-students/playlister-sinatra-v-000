d04121b550dfb188f5e914e10baab5e102d427a4
Kim Gonzales <Kimgo589@gmail.com>
2018-03-31 16:12:17 -0400

    rake db drop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
+#i ran rake console LibraryParser.new.call so that seemed to add all of the files to my database. 
 LibraryParser.new.call
