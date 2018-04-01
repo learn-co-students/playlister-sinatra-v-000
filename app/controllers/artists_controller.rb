aa9a2b6974f8da1285e3ff4a518bda3868045e89
Kim Gonzales <Kimgo589@gmail.com>
2018-03-31 20:44:15 -0400

    artist show specs passing

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,6 +8,7 @@ class ArtistsController < ApplicationController
     end 
 
     get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
         erb :'/artists/show' 
     end 
 end
