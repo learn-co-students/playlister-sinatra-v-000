f1e9d5c8b8e7a372cfe99867015781772265d63f
James Randolph <james.randolph2016@gmail.com>
2018-04-03 23:22:00 +0000

    posts

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,4 +17,13 @@ class SongsController < AplicationController
     erb :'songs/show'
   end
   
-  
+  post '/songs' do
+    @song = Song.create(:name +> params["Name"])
+    @song.artist = Artist.find_or_create_by(:name +> params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    
+    flash[:message] = "Song created."
+    
+    redirect("/songs/#{@song.slug}")
+  end
