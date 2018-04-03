73d786a68c04e880c4a61f27ed2085cebd851c81
Sean Oughton <seanoughton@gmail.com>
2018-04-03 18:45:54 +0000

    Done.

---
+++ db/migrate/20180403183524_create_songs_table.rb
@@ -0,0 +1,10 @@
+class CreateSongsTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.string :artist
+      t.string :genre
+      t.integer :artist_id
+    end
+  end
+end
