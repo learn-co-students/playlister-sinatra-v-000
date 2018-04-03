137de5b082032bef2af4c570b1ff6244bec426ba
James Randolph <james.randolph2016@gmail.com>
2018-04-03 23:18:05 +0000

    tart of songs_controller.rb

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,20 @@
+require 'rack-flash'
+
+class SongsController < AplicationController
+  use Rack::Flash
+  
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+  
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+  
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+  
+  
