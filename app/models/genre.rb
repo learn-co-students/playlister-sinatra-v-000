e7510959b16bf8fa857944267d43a32083ad28f4
Sean Oughton <seanoughton@gmail.com>
2018-04-03 20:47:30 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ActiveRecord::Base
-  has_many :songs
-  has_many :artists, through: :songs
+  has_many :songs_genres
+  has_many :songs, :through => :songs_genres
+  has_many :artists, :through=> :songs
 end
--- db/migrate/20180403183524_create_songs_table.rb
+++ db/migrate/20180403183524_create_songs_table.rb
@@ -2,10 +2,7 @@ class CreateSongsTable < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
       t.string :name
-      t.string :artist
-      t.string :genre
       t.integer :artist_id
-      t.integer :genre_id
     end
   end
 end
