18ff1e2ddec7aea4aafb3093c938911b00525918
Sean Oughton <seanoughton@gmail.com>
2018-04-04 19:46:04 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -8,11 +8,11 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      name = slug.gsub(/-/, " ").split(" ").map do |word|
+      object_name = slug.gsub(/-/, " ").split(" ").map do |word|
         word.capitalize
       end.join(" ")
-      song = self.find_by(name: "#{name}")
-      #song = self.find_by(name: name)
+      #song = self.find_by(name: "#{name}")
+      song = self.find_by(name: object_name)
         binding.pry
     end
   end
