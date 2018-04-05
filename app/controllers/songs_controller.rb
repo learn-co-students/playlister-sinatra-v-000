079df09885654822f47c441d039fca2f13b8c16a
Sean Oughton <seanoughton@gmail.com>
2018-04-05 21:08:32 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,19 +42,18 @@ class SongsController < ApplicationController
       if !@artist
         @artist = Artist.create(name: params[:song][:artist])
       end
+    elsif
+      @artist = Artist.find(params[:song][:artist_id])
     end
+=begin
     if params[:song][:artist_id]
       @artist = Artist.find(params[:song][:artist_id])
     end
-    @song = Song.find(params[:id])
-=begin
-    @artist = Artist.find_by(name: params[:song][:artist])
-    if !@artist
-      @artist = Artist.create(name: params[:song][:artist])
-    end
 =end
+    @song = Song.find(params[:id])
     @song.artist = @artist
     @song.save
+    binding.pry
     flash[:message] = "Successfully updated song."
     redirect to "/songs/#{@song.slug}"
   end
