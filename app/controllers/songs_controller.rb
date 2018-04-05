8a822f1a701876e6daa922553289d9a35160ffb7
Sean Oughton <seanoughton@gmail.com>
2018-04-05 19:26:46 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,15 +26,17 @@ class SongsController < ApplicationController
     redirect to "/songs/#{@song.slug}"
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
 
-  get '/songs/:slug/edit' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/edit'
-  end
+
 
   post '/songs/edit' do
 
