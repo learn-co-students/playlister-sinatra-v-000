b453b5631329be8f1b7ebd64e923c46e98f8da64
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-05 16:16:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  use Rack::Flash
 
   get '/' do
     erb :index
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
+  require 'rack-flash'
 class SongsController < ApplicationController
-
+  use Rack::Flash
   get '/songs' do
     @songs = Song.all
     erb :"/songs/index"
@@ -17,10 +18,33 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(name: params[:name])
-    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    #binding.pry
+    @song = Song.create(name: params[:Name])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
+
+    flash[:message] = "Successfully created song."
+
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    #binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/edit"
+  end
+
+#remember update corresponds to PATCH!! the edit form needs syntax for patch (hidden input _method)
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    #binding.pry
+    @song.update(name: params["Name"], artist: ["Artist Name"], genre_ids: params[:genres])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.save
+
+
+    flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
 
