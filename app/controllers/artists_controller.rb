f5403cc654280e0c4d9d317cc7e367e7b0c0d179
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-09 23:13:33 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,22 @@
 class ArtistsController < ApplicationController
+
   get '/songs' do
-    
+
   end
+
+  get '/artists' do
+    #displays a list of artists
+    @artist = Artist.all
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    #displays the artist's songs
+    #{"captures"=>[], "slug"=>"Artist"}
+    # #<Artist:0x000000021d54f0 id: 1, name: "Artist">
+    @artist = Artist.find_by_slug(params["slug"])
+    erb :'/artists/show'
+  end
+
+
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,35 @@
 class SongsController < ApplicationController
   get '/songs' do
+    #shows all of the songs
     @songs = Song.all
     erb :'/songs/index'
   end
+
+  get '/songs/new' do
+    #creates a new song without a new artist
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    #posts the user inputs to the new song
+    #params {"song_name"=>"Bad Company", "artist"=>{"name"=>"A$AP Rocky"}, "captures"=>[]}
+    if !params["Artist Name"].empty?
+      # binding.pry
+      @song = Song.create(name: params["Name"], artist: Artist.find_or_create_by(name: params["Artist Name"]), genre_ids: params[:genres])
+    else
+      @song = Song.create(name: params["Name"], artist: Artist.find(params["artist"]["id"]), genre_ids: params["genres"])
+    end
+    @song.save
+
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
+    #displays the song's artist and genre/s
+    #params {"captures"=>[], "slug"=>"song"}
+    @song = Song.find_by_slug(params["slug"])
+    erb :'/songs/show'
+  end
+
+
 end
