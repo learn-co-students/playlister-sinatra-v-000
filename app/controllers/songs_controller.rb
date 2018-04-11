5b96f8084afe6e75b1739bd151b74fd4446c55ea
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-11 11:06:52 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -41,7 +41,7 @@ class SongsController < ApplicationController
     #{"_method"=>"patch", "name"=>"Poetic Justice", "artist"=>{"id"=>"1", "name"=>""}, "captures"=>[], "slug"=>"poetic-justice"}
     # binding.pry
     @song = Song.find_by_slug(params[:slug])
-    @song.update(params[:song])
+    @song.name = params["Song Name"]
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
