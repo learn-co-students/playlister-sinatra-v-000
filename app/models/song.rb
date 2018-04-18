d7f183c2cdc47f3a7e15f58e763f77ef2c6851cb
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-18 17:50:33 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,14 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, :through => :song_genres
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.find{|song| song.slug == slug}
+  end
+
+end
