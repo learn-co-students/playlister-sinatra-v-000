36de0e48d556d0aa27b298052d3fe741d989ed15
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-23 05:05:57 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,17 +7,15 @@ class GenresController < ApplicationController
 
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params[:slug])
-    #binding.pry
     @songs = []
     SongGenre.all.each do |sg|
       #binding.pry
-      if sg.genre_id == @genre.id
+      if @genre.id == sg.genre_id
         @songs << Song.find(sg.song_id)
       end
     end
     @artists = []
     @songs.each do |song|
-      #binding.pry
       @artists << song.artist
     end
     #binding.pry
