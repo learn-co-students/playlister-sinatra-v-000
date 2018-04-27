98007219d301870ab1d0ddee7a36c94c9eb99bdb
Denise Pendleton <dpendl1@hotmail.com>
2018-04-27 02:26:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable :sessions
 
   get '/' do
     erb :index
@@ -11,6 +12,8 @@ class ApplicationController < Sinatra::Base
     erb :'/songs/index'
   end
   get '/songs/new' do
+    # session[:success_message] = "Song successfully updated."
+    #   @success_message = session[:success_message]
     erb :'/songs/new'
   end
 
@@ -21,6 +24,7 @@ class ApplicationController < Sinatra::Base
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug] )
     # @song.slug = params[:slug]
+
     erb :'/songs/show'
   end
 
@@ -45,23 +49,26 @@ class ApplicationController < Sinatra::Base
 
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params[:slug] )
-    # @genre.slug = params[:slug]
     erb :'/genres/show'
   end
 
    post '/songs' do
     #  raise params.inspect
-
-    @artist = Artist.find_or_create_by(:name => params["artist_name"])
+      @artist = Artist.find_or_create_by(:name => params["artist_name"])
       @song = Song.create(:name => params["song_name"], :artist_id => @artist.id)
-    # @song.artist = Artist.find_or_create_by(:name => params[:song]["Artist Name"])
-    @song.genre_ids = params[:genres]
-    @song.save
-      # binding.pry
-      # raise params.inspect
-       erb :"/songs/show"
-    end
 
-    # raise params.inspect
-  end
+      @song.genre_ids = params[:genres]
+
+      @song.save
+      #
+      # session[:success_message] = "Song successfully updated."
+      #   @success_message = session[:success_message]
+
+        # binding.pry
+
+       redirect "/songs/#{@song.slug}"
+
+
+    end
+end
 #
