0e6b45ddb994e8105d4f2470b1a344c19cb453b4
Denise Pendleton <dpendl1@hotmail.com>
2018-04-27 19:25:02 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'sinatra-flash'
+gem 'tux'
 
 group :test do
   gem 'rspec'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  require 'sinatra/flash'
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
-  enable :sessions
+
 
   get '/' do
     erb :index
@@ -12,18 +14,12 @@ class ApplicationController < Sinatra::Base
     erb :'/songs/index'
   end
   get '/songs/new' do
-    # session[:success_message] = "Song successfully updated."
-    #   @success_message = session[:success_message]
+    #
     erb :'/songs/new'
   end
 
-
-
-
-
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug] )
-    # @song.slug = params[:slug]
 
     erb :'/songs/show'
   end
@@ -61,14 +57,44 @@ class ApplicationController < Sinatra::Base
 
       @song.save
       #
-      # session[:success_message] = "Song successfully updated."
+      # flash[:success_message] = "Song successfully updated."
       #   @success_message = session[:success_message]
 
         # binding.pry
 
        redirect "/songs/#{@song.slug}"
+       end
 
+       get "/songs/:slug/edit" do
+         @song = Song.find_by_slug(params[:slug])
 
-    end
+         erb :'/songs/edit'
+       end
+
+       patch '/songs/:slug' do
+         @song = Song.find_by_slug(params[:slug])
+        @artist = Artist.find_or_create_by(:name => params["artist_name"])
+        @song.update(:name => params["song_name"], :artist_id => @artist.id)
+
+         @genres = Genre.find(params[:genres])
+
+         @song.song_genres.clear
+
+        @genres.each do |genre|
+          song_genre = SongGenre.new(:song_id => @song.id, :genre => genre)
+        end
+         @song.save
+         flash[:success] = "Successfully updated song."
+         redirect "/songs/#{@song.slug}"
+        #
+       end
+
+       delete '/songs/:slug'  do
+         @song = Song.find_by_slug(params[:slug])
+         @song.destroy
+         redirect '/songs'
+
+       end
+
 end
 #
