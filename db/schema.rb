a3b32124a592991f5e515e8475ff5e83dbafd1d5
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-05-01 14:46:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427160538) do
+ActiveRecord::Schema.define(version: 20180501135357) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
   end
 
 end
