e70b8ff9d29d0e443c69c9db5058c9e9277f26e6
John O'Connor <oconnojb@gmail.com>
2018-05-02 13:38:01 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -9,13 +9,27 @@ module Slugifiable
   module ClassMethods
     def unslug(slug)
       array = slug.split("-")
-      n_array = array.collect {|word| word.capitalize}
+      n_array = []
+      n_array << array[0].capitalize
+      array.each_with_index do |item, i|
+        if i>0
+          if item == "with" || item == "the" || item == "a" || item == "of"
+            n_array << item
+          else
+            n_array << item.capitalize
+          end
+        end
+      end
       n_array.join(" ")
     end
 
     def find_by_slug(slug)
       result = unslug(slug)
-      self.find_by_name(result)
+      if self.name == "Genre" && result.split.size == 1
+        self.find_by_name(result.downcase)
+      else
+        self.find_by_name(result)
+      end
     end
   end
 end
