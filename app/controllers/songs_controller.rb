c75728a7ce2ef3b20fb89595066f4edcc7fedca1
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-05-03 14:36:36 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,13 @@
+require 'rack-flash'
+
+
+
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
+
+
 
   get '/songs' do
 
@@ -12,24 +21,30 @@ class SongsController < ApplicationController
     end
 
     get '/songs/:slug' do
+      #binding.pry
       @song = Song.find_by_slug(params[:slug])
       erb :'/songs/show'
     end
 
     post '/songs' do
-      if !params["artist"].nil?
-      @song = Song.create(name:params["name"])
+    #  binding.pry
+      @song = Song.create(name:params[:name])
       @song.artist = Artist.find_or_create_by(:name => params["artist"])
-      @song.genre_ids = params[:genres]
-      end
-      binding.pry
       @song.save
-      flash[:message] = "Successfully created song."
-binding.pry
-      redirect to("/songs/#{@song.slug}")
-      binding.pry
-  end
+#binding.pry
+      @song.genre_ids = params[:genres]
 
+      flash[:message] = "Successfully created song."
+
+#binding.pry
+      redirect to("/songs/#{@song.slug}")
+    end
+# use in console to delete empty songs -> Artist.all.select{|artist| artist unless artist.name}.each do |artist| artist.destroy end
+
+    patch '/songs/:slug/edit' do
+
+      erb :'/songs/edit'
+    end
 
     get '/songs/:id' do
       @song = Song.find(params[:id])
