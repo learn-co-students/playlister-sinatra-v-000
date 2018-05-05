8b6f722486f7d7549865e6958e899af6b7f626cd
Ariana Berger <ariana.berger@gmail.com>
2018-05-05 18:23:57 +0000

    Done.

--- db/migrate/20180503221942_create_songgenre.rb
+++ db/migrate/20180503221942_create_songgenre.rb
@@ -3,7 +3,6 @@ class CreateSonggenre < ActiveRecord::Migration[5.2]
     create_table :song_genres do |t|
       t.integer :song_id
       t.integer :genre_id
-      t.integer :artist_id
     end
   end
 end
