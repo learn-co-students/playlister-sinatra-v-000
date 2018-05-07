454aa3a8f07486c6d8a535f21528a28d754c8b80
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-07 00:35:42 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504033507) do
+ActiveRecord::Schema.define(version: 20180506234431) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20180504033507) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
