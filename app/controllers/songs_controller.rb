25ac852226086b0a219e29771111fbb78fdc8854
Elle Faraday <faraday.elle@gmail.com>
2018-05-09 15:13:04 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,13 +17,57 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @artists = Artist.all
+    @genres = Genre.all
+    erb :'/songs/edit'
+  end
+
   post '/songs' do
-    binding.pry
-    @artist = Artist.create(params[:song][:artist])
+    if Artist.find_by_name(params[:song][:artist][:name]) != nil
+      @artist = Artist.find_by_name(params[:song][:artist][:name])
+    elsif params[:song][:artist][:name] != "name"
+      @artist = Artist.create(params[:song][:artist])
+    else
+      @artist = Artist.find_by_id(params[:song][:artist_id]["{}"].to_i)
+    end
     @song = Song.create(name: params[:song][:name], artist: @artist)
-    @genres_ids = params[:song][:genres].collect {|i| i.to_i}
-    @song.genres << Genre.find_by_id(@genres_ids.first)
-     redirect to '/songs'
+    if params[:song][:new_genre] != "name"
+      @new_genre = Genre.create(name: params[:song][:new_genre])
+      @song.genres << @new_genre
+    elsif !params[:song][:genres].empty?
+      @genres_ids = params[:song][:genres].collect {|i| i.to_i}
+      @genres_ids.each do |id|
+        @song.genres << Genre.find_by_id(id)
+      end
+      @song.save
+    end
+     redirect to "songs/#{@song.slug}"
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    if Artist.find_by_name(params[:song][:artist][:name]) != nil
+      @artist = Artist.find_by_name(params[:song][:artist][:name])
+    elsif params[:song][:artist][:name] != "name"
+      @artist = Artist.create(params[:song][:artist])
+    else
+      @artist = Artist.find_by_id(params[:song][:artist_id]["{}"].to_i)
+    end
+    @song.artist = @artist
+    if params[:song][:new_genre] != "name"
+      @new_genre = Genre.create(name: params[:song][:new_genre])
+      @song.genres << @new_genre
+    elsif !params[:song][:genres].empty?
+      @genres_ids = params[:song][:genres].collect {|i| i.to_i}
+      @genres_ids.each do |id|
+        @song.genres << Genre.find_by_id(id)
+      end
+      @song.save
+    end
+
+    redirect to "songs/#{@song.slug}"
   end
 
 
