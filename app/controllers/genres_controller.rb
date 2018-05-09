383430bddbb0b52f8e32b9b9b3f02fe9dcfff373
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-09 00:29:33 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,10 @@ class GenresController < ApplicationController
     @genres = Genre.all
     erb :'genres/index'
   end
+
+  get '/genres/:slug' do
+    slug = params[:slug]
+    @genre = Genre.all.detect { |genre| genre.slug == slug}
+    erb :'genres/show'
+  end
 end
