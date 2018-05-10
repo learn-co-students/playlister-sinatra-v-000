d78aaccc8f2281d133dc6dde212e8fe1fd3cbfc3
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-10 14:42:13 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,26 +1,38 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
+
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
   end
 
   get '/songs/new' do
+    @genres = Genre.all
     @artists = Artist.all
     erb :'songs/new'
-  end
-
-  post '/songs' do
-    @song = Song.create(params[:song])
-    if !params["artist"]["name"].empty?
-      @song.artist = Artist.create(name: params["artist"]["name"])
-    end
-    @song.save
-    redirect to "songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
     slug = params[:slug]
     @song = Song.all.detect { |song| song.slug == slug}
     erb :'songs/show'
+  end
+
+  post '/songs' do
+    song = Song.create(params[:song])
+    song_artist = Artist.find_or_create_by(params[:artist])
+    song.artist = song_artist
+    song_artist.songs << song
+    song_artist.save
+    song.save
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{song.slug}")
+  end
+
+  post '/songs/:slug' do
+    
   end
 end
--- config.ru
+++ config.ru
@@ -5,7 +5,7 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 use Rack::MethodOverride
-use SongsController
 use ArtistsController
+use SongsController
 use GenresController
 run ApplicationController
