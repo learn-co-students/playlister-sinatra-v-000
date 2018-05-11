fca45b3ab88315d0bb6bb7c5cea6210b0a250609
Cassandra Dickson <cassandradickson7@gmail.com>
2018-05-11 19:00:18 +0000

    Done.

---
+++ db/migrate/20180510191808_create_songs.rb
@@ -0,0 +1,9 @@
+class CreateSongs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.timestamps
+    end
+  end
+end
