2dc8dbb68ca0bd35d1b3f9186181095131df7cea
amanda johnson <manda.panda91@yahoo.com>
2018-05-15 00:11:08 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,11 +34,11 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
-
-    @song = Song.find_by_slug(params[:slug])
-    @song.update(params["Name"])
-    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     #binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+
     @song.genre_ids = params[:genres]
     @song.save
     #binding.pry
