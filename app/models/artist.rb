74387aa6ccfcbf2f2ac18826a7dc1ee8e79b07f0
Raiet Intraub <raietsaeed@gmail.com>
2018-05-19 01:17:01 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :song_genres, through :songs
-  has_many :genres, through :song_genres
+  has_many :song_genres, through: :songs
+  has_many :genres, through: :song_genres
 end
 
 
