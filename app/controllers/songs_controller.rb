71730b4481f9c7bd68d98791984171da34ba180c
Raiet Intraub <raietsaeed@gmail.com>
2018-05-19 04:19:37 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,13 +6,27 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
-
+    @genres = Genre.all
     erb :'/songs/new'
   end
 
   post '/songs' do
-    # @song = Song.create()
-    # redirect to "/songs/#{@song.slug}"
+    puts "Params = #{params}"
+    @song = Song.create(name: params[:song_name])
+
+    if !Artist.find_by(name: params[:artist_name])
+      @artist = Artist.create(name: params[:artist_name])
+    else
+      @artist = Artist.find_by(name: params[:artist_name])
+    end
+    @song.artist_id = @artist.id
+
+    if !params["genres"].empty?
+      SongGenre.create(genre_id: params["genres"], song_id: @song.id)
+    end
+
+    @song.save
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
