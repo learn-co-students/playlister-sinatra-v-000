7c777cb314cbc5b1b8b74f32b251a003f152b1d0
Raiet Intraub <raietsaeed@gmail.com>
2018-05-19 01:14:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,10 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 end
+
+
+
+# Referenced Labs
+# sinatra-complex-forms-associations-v-000
+# sinatra-restful-routes-lab-v-000
+# sinatra-user-auth-v-000
