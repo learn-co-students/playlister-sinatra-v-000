853d20b52b9beab237e95565aca4d99ffa966d61
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-05-23 14:45:38 -0700

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,18 @@
+class Genre < ActiveRecord::Base 
+    has_many :song_genres 
+    has_many :songs, through: :song_genres 
+    has_many :artists, through: :songs
+    
+      def slug 
+        self.name.gsub(/\s/, "-").downcase
+    end
+    
+    def self.find_by_slug(slug) 
+       self.all.each do |g|
+           if g.slug == slug 
+               return g 
+           end 
+       end
+   end
+end
+    
