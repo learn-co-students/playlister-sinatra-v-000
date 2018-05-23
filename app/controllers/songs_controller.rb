436767b33aac8abc83b33c4b257b97ee58d5a0d6
Sam. O <samrey2018@gmail.com>
2018-05-23 14:32:57 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,27 @@ class SongsController < ApplicationController
       erb :'/songs/index'
     end
 
+    get '/songs/new' do
+       @genres = Genre.all
+       @artists = Artist.all
+      erb :'/songs/new'
+    end
+
+
+
+   post '/songs' do
+ #create a new artist
+ @song = Song.create(params[:Name])
+   if !params[:artist][:name].empty?
+     @song.artists << Artist.create(name: params[:artist][:name])
+   end
+   if !params[:song][:genre_ids].empty?
+        @song.genre_ids = params[:song][:genre_ids]
+   end
+       @song.save
+    redirect to "/songs/:slug"
+
+    end
 
     get '/songs/:slug' do
      @song = Song.find_by_slug(params[:slug])  # slug helps to find by name instaed of ID
@@ -16,4 +37,4 @@ class SongsController < ApplicationController
      erb :'/songs/show'
     end
 
-end
+end #end for class method
