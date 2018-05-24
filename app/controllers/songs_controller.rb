f562a8da5f8faec1773001e3a79bef0f7c64d03f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-24 20:52:16 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,16 +5,18 @@ class SongsController < ApplicationController
     erb :"songs/index"
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :"songs/new"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :"songs/show"
   end
 
-  get '/songs/new' do
-    erb :"songs/new"
-  end
-
   post '/songs' do
+    binding.pry
     @song = Song.new(name: params[:name], artist: params[:artist], genre: params[:genre])
   end
 end
