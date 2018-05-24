92c85cb4023c035cb7f2c894ce199ea6b0754bc7
Sam. O <samrey2018@gmail.com>
2018-05-24 18:28:02 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,9 @@ class SongsController < ApplicationController
    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params["song"]["genre_ids"]
 
-   if !params[:song][:genre_ids].empty?
+   if !params["song"]["genre_ids"].empty?
+     binding.pry
+
         @song.genre_ids = params[:song][:genre_ids]
    end
        @song.save
