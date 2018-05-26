0d06a22bfd9e9248f0766c591dc86c682b0f2595
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-26 02:02:19 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,13 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split("-").map(&:capitalize).join(' ')
+    self.find_by(:name => name)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.split("-").map(&:capitalize).join(' ')
+    self.find_by(:name => name)
+    binding.pry
+  end
+
+
 end
