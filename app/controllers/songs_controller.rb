1a7cae0e5a122bb3a94a3baf4530be4f194855ce
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-26 23:22:25 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,25 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+
+    if !params["artist"]["name"].empty?
+      @song.artist = Artist.create(name: params["artist"]["name"])
+    end
+
+    @song.save
+    redirect to "songs/#{@song.id}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :"songs/show"
   end
+
+
 end
