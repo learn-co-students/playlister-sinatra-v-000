b061e6c6f3cd73dffd6949719353726b8c1baa4d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-26 05:11:18 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
-artist1 = Artist.create(name: "Artist1")
-genre1 = Genre.create(name: "Genre1")
-genre2 = Genre.create(name: "genre2")
-song1 = Song.create(name: "song1", artist: artist1, genres:[genre1,genre2])
-#song1.genres << genre1
-song2 = Song.create(name: "song1a", artist: artist1)
-#song2.genres << genre1
-artist2 = Artist.create(name: "Artist2")
-song3 = Song.create(name: "song3", artist: artist2)
-song4 = Song.create(name: "song4", artist: artist2)
-#song3.genres << genre1
-#song4.genres << genre1
-#song4.genres << genre2
+require_relative "../lib/library_parser"
+LibraryParser.new.call
