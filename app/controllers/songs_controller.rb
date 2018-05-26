c3efa58c895b59fb3a5e458ce742f46a3b4192fe
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-26 05:04:57 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,16 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.update(params)
+    @song.name = params[:song_name]
+    if @song.artist.name != params[:artist_name]
+      artist = Artist.find_by(name: params[:artist_name])
+      artist = Artist.create(name: params[:artist_name]) if !artist
+      @song.artist = artist
+    end
+    @song.genres.clear
+    @params[:genres].each do | genre |
+      @song.genres.push (Genre.find(genre))
+    end
     @song.save
     flash[:message] = "Successfully updated song."
     redirect :"/songs/#{@song.slug}"
