a120b7a0e339f1270ec3e91bd652984c737c2452
goatcheese <nonsquishy@gmail.com>
2018-05-28 14:31:02 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
+  get "/" do
     erb :index
   end
 end
