03854c3dcce27c9ab1690ceb4d123d52e0c2b71d
suzy baldwin <gbaldwin24@comcast.net>
2018-05-31 15:52:03 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,18 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.find do |song|
+      slug == song.slug
+    end
+  end
+
+
+end
