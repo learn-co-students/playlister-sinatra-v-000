fc76643b9bbb3ea8fb4d12e72b9c19a3bba239b8
Victoria Meng <nights1ststar@protonmail.com>
2018-06-03 03:51:24 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,8 +3,26 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    @artist = Artist.find(@song.artist_id)
     erb :'songs/show'
   end
+
+  post '/songs/new' do
+    # params.delete(:captures)
+    binding.pry
+    @song = Song.create(name: params[:song_name])
+    @artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.artist_id = @artist.id
+    @song.genre_ids = params[:genres]
+    @song.save
+    binding.pry
+    redirect "/songs/#{@song.slug}"
+  end
+
 end
