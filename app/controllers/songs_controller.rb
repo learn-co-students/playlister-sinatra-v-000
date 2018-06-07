387f01c5df47ae89d0434184ce630ee19d1da620
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-07 21:16:51 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,28 @@
 class SongsController < ApplicationController
-  get '/songs' do
-    @songs = Song.all
-    erb :'/songs/index'
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @artist = Artist.find_by(name: params[:artist][:name])
+    if !@artist
+      @artist = Artist.create(params[:artist])
+    end
+    @artist.songs << @song
+    redirect "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
   end
 
 end
