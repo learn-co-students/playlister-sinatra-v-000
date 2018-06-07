11ed23c426a6e50049609a9364efce0657b0eee9
Vanessa Toro <vanetoro84@gmail.com>
2018-06-07 17:46:07 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,7 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @genres = @song.song_genres
     @artist = @song.artist
-    binding.pry
+    # binding.pry
     erb :'/songs/show'
   end
 
