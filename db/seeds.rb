823e153d2b94084614750d012f81339320635a2f
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-07 02:24:43 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,2 @@
 # Add seed data here. Seed your database with `rake db:seed`
-ed_s = Artist.create(name: "Ed Sheeran")
-pop = Genre.create(name: "Pop")
-song = Song.create(name: "Perfect")
+LibraryParser.parse
