817dbf43c2432a7836a17e695b4bf8a1749f46fb
Vanessa Toro <vanetoro84@gmail.com>
2018-06-08 13:41:40 +0000

    Done.

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -4,4 +4,11 @@ class GenreController < ApplicationController
     @genres = Genre.all
     erb :'/genres/index'
   end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    @artists = @genre.artists
+    @songs = @genre.songs
+    erb '/genres/show'
+  end
 end
