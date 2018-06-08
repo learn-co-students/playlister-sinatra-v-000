435c95ac3e9d6540c392771bc0dcdf1ae6c3ff12
Vanessa Toro <vanetoro84@gmail.com>
2018-06-08 19:04:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
@@ -7,5 +9,5 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  
+
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,7 @@
-class SongsController < ApplicationController
+require 'rack-flash'
 
+class SongsController < ApplicationController
+  use Rack::Flash
 
 
   get '/songs' do
@@ -14,27 +16,27 @@ class SongsController < ApplicationController
     @genres = Genre.all
   erb :'/songs/new'
   end
+  get '/songs/:slug/edit' do
+
+  end
 
   get '/songs/:slug' do
       @song = Song.find_by_slug(params[:slug])
-      binding.pry
-      @genres = @song.song_genres
+      @genres = @song.genres
       @artist = @song.artist
       erb :'/songs/show'
     end
 
   post '/songs' do
     @song = Song.create(name: params[:name])
-    @artist = Artist.create(name: params[:artist])
-
     @genre = Genre.find(params[:genre])
-    @song.artist = @artist
     @song.genres << @genre
+    @artist = Artist.find_or_create_by(name: params[:artist])
+    @song.artist = @artist
     @artist.save
     @song.save
-    # binding.pry
+    flash[:message] = 'Successfully created song.'
     redirect("/songs/#{@song.slug}")
-
   end
 
 
