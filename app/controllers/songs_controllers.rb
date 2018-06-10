79d0830f088a99931cd2617a0c22a0d6942c6e46
Augusto Granitto <augustogranitto@gmail.com>
2018-06-10 21:43:18 +0000

    Done.

--- app/controllers/songs_controllers.rb
+++ app/controllers/songs_controllers.rb
@@ -30,11 +30,10 @@ end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'songs/edit'
   end
 
-  patch '/songs/:slug' do
+  post '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
@@ -44,4 +43,4 @@ end
     redirect("/songs#{song.slug}")
   end
 
-end 
+end
--- config.ru
+++ config.ru
@@ -1,8 +1,14 @@
 require './config/environment'
+require 'sinatra'
+
 
 if ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
+  raise 'Migrations are pending. Run &#96;rake db:migrate&#96; to resolve the issue.'
 end
 
 use Rack::MethodOverride
+
+use ArtistsController
+use GenresController
+use SongsController
 run ApplicationController
