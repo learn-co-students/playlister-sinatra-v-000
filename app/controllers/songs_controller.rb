27578ef72b1f7a34d267d1bcc538d2a7feb586ad
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-10 14:29:52 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,19 @@ class SongsController < ApplicationController
   enable :sessions
   register Sinatra::Flash
 
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
   get '/songs/new' do
     @genres = Genre.all
     erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
   end
 
   post '/songs' do
@@ -17,33 +27,21 @@ class SongsController < ApplicationController
     redirect ("/songs/#{@song.slug}")
   end
 
-  get '/songs' do
-    @songs = Song.all
-    erb :'/songs/index'
-  end
-
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
-  end
-
-  post '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    params[:song][:genre_ids].each do |genre_id|
-      @song.genres << Genre.find_by_id(genre_id)
-    end
-    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    @song.save
-    flash[:message] = "Successfully updated song."
-    redirect ("/songs/#{@song.slug}")
-  end
-
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     @genres = Genre.all
     erb :'/songs/edit'
   end
 
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.genre_ids = params[:genres]
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect ("/songs/#{@song.slug}")
+  end
 
 
 end
