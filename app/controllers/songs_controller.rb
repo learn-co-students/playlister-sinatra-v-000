48e9e9d609aeda9429363933be6fad68146b878f
Petya Djanovska <petya.djanovska@gmail.com>
2018-06-12 18:06:30 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,13 +5,21 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @artist = Artist.create(name: params['artist_name'])
+    @song = Song.create(name: params['song_name'])
+    @artist.songs << @song
+    redirect "/songs/#{@song.id}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
-  end
-
-  get '/songs/new' do
-    erb :'/songs/new'
   end
 
 end
