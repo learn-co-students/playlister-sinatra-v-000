882be99894ab74b0903826bb3106b0952221d57c
David Markel <davidgmarkel@gmail.com>
2018-06-13 05:26:01 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,15 +18,15 @@ class SongsController < ApplicationController
     @song = Song.create(name: params[:song_name])
     genres = Genre.find(params[:genres])
     genres.each {|genre| @song.genres << genre}
-    if !params[:artist_name].empty? && !params[:artist_id].empty?
+    #if Artist.all.include?(params)
+    if !params[:artist_name].empty? && params[:artist_id]
       redirect to '/songs/new'
-    elsif !params[:artist_name].empty?
+    elsif !params[:artist_name].empty? && !Artist.all.detect {|artist| artist.name == params[:artist_name]}
       @song.artist_id = Artist.create(name: params[:artist_name]).id
-      @song.save
     else
       @song.artist_id = params[:artist_id]
-      @song.save
     end
+    @song.save
     flash[:message] = "Successfully created song."
     redirect to "/songs/#{@song.slug}"
   end
@@ -39,6 +39,21 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    genres = Genre.find(params[:genres])
+    genres.each {|genre| @song.genres << genre if !@song.genres.include?(genre)}
+    @song.name = params[:song_name]
+    if params[:artist_id] != nil
+      @song.artist_id = params[:artist_id]
+    elsif params[:artist_name] != ""
+      @song.artist_id = Artist.create(name: params[:artist_name]).id
+    end
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
