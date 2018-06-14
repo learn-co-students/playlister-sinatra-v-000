817dedd704c9ce5ebe02a176a97d49472cbd6e35
Michael Kang <michaelkang85@gmail.com>
2018-06-14 21:49:03 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,10 @@ class GenresController < ApplicationController
     @genres = Genre.all
     erb :'/genres/index'
   end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    #binding.pry
+    erb :'/genres/show'
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,21 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
     get '/songs/:slug' do
       @song = Song.find_by_slug(params[:slug])
       erb :'/songs/show'
+    end
+
+    post '/songs' do
+      if !params["owner"]["name"].empty?
+      @song.artist = Song.create(name: params["song"]["name"])
+    end
+      @song.save
+      redirect '/songs/:slug'
     end
 
 end
