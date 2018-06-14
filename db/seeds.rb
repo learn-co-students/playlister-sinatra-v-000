6ad4175cf0aa781d4d3f15dd409e61194edc1cb6
eyi1 <ej7729@gmail.com>
2018-06-14 15:32:01 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
 
-yeezy = Artist.create(name: "Kanye West")
-miley = Artist.create(name: "Miley Cyrus")
+Artist.create(:name => "Kanye West")
+Artist.create(name: "Miley Cyrus")
 
+Song.create(name: "Famous")
+Song.create(name: "Ghost Town")
+Song.create(name: "Wrecking Ball")
 
-famous = Song.create(name: "Famous")
-ghost_town = Song.create(name: "Ghost Town")
-wrecking_ball = Song.create(name: "Wrecking Ball")
-
-
-
-rap = Genre.create("rap")
-pop = Genre.create("pop")
-hip_hop = Genre.create("hip hop")
+Genre.create("rap")
+Genre.create("pop")
+Genre.create("hip hop")
