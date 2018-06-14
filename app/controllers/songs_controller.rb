3ea5a806fab0218404651bea73bb0b879e179753
Zac Willmington <zac.willmington@gmail.com>
2018-06-14 15:52:04 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,6 @@ class SongController < ApplicationController
   end
 
   get '/songs/new' do
-    #    binding.pry
        @genres = Genre.all
       erb :'songs/new'
   end
@@ -20,20 +19,23 @@ class SongController < ApplicationController
           @artist.songs << @song
       elsif !@artist.songs.include?(@song)
          @artist.songs << @song
+
       end
 
       @genres = Genre.find(params['genres'])
       @song.genres << @genres
-       redirect to "/songs/#{@song.slug}"
+      flash[:message] = "Successfully created song."
+    #   erb :'songs/show'
+    redirect to "songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
+
      @song = Song.find_by_slug(params[:slug])
      erb :'songs/show'
  end
 
 get '/songs/:slug/edit' do
-     binding.pry
      @genres = Genre.all
      @song = Song.find_by_slug(params[:slug])
      erb :'songs/edit'
@@ -49,7 +51,16 @@ get '/songs/:slug/edit' do
       else
           @song.artist = @artist
       end
+      @genre = Genre.find(params['genres'])
+      ###check if the params holds the the ids and delete if they are not present in params but are in the hash-pramas
+      binding.pry
+      if !@song.genres.include?(@genre)
+          @song.genres << @genre
+          binding.pry
+      end
       @song.save
-      redirect to "/songs/#{@song.slug}"
+       flash[:message] = "Successfully updated song."
+       redirect to "songs/#{@song.slug}"
+    #    erb :'songs/show'
   end
 end
--- config.ru
+++ config.ru
@@ -1,10 +1,13 @@
 require './config/environment'
+require 'rack/flash'
 
 # if ActiveRecord::Migrator.needs_migration?
 #   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 # end
 
 # use Rack::MethodOverride
+
+# use Rack::Flash, :accessorize => [:message]
 run ApplicationController
 use ArtistController
 use GenreController
