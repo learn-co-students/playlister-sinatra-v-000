73b2411d2fdc9f9a7be3e4cce705e00348bc931b
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-20 20:41:52 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,13 +37,13 @@ class SongsController < ApplicationController
   end
 
 
-  patch 'songs/:slug' do
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params["genres"]
     @song.save
     flash[:message] = "Successfully updated song."
-    redirect "/songs/#{@song.slug}"
+    redirect("/songs/#{@song.slug}")
   end
 end
