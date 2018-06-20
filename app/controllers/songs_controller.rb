c7a6eff2f0bc4611e55157407650abf044f041a9
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-20 18:41:40 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,15 +5,17 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.artist = Artist.find_by_id(@song.artist_id)
     erb :'/songs/show'
   end
 
-  get '/songs/new' do
-
-  end
 
   post '/songs' do
 
