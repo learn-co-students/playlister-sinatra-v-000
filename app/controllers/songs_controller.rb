5cb5f0682fd1244f9441ea8712ebaaf9475effa5
Sara Stanton <sara.mpa13@gmail.com>
2018-06-21 04:55:16 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,51 +1,50 @@
 class SongsController < ApplicationController
 
-    use Rack::Flash
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
 
-    get '/songs' do
-        @songs = Song.all
-        erb :'/songs/index'
-    end
+  get '/songs/new' do
+    @songs = Song.all
+    @artists = Artist.all
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
 
-    get '/songs/new' do
-        @artists = Artist.all
-        @genres = Genre.all
-        erb :'/songs/new'
-    end
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
 
-    get '/songs/:slug' do
-        @song = Song.find_by_slug(params[:slug])
-        erb :'/songs/show'
+  post '/songs' do
+    @song = Song.create(params['song'])
+    if !params["artist"]["name"].empty?
+      @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
     end
+    @song.genre_ids = params['genre']
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
-    post '/songs' do
-        @song = Song.create(params['song'])
-        if !params['artist']['name'].empty?
-            @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
-        end
-        @song.genre_ids = params['genre']
-        @song.save
-        flash[:message] = "Successfully created song."
-        redirect "/songs/#{@song.slug}"
-    end
+  get '/songs/:slug/edit' do
+    @songs = Song.all
+    @artists = Artist.all
+    @genres = Genre.all
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
 
-    get '/songs/:slug/edit' do
-        @artists = Artist.all
-        @genres = Genre.all
-        @song = Song.find_by_slug(params[:slug])
-        erb :'/songs/edit'
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    if !params["artist"]["name"].empty?
+      @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
     end
-
-    patch '/songs/:slug' do
-        @song = Song.find_by_slug(params[:slug])
-        @song.update(params['song'])
-        if !params['artist']['name'].empty?
-            @song.artist = Artist.find_or_create_by(name: params['artist']['name'])
-        end
-        @song.genre_ids = params['genre']
-        @song.save
-        flash[:message] = "Successfully updated song."
-        redirect "/songs/#{@song.slug}"
-    end
+    @song.genre_ids = params[:genre]
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
 end
