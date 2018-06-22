8ac934c9c18b8dfbc1befa14efacd4de250f030f
Suchitra Naik <susanaik@gmail.com>
2018-06-22 12:39:19 +0000

    Done.

--- db/migrate/20180622121930_create_songs.rb
+++ db/migrate/20180622121930_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration
     create_table :songs do |t|
       t.string :name
       t.integer :artist_id
-      t.integer :genre_ids
+      t.integer :genre_id
     end
   end
 end
--- db/migrate/20180622121954_create_song_genres.rb
+++ db/migrate/20180622121954_create_song_genres.rb
@@ -2,7 +2,7 @@ class CreateSongGenres < ActiveRecord::Migration
   def change
     create_table :song_genres do |t|
       t.integer :song_id
-      t.integer :genre_ids
+      t.integer :genre_id
     end
   end
 end
