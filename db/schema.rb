c8bad1b667e4e20e26d95394e1ccc6af442a0f50
Karl Amoguis <ktamoguis@gmail.com>
2018-06-23 22:33:16 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623211921) do
+ActiveRecord::Schema.define(version: 20180623222935) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
