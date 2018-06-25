c931597d5da177bbb950d6b29e53593dbb10821f
Karl Amoguis <ktamoguis@gmail.com>
2018-06-25 22:13:48 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,5 +8,20 @@ class SongsController < ApplicationController
   end
 
 
+  get '/songs/new' do
+    #binding.pry
+
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+
+    binding.pry
+
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'/songs/params[:slug]'
+  end
+
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,8 @@ class Song < ActiveRecord::Base
 
   def self.find_by_slug(slug)
     reverse_slug = slug.gsub("-"," ").split.map{|w| w.capitalize}.join(" ")
+    binding.pry
     self.all.find_by(name: reverse_slug)
   end
-  
+
 end
