df58b00dc1e2702401d41ee424badf7fe6d239d4
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-26 16:07:04 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,15 @@ class Artist <ActiveRecord::Base
   has_many :song_genres
   
   def slug 
-    self.name.gsub!(/\s/,'-')
+    self.name.gsub(/\s/,'-').downcase
   end 
+  
+  def self.find_by_slug(slug)
+    Artist.all.each do |s|
+      if s.slug == slug 
+        return s
+      end
+    end
+  end
   
 end
