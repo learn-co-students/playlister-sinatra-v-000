d39b8a433237534bafb56549d11943a5d9ceca0a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-26 17:40:05 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,9 +5,17 @@ class ArtistsController < ApplicationController
     erb :'artists/index'
   end
   
+  
+  post '/artists' do 
+    @artist = Artist.create(name: params[:name])
+    redirect to '/artists/#{@artist.slug}'
+  end
+  
   get '/artists/:slug' do
-    
+    @artist = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
   end
+  
+  
   
 end
