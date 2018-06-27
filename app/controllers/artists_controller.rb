834a1221e0cfdb4d2554ff8b3d7a754c464b0c4e
Alex Zhang <alexgzhang92@gmail.com>
2018-06-27 22:47:42 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,11 @@
-class ArtistsController < ApplicationController
+class ArtistController < ApplicationController
+  get '/artists' do
+    @artists = Artist.all
+    erb :'artists/index'
+  end
+  
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
+  end
 end
