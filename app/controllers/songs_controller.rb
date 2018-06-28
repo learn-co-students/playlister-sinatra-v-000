9498e2f2deebfa9233120506b5cac83fa9405ad1
netzay <netzelally@gmail.com>
2018-06-27 19:13:57 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,7 @@ class SongsController < ApplicationController
 
   	get '/songs/:slug' do
     	@song = Song.find_by_slug(params[:slug])
-    	erb :'songs/show'
+    	erb :'/songs/show'
   	end
 
   	get '/songs/:slug/edit' do 
@@ -20,7 +20,6 @@ class SongsController < ApplicationController
   	end
 
   	post '/songs' do
-
     	@song = Song.create(name: params["Name"])
     	@song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     	@song.genre_ids = params[:genres]
@@ -31,10 +30,11 @@ class SongsController < ApplicationController
 
   	patch '/songs/:slug' do
     	@song = Song.find_by_slug(params[:slug])
+      @song.update(params[:slug])
     	@song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     	@song.genre_ids = params[:genres]
-    	@song.update(name: params["Name"], name: params["Artist Name"], name: params["genre"])
-    	flash[:message] = "Successfully updated song."
+    	@song.save
+      flash[:message] = "Successfully updated song."
   		redirect to("/songs/#{@song.slug}")
   	end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -5,12 +5,12 @@ class Song < ActiveRecord::Base
 
  
 	def slug
-		slug = self.name.gsub(" ","-").downcase
+		name.downcase.gsub(" ","-")
 	end
 	
 
 	def self.find_by_slug(slug)
-		self.all.find {|t| t.slug == slug}
+		Song.all.find {|t| t.slug == slug}
 	end
 
 
