c651bdafb562bcb3024a7c39b21fb489234bdfb9
Isaac Villicana <villicana.i@gmail.com>
2018-06-28 21:03:51 +0000

    Done.

---
+++ app/models/artist.rb
@@ -0,0 +1,13 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def slug
+    self.name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(arg)
+    Artist.all.find{|artist| artist.slug == arg}
+  end
+
+end
