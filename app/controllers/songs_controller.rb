bd7c64fb2890cc3d626358dd4ef8453780ba8e6c
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-07-03 09:51:00 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -11,9 +15,14 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(name: params['Name'], genre_ids: params["genres"])
+    # why does it work with genre_ids? is this the best way?
     @song.artist = Artist.find_or_create_by(name: params['Artist Name'])
     @song.save
-    redirect "/songs/#{@song.slug}"
+    # binding.pry
+    flash[:message] = "Successfully created song."
+
+    # redirect works without flash but not with - is flash set up correctly?
+    redirect to "/songs/#{@song.slug}"
   end
 
    get '/songs/:slug' do
@@ -24,9 +33,26 @@ class SongsController < ApplicationController
    end
 
   get '/songs/:slug/edit' do
-    erb :'/songs/show'
+    @song = Song.find_by_slug(params[:slug])
+    # @song.artist = Artist.find_or_create_by(name: params['Artist Name'])
+    # @song.save
+
+    erb :'/songs/edit'
   end
 
+  patch '/songs/:slug' do
+
+    # puts params
+    @song = Song.find_by_slug(params[:slug])
+    # binding.pry
+    @song.artist = Artist.find_or_create_by(name: params['Artist Name'])
+    @song.genre_ids = params["genres"]
+    # Be able to change everything about a song, including the genres associated with it and its artist.
+    # binding.pry
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
 
 end
