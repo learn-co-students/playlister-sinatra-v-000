b78fbe0c95eff2e635be1551e5534901d45890b4
taryn yaeger <tsyaeger@gmail.com>
2018-07-06 18:12:20 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,39 +7,30 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
   
-  
-  
-  post '/songs' do 
-    
-    song = Song.create(:name => params[:name])
-    # genre = Genre.find_or_create_by(:name => genre)
-    
-    genres = []
-    params[:genres].each do |genre|
-      genres << Genre.find_or_create_by(:name => genre)
-      song.song_genres.build(:genre => genre)
-    end 
-    
-    artist = Artist.find_or_create_by(:name => params[:artist_name])
-
-    # song.song_genres.build(:genre => genre)
-    song.artist = artist
-    
-    # binding.pry
-    #if artist exists
-    #create new song and associate it
-    
-    redirect to '/songs/#{song.slug}'
-  end
-
-
-
 
   get '/songs/new' do
     @genres = Genre.all
     @artists = Artist.all 
     erb :'songs/new'
   end
+  
+  
+  post '/songs' do 
+    song = Song.create(:name => params[:song_name])
+    
+    params[:genres].each do |genre|
+      genre = Genre.find(genre)
+      song.song_genres.build(:genre => genre).save
+    end 
+    
+    artist = Artist.find_or_create_by(:name => params[:artist_name])
+    song.artist = artist 
+    
+    song.save
+    redirect to '/songs/#{song.slug}'
+  end
+    
+    
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
