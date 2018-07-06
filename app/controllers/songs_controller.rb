145190e11071a278754a2347399013d765b26991
taryn yaeger <tsyaeger@gmail.com>
2018-07-06 22:21:06 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,45 @@
+class SongsController < ApplicationController
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    @artists = Artist.all
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+
+    @song = Song.find_by_slug(params[:slug])
+    # binding.pry
+    erb :'/songs/show'
+  end
+
+  post '/songs' do
+    # binding.pry
+    @song = Song.create(:name => params[:name])
+    params[:genres].each do |genre|
+      genre = Genre.find(genre)
+      @song.song_genres.build(:genre => genre).save
+    end
+
+    artist = Artist.find_or_create_by(:name => params[:artist_name])
+    @song.artist = artist
+
+    @song.save
+    flash[:notice] = "Successfully created song."
+    
+    redirect "/songs/#{@song.slug}"
+  end
+  
+  
+  # get '/songs/:slug/edit'
+  #   erb :'/songs/edit'
+  # end
+
+end
---
+++ app/models/aaslugifiable.rb
@@ -0,0 +1,23 @@
+
+module Slugifiable
+
+  module InstanceMethods
+
+    def slug
+      # binding.pry
+      self.name.gsub(" ", "-").gsub(/[^\w-]/, '').downcase
+      # binding.pry
+    end
+
+  end
+
+
+  module ClassMethods
+
+    def find_by_slug(value)
+      self.all.detect{ |a| a.slug == value}
+    end
+
+  end
+
+end
