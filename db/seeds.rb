df47c6f675d5d6e907e4c66ced787b124b84aeed
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-09 00:28:03 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+# artist1 = Artist.create(name: "Artist Name")
+# hello = Song.create(name: "Hello", owner: artist1)
+# world = Song.create(name: "World", owner: artist1)
+# rap = Genre.create(name: "rap")
+LibraryParser.new.call
