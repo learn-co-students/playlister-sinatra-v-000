4f8a96282ac6a34a583468d7da2320c1ef57bfde
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-13 20:40:35 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
-
+require_relative "concerns/slugifiable.rb"
 class Genre < ActiveRecord::Base
+   extend Slugifiable::ClassMethods
+   include Slugifiable::InstanceMethods
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,9 @@
-
+require_relative "concerns/slugifiable.rb"
 class Song < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+
   has_many :song_genres
   has_many :genre, through: :song_genres
+  belongs_to :artist
 end
