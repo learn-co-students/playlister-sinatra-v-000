83603acfa5d2023f30a2866b653fe85679917ce5
Shirley Collins <shirley@m104.us>
2018-07-16 09:44:41 -0700

    passes tests for adding new songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class SongController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -6,9 +9,27 @@ class SongController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
 
     erb :'/songs/show'
+  end
+
+  post '/songs' do
+    @artist = Artist.find_or_create_by(:name => params[:artist_name])
+    @new_song = Song.create(:name => params[:song_name], :artist => @artist)
+    @new_song.genre_ids = params[:genre_ids]
+    @new_song.save
+
+    # https://github.com/nakajima/rack-flash
+    flash[:message] = "Successfully created song."
+
+    redirect "/songs/#{@new_song.slug}"
   end
 end
