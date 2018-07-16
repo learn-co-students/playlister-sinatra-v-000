925134da4f87ef06b7e1f291fd647cf1b78f773d
Shirley Collins <shirley@m104.us>
2018-07-16 10:59:34 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
+gem 'tux'
 
 group :test do
   gem 'rspec'
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,4 +32,22 @@ class SongController < ApplicationController
 
     redirect "/songs/#{@new_song.slug}"
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :"/songs/edit"
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params[:song_name]
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+
+    redirect "/songs/#{@song.slug}"
+  end
 end
