2952763d2edfa40220ca85b3852669b2fe404c55
haydenbetts <haydenbetts@gmail.com>
2018-07-16 18:08:36 -0700

    working on new song form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,6 +7,7 @@ class SongsController < ApplicationController
 
     get "/songs/new" do
         @artists = Artist.all
+        @genres = Genre.all
         erb :"songs/new"
     end
 
@@ -14,6 +15,26 @@ class SongsController < ApplicationController
         # currently possible to have duplicate slugs
         @song = Song.all.detect {|song| song.slug == params[:slug]}
         erb :"songs/show"
+    end
+
+    post "/songs" do
+       
+        @song = Song.create(name: params["song"]["name"])
+       
+        params["song"]["genre_ids"].each do |genre_id|
+            @song.genres << Genre.find(genre_id.to_i)
+        end
+
+        if params["artist"]["name"]
+            @artist = Artist.create(name: params["artist"]["name"])
+            @song.artist = @artist
+        else
+            @artist = Artist.find(params["song"]["artist_id"])
+            @song.artist = @artist
+        end
+
+        @song.save
+        redirect "/songs/#{@song.slug}"
     end
 
 end
