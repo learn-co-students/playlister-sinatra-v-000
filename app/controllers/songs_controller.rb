7804accc80cc6961011a0014da9feef595cadfbc
Frank McCallion <fkmccallion@gmail.com>
2018-07-17 13:45:13 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,29 +1,55 @@
+
+
 class SongsController < ApplicationController
+
 
   get '/songs' do
     @songs = Song.all
-
-    erb :'songs/index'
+    erb :'/songs/index'
   end
 
   get '/songs/new' do
-    @artist = Artist.all
-
-    erb :'songs/new'
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @artist = Artist.find(@song.artist_id)
-
-    songgenre = SongGenre.find_by song_id: @song.id
-    @genre = Genre.find(songgenre.genre_id)
+    @artist = @song.artist
 
     erb :'songs/show'
   end
 
-  post 'songs/new' do
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    if params["Genre Name"] = ""
+      @song.genre_ids = params[:genres]
+    else
+      new_genre = Genre.find_or_create_by(:name => params["Genre Name"])
+      @song.genre_ids = new_genre.id
+    end
+    @song.save
 
+
+
+
+    redirect("/songs/#{@song.slug}")
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+
+    redirect("/songs/#{@song.slug}")
   end
 
 end
