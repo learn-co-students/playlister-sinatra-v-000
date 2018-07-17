26e0396dbbdddb215bc14231495edbd45a8e3c57
haydenbetts <haydenbetts@gmail.com>
2018-07-16 20:11:02 -0700

    rack flash working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
+#require 'rack-flash3'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable :sessions
 
   get '/' do
     erb :index
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+    use Rack::Flash
 
     get "/songs" do
         @songs = Song.all
