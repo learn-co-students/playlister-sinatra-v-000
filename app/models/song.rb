c34ed1aa4c1e75a3ad8463a95d8e4cee32400fa9
Carrie Savinell <carrielane89@gmail.com>
2018-07-19 01:33:36 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,14 @@
 class Song < ActiveRecord::Base
 
  belongs_to :artist
- has_many :genres, through: :song_genres
  has_many :song_genres
+ has_many :genres, through: :song_genres
 
+ def slug
+   @name.downcase.gsub(" ", "-")
+ end
+
+ def find_by_slug()
+ end
 
 end
--- app/models/song_genre.rb
+++ app/models/song_genre.rb
@@ -1,6 +1,6 @@
-class Song_Genre < ActiveRecord::Base
+class SongGenre < ActiveRecord::Base
 
   belongs_to :song
-  belongs_to :genre 
+  belongs_to :genre
 
 end
