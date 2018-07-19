df4eb685426a1b451c7d84c29b7ec6fddd71ed28
Shirley Collins <shirley@m104.us>
2018-07-19 10:03:36 -0700

    empty commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+
+
   get '/' do
     erb :index
   end
