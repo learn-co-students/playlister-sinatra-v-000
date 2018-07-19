901a1020ce07870991f2ba0532be817db7f88b92
Maverick Tesini <mavericktesini@gmail.com>
2018-07-19 20:12:25 +0000

    Done.

---
+++ db/migrate/20180719024844_create_song_genre_table.rb
@@ -0,0 +1,9 @@
+class CreateSongGenreTable < ActiveRecord::Migration
+  def change
+    create_table :song_genres do |t|
+      t.integer :genre_id
+      t.integer :song_id
+    end
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV["SINATRA_ENV"] = "test"
 
 require_relative '../config/environment'
+require 'rack/flash/test'
 require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
