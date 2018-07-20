40e834b0ccc7bbf50d8a6aa1ba632aa5597d41d5
Elaine Anderson <eparie@bu.edu>
2018-07-20 17:44:18 +0000

    saftey

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,17 +5,32 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params["slug"])
-    erb :'songs/show'
-  end
 
   get '/songs/new' do
-
+    @songs = Song.all
+    @genres = Genre.all
     erb :'songs/new'
   end
 
   post '/songs' do
-    binding.pry
+    if Artist.find_by(name: params["Artist Name"])
+      @artist = Artist.find_by(name: params["Artist Name"])
+      @song = Song.create(:name => params["Name"], :artist_id => @artist.id)
+    else
+      @artist = Artist.create(:name => params["Artist Name"])
+      artist_id = @artist.id
+      @song = Song.create(:name => params["Name"],:artist_id => artist_id)
+      SongGenres.create(:song_id => @song.id)
+    end
+    @song.save
+    redirect to "songs/#{@song.slug}"
   end
+
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -12,11 +12,8 @@ class Song < ActiveRecord::Base
   end
 
   def self.find_by_slug(slug)
-  song_object = self.all.find do |song|
-    d_song = song.name.downcase
-    song_name = slug.split('-')
-  song_name = song_name.join(" ")
-      d_song == song_name
+  self.all.find do |song|
+  song.slug == slug
     end
   end
 
