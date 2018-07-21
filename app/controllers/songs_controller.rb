7e09bec88809d1ca4666c3c6f0c001123437e5f1
Elaine Anderson <eparie@bu.edu>
2018-07-21 12:10:52 +0000

    saftey

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,19 +13,24 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    if Artist.find_by(name: params["Artist Name"])
+    if !!Artist.find_by(name: params["Artist Name"])
       @artist = Artist.find_by(name: params["Artist Name"])
       @song = Song.create(:name => params["Name"], :artist_id => @artist.id)
+    #  @genre = Genre.find_by(name: params["Genre Name"])
+      @songgenre = SongGenre.create(:song_id => @song.id, :genre_id => @genre.id)
+    #  @songgenre.save
+      redirect to "/songs/#{@song.slug}"
     else
       @artist = Artist.create(:name => params["Artist Name"])
       artist_id = @artist.id
       @song = Song.create(:name => params["Name"],:artist_id => artist_id)
-      SongGenres.create(:song_id => @song.id)
+    #  @genre = Genre.find_all(name: params["Genre Name"])
+      #@song.genres = @genre
+    #  SongGenre.create(:song_id => @song.id, :genre_id => @genre.id)
+      redirect to "/songs/#{@song.slug}"
     end
     @song.save
-    redirect to "songs/#{@song.slug}"
   end
-
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
