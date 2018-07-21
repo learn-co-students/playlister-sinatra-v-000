d6769d483937fe9b79958d305b276fe02488a734
Kayla Reopelle <kreopelle@gmail.com>
2018-07-21 03:53:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
   register Sinatra::ActiveRecordExtension
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
--- config.ru
+++ config.ru
@@ -7,5 +7,5 @@ end
 use Rack::MethodOverride
 use SongsController
 use ArtistsController
-use GenresController 
+use GenresController
 run ApplicationController
