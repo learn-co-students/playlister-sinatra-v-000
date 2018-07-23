44100122065b4fb219d2128cfe04c4bf2c21c892
Elaine Anderson <eparie@bu.edu>
2018-07-23 18:46:24 +0000

    saftey

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,8 @@ class SongsController < ApplicationController
     if !!Artist.find_by(name: params["Artist Name"])
       @artist = Artist.find_by(name: params["Artist Name"])
       @song = Song.create(:name => params["Name"], :artist_id => @artist.id)
-      @song.genres << Genre.find_by(name: params["Genre Name"])
+      @song.genres << Genre.find_by(id: params["Genre Name"])
+      @song.save
     #  @song.genres.each do |genre|
     #  @songgenre = SongGenre.create(:song_id => @song.id, :genre_id => genre.id)
     #  @songgenre.save
@@ -25,7 +26,7 @@ class SongsController < ApplicationController
       @artist = Artist.create(:name => params["Artist Name"])
       artist_id = @artist.id
       @song = Song.create(:name => params["Name"],:artist_id => artist_id)
-    #  @genre = Genre.find_all(name: params["Genre Name"])
+      @song.genres << Genre.find_by(id: params["Genre Name"])
       #@song.genres = @genre
     #  SongGenre.create(:song_id => @song.id, :genre_id => @genre.id)
       redirect to "/songs/#{@song.slug}"
@@ -33,10 +34,21 @@ class SongsController < ApplicationController
     @song.save
   end
 
+  get '/songs/:slug/edit' do
+      @song = Song.find_by_slug(params[:slug])
+      erb :'/songs/edit'
+    end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
+    erb :'/songs/show'
   end
+
+  post '/songs/:slug' do
+    erb :'/song/show'
+  end
+
+
 
 
 end
