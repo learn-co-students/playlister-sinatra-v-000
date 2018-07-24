2108e34e55c077303c67c37bc33c6e1f4add9c5b
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-24 01:39:10 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724012050) do
+ActiveRecord::Schema.define(version: 20180724013203) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -21,8 +21,14 @@ ActiveRecord::Schema.define(version: 20180724012050) do
     t.string "name"
   end
 
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
   create_table "songs", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "artist_id"
   end
 
 end
