4916a7d42b8f8981f13b42102b80ec17ef301997
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-24 18:53:20 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,8 @@
 #require 'slugifyer'
 
 class Artist < ActiveRecord::Base
-  #extend Slugifyer::ClassMethods
-  #include Slugifyer::InstanceMethods
+  extend Slugifyer::ClassMethods
+  include Slugifyer::InstanceMethods
   
   has_many :songs
   has_many :genres, through: :songs
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,8 @@
 #require 'slugifyer'
 
 class Genre < ActiveRecord::Base  
-  #extend Slugifyer::ClassMethods
-  #include Slugifyer::InstanceMethods
+  extend Slugifyer::ClassMethods
+  include Slugifyer::InstanceMethods
   
   has_many :song_genres
   has_many :songs, through: :song_genres
