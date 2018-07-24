b088d51cecfaffdf32ec507e3061e834960b7540
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-07-24 15:51:09 -0400

    Slugifyer module working in at least Artist class

--- app/concerns/slugifyer.rb
+++ app/concerns/slugifyer.rb
@@ -1,11 +1,14 @@
 module Slugifyer
-  
+
   module ClassMethods
-    def find_by_slug(name)
-      self.class.all.find { |instance| instance.slug == slug }
+    def find_by_slug(name_slug)
+      all.detect do |instance|
+        # binding.pry
+        instance.slug == name_slug
+      end
     end
   end
-  
+
   module InstanceMethods
     def slug
       name.downcase.strip.gsub(/\p{P}/, '').gsub(/\W+/, '-')
