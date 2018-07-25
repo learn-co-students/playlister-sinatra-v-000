f4c8ec96f8ae9dfac6efcd79bf530960e261178f
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-07-24 20:17:08 -0400

    added route and view for new songs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,4 @@
+
 class ArtistsController < ApplicationController
 
   get "/artists" do
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+
 class SongsController < ApplicationController
 
   get "/songs" do
@@ -5,9 +6,21 @@ class SongsController < ApplicationController
     erb :"/songs/index"
   end
 
+  get "/songs/new" do
+    binding.pry
+    @genres = Genres.all
+    erb :"/songs/new"
+  end
+
   get "/songs/:slug" do
     @song = Song.find_by_slug(params[:slug])
     erb :"songs/show"
+  end
+
+  post "/songs" do
+    binding.pry
+
+    redirect "/songs/:slug"
   end
 
 end
