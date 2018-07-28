543e086f34b6de4e9317fae99cc1a3b731f78332
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-28 16:46:27 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_many :song_genres
-  has_many :genres, :through => :song_genres
+  has_many :songgenres
+  has_many :genres, :through => :songgenres
 end
