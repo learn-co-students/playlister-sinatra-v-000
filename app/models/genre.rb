27c936f8ac4019a7b43eeb0008d7ea042a21353a
osxgirl <osxgirl@mac.com>
2018-07-28 17:32:37 -0400

    //❤️

---
+++ app/models/genre.rb
@@ -0,0 +1,9 @@
+class Genre < ActiveRecord::Base
+    has_many :songs, through: :song_genres
+    has_many :song_genres
+    has_many :artists, through: :songs
+    
+    include Slugifiable::InstanceMethods
+    extend Slugifiable::ClassMethods
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,6 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-
+require_relative '../app/models/concerns/slugifiable'
 require_all 'app'
 require_all 'lib'
