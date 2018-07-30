fc038408fd4108304cab743d236bd118491a9134
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-30 01:29:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,7 @@
 class SongsController < ApplicationController
 
 get '/songs' do
+  binding.pry
   @songs = Song.all
   erb :'/songs/index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "genre_id"
+    t.integer "song_id"
   end
 
   create_table "songs", force: :cascade do |t|
