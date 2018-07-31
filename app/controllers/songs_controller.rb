49424c49640083853292bebd2c96bdfa5335de3a
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-31 23:57:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,10 @@ get '/songs' do
   erb :'/songs/index'
 end
 
+get '/songs/new' do
+    erb :'/songs/new'
+  end
+
 get '/songs/:slug' do
   @song = Song.find_by_slug(params[:slug])
   erb:'/songs/show'
