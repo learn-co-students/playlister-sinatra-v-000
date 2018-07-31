660300224ce6424437773ca2f17ff2f07c8d25a1
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-31 20:30:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,19 @@ class ApplicationController < Sinatra::Base
    end
 
    get '/artists/:slug' do
-
+     @artist = Artist.find_by_slug(params[:slug])
+     @artistsongs = []
+     Song.all.each do |song|
+       if song.artist_id == @artist.id
+          @artistsongs << song.name
+       end
+     end
+     @genreartists = []
+     Genre.all.each do |song|
+       if genre.artist_id == @artist.id
+          @genreartists << genre.name
+       end
+     end
      erb :actualartist
    end
 
