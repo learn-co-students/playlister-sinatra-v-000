2ae64f589cb240a9c8ac9fcaf99dc5e92be70ff2
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-31 22:25:43 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,23 +11,55 @@ class SongsController < ApplicationController
 
     post '/songs' do
        @song = Song.create(name: params[:Name])
-       @song.artist = Artist.find_by_or_create_by(name: params["Artist Name"])
+       #binding.pry
+       @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+       params["genres"].each do |genre_id|
+         songgenre = Genre.find_by_id(genre_id.to_i)
+         @song.genres << songgenre
+       end
+       #@song.genres = Genre.find_by_id(params["genres"][0].to_i)
+       @song.save
 
-        @songgenre = SongGenre.create(song_id: @song.id, genre_id: params[:genres][0].to_i)
-        @genre = Genre.find_by_id(@songgenre.genre_id)
+      #  @songgenre = SongGenre.create(song_id: @song.id, genre_id: params[:genres][0].to_i)
+      #  @genre = Genre.find_by_id(@songgenre.genre_id)
        redirect to "/songs/#{@song.slug}"
      end
 
      get '/songs/:slug' do
+
        @song = Song.find_by_slug(params[:slug])
-       @artist = Artist.find_by_id(@song.artist_id)
+       @genre_names = ""
+       @song.genres.each do |genre1|
+        @genre_names << " #{genre1.name}"
+       end
+      # binding.pry
+       #@artist = Artist.find_by_id(@song.artist_id)
    #binding.pry
-       @songgenre = SongGenre.find_by_song_id(@song.id)
-       @genre = Genre.find_by_id(@songgenre.genre_id)
+       #@songgenre = SongGenre.find_by_song_id(@song.id)
+       #@genre = Genre.find_by_id(@songgenre.genre_id)
+
+       #<h4>Genre : <%= @song.genres.each do |genre1|%>
+        # <a href= "/genres/<%=genre1.slug%>"><%= genre1.name %></a></h4><%=end%>
        erb :'songs/show'
      end
 
+     get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
 
+  post '/pets/:id' do
+    #binding.pry
+    @song = Song.find_by_slug(params[:slug])
+  #binding.pry
+  @song.update(params[:pet])
+    #@pet.update(name: params[:pet][:name], owner_id: params[:pet][:owner_id])
+  #  if !params[:owner][:name].empty?
+  #    @pet.owner = Owner.create(name: params[:owner][:name])
+  #  end
+      @song.save
+    redirect to "songs/#{@song.slug}"
+  end
 
 
 
