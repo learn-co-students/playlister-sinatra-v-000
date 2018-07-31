44424e8463e2b2b28a540f64dfff99e4df94186e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-31 20:19:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,26 +26,38 @@ class ApplicationController < Sinatra::Base
 
   post '/songs' do
      @song = Song.create(name: params[:Name])
-     if !artist_name.empty?
+     if !params[:"Artist Name"].empty?
        @artist = Artist.create(name: params[:"Artist Name"])
        @song.artist_id = @artist.id
+       @song.save
      else
        @artist = Artist.find_by_id(params[:artist][:id])
        @song.artist_id = @artist.id
+       @song.save
      end
-      @songgenre = SongGenre.find_by_song_id(@song.id)
-      @genre = Genre.find_by_id(params[:genre][0].to_i)
-      @songgenre.genre_id = @genre.id
-     binding.pry
+
+      @songgenre = SongGenre.create(song_id: @song.id, genre_id: params[:genres][0].to_i)
+      @genre = Genre.find_by_id(@songgenre.genre_id)
      redirect to "/songs/#{@song.slug}"
    end
 
    get '/songs/:slug' do
      @song = Song.find_by_slug(params[:slug])
      @artist = Artist.find_by_id(@song.artist_id)
+ binding.pry
      @songgenre = SongGenre.find_by_song_id(@song.id)
      @genre = Genre.find_by_id(@songgenre.genre_id)
-     erb :actual_song
+     erb :actualsong
+   end
+
+   get '/artists/:slug' do
+
+     erb :actualartist
+   end
+
+   get '/genres/:slug' do
+
+     erb :actualgenre
    end
 
 end
