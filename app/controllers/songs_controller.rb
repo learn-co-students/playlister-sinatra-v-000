3ca29a97ee269309eb11e6d01efca02d1c889ea4
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-01 03:30:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,4 +14,18 @@ get '/songs/:slug' do
   erb:'/songs/show'
 end
 
+get '/songs/:id/edit' do
+    @song = Song.find_by_id(params[:id])
+    erb :edit
+  end
+
+patch '/songs/:id' do
+  @song = Song.find_by_id(params[:id])
+  @song.name = params[:name]
+  @song.artist = params[:artist]
+  @song.genres = params[:genres]
+  @song.save
+  redirect to "/song/#{@song.id}"
+end
+
 end
