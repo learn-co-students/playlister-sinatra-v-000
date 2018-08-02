ca58ef568599d259cb7c7cf7feeef73572a45fcb
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-02 23:39:58 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,11 @@
 class SongsController < ApplicationController
 
   get '/songs' do
-    erb :'songs/index'
+    erb :'/songs/index'
   end
 
+  get '/songs/:slug' do
+    @current_song = Song.find_by_slug(params[:slug])
+    erb :"/songs/show"
+  end
 end
