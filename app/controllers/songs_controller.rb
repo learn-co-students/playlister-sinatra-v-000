be9b6a97a8b1d8dd25ad295e818b276c672faad8
Christine Cua <cloudycua@gmail.com>
2018-08-02 16:57:00 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,7 @@
-require 'sinatra/base'
 require 'rack-flash'
 
-class SongsController < Sinatra::Base
-  enable :sessions
+class SongsController < ApplicationController
   use Rack::Flash
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
 
   get '/songs' do
     @songs = Song.all
