bf97c836252037c12af14bdc30f93c735583a4d4
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-03 00:01:06 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,11 @@
 class GenresController < ApplicationController
+
+  get '/genres' do
+    erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @current_genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
 end
