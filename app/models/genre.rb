41c94c5bd3214ba45c1dab096d006506204c1253
Rebecca Tong <rtong@buffalo.edu>
2018-08-03 13:27:13 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,9 @@
 class Genre < ActiveRecord::Base
 
   has_many :song_genres
-  has_many :artists, :through => :songs
   has_many :songs, :through => :song_genres
+  has_many :artists, :through => :songs
+  
 
   def slug
     [name.parameterize].join("-")
