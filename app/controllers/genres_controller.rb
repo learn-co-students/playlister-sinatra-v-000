f8406d80d50c1e0112dba317465305b3a9022e39
Rebecca Tong <rtong@buffalo.edu>
2018-08-03 21:35:35 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,8 +1,8 @@
 class GenresController < ApplicationController
 
-get '/genres' do
-  @genres = Genres.all
-  erb :'/genres/index'
-end
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
 
 end
