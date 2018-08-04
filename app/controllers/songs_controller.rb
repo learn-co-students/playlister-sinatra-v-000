a0b2f80136dee118e6d32398243e0c7b26d369a8
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-04 21:53:17 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,13 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
+
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_by_id(@song.artist_id)
+
+    genre_id = SongGenre.find_by_id(@song.id).genre_id
+    @genre = Genre.find_by_id(genre_id)
+    
     erb :'/songs/show'
   end
 
