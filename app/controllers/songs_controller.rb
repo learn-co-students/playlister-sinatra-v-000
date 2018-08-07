99c860870ede681381d8c7db9a84bb4b94e1b4e6
Kevin Chi <kevinchi118@gmail.com>
2018-08-07 18:46:59 +0000

    new songs and flash message

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,36 @@
-
+require 'pry'
 class SongsController < ApplicationController
+
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
     erb :songs
   end
 
+  get '/songs/new' do
+    erb :new_song
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :show_song
   end
+
+  post '/songs' do
+    @song = Song.create(name: params[:song][:name])
+    @song.artist = Artist.find_or_create_by(params[:artist])
+    params[:genres].each do |genre_id|
+      @song.genres << Genre.find_by_id(genre_id)
+    end
+    @song.save
+
+    if Song.all.include?(@song)
+      flash[:message] = "Successfully created song."
+      redirect "/songs/#{@song.slug}"
+    end 
+  end
+
+
 end
