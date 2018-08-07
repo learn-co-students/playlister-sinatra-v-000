9b716b069eb37d537d2f39c059a424430af2b0bb
Rebecca Tong <rtong@buffalo.edu>
2018-08-07 17:15:15 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,39 @@
+
 class SongsController < ApplicationController
+
+  enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
   end
 
-  get 'songs/new' do
+  get '/songs/new' do
     erb :'/songs/new'
   end
 
   get '/songs/:slug' do
-    @songs = Song.all
+    @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
+
+  post '/songs' do
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params["genres"]
+    @song.save
+     flash[:message] = "Successfully created song."
+    redirect("/songs/#{@song.slug}")
+  end
+
+  get '/songs/:slug/edit' do
+    erb :'/songs/edit'
+
+  end
+
+
+
 
 
 end
