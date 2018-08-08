6faa36ad480aac8074e07681a2b5c708cea861b2
Anu Khambete <anukhambete@gmail.com>
2018-08-08 19:00:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,12 +53,19 @@ class ApplicationController < Sinatra::Base
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    binding.pry
+    #binding.pry
     erb :'/songs/edit'
   end
 
   post '/songs/:slug' do
-
+    #binding.pry
+    @song = Song.find_or_create_by(name: params["Name"])
+    #@song.save
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 
 
