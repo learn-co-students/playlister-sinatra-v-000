80ccb04cc68aabf45166861c951091b18b06338b
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-08 10:50:02 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,13 +22,25 @@ class SongsController < ApplicationController
     if Artist.find_by_name(params[:artist_name]) == nil
       @artist = Artist.create(name: params[:artist_name])
       @song = Song.create(name: params[:Name])
-      params
+      @song.artist = @artist
+      @song.save
+
+      clean_genres = params[:genres].reject(&:empty?)
+      @song_genre = []
+      clean_genres.each do |genre|
+      if Genre.find_by_name(genre) == nil
+        new_genre = Genre.create(genre)
+        @song_genre << new_genre
+      else found_genre = Genre.find_by_name(genre) == true
+        @song_genre << found_genre
+      end
+    end
+
 #create array
 #prevent dup
 ##prevent blanks
 #save relationshp between genre and song
-      @song.artist = @artist
-      @song.save
+
     else
       @artist = Artist.find_by_name(params[:artist_name])
       @song = Song.create(name: params[:Name])
