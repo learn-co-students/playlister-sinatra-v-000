cdf8f8546b48a24a0e28170647fae214c0b567da
Anu Khambete <anukhambete@gmail.com>
2018-08-08 18:03:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  use Rack::Flash
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
@@ -12,11 +16,42 @@ class ApplicationController < Sinatra::Base
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    #binding.pry
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    #binding.pry
+    @song = Song.create(name: params["Name"])
+    #@song.save
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    #@gen = params["genres"]
+    #@gen.each do |gen|
+    #  @sg = SongGenre.create(song_id: @song.id, genre_id: gen)
+  #    @sg.save
+  #  end
+  #  @song.save
+
+    #if !params["Artist Name"].blank?
+  #  @artist = Artist.find_or_create_by(name: params["Artist Name"])
+    #@artist.songs << @song
+    #end
+    @song.save
+    #binding.pry
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     #binding.pry
     erb :'/songs/show'
   end
+
+
 
 
   get '/artists' do
