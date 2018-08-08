58436070399c2f04f154b121bdfb2a0910adf02f
Anu Khambete <anukhambete@gmail.com>
2018-08-08 01:13:15 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,23 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    a = self.name.downcase
+    slug = a.gsub!(/[!@% &"]/,'-')
+  end
+
+  def self.find_by_slug(slug)
+    value = nil
+    Song.all.each do |song|
+      #binding.pry
+      if song.slug == slug
+        #binding.pry
+        #val = artist
+        value = song
+      end
+    end
+    value
+  end
+  
 end
