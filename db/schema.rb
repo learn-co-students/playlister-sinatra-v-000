37d644bba972d165d62076b15e95720ed27bfa69
Anu Khambete <anukhambete@gmail.com>
2018-08-08 00:15:04 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,8 @@ ActiveRecord::Schema.define(version: 20180807220940) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "artist_id"
   end
 
 end
