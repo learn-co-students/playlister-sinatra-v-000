8747cd5251462b4aa58cb3647c20f99a60680096
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-08 23:48:10 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,38 +18,40 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    binding.pry
+    #binding.pry
     if Artist.find_by_name(params[:artist_name]) == nil
       @artist = Artist.create(name: params[:artist_name])
       @song = Song.create(name: params[:Name])
       @song.artist = @artist
+      @song.genre_ids = params[:genres]
       @song.save
 
-      clean_genres = params[:genres].reject(&:empty?)
-      @song_genre = []
-      clean_genres.each do |genre|
-      if Genre.find_by_name(genre) == nil
-            new_genre = Genre.create(name: genre)
-            @song_genre << new_genre
-      elsif Genre.find_by_name(genre) == true
-            found_genre = Genre.find_by_name(genre)
-            @song_genre << found_genre
-      end
-    end
+    #  clean_genres = params[:genres].reject(&:empty?)
+    #  @song_genre = []
+    #  clean_genres.each do |genre|
+    #  if Genre.find_by_name(genre) == nil
+    #        new_genre = Genre.create(name: genre)
+    #        @song_genre << new_genre
+    #  else
+    #        found_genre = Genre.find_by_name(genre)
+    #        @song_genre << found_genre
 
+    #  end
+  #  end
 #create array
 #prevent dup
 ##prevent blanks
-#save relationshp between genre and song
-
+#save relationshp between genre and song ************
     else
       @artist = Artist.find_by_name(params[:artist_name])
       @song = Song.create(name: params[:Name])
       @song.artist = @artist
+      @song.genre_ids = params[:genres]
       @song.save
 
     end
-     redirect to "/songs/#{@song.slug}"
+     flash[:message] = "Successfully created song."
+      redirect to ("/songs/#{@song.slug}")
   end
 
   get '/songs/:id/edit' do
@@ -58,7 +60,6 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:id' do
-binding.pry
     @song = Song.find_by_slug(params[:id])
     params
 
