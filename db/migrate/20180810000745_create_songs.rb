bcc02268296511b9b61526cc01edda48e82578de
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-11 11:26:11 +0000

    Done.

--- db/migrate/20180810000745_create_songs.rb
+++ db/migrate/20180810000745_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :name
-      t.string :genre_ids
+#      t.string :genre_ids
       t.string :artist_id
     end
   end
