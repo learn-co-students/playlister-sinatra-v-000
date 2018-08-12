56f0e993f5b9467daf170cf59e98f39d14caca4b
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-12 14:44:24 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,7 @@
 class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :songs, through: :song_genres
   has_many :artists, through: :songs
-  has_many :songs
 
   def slug
     self.name.parameterize
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_many :genres
+  has_many :song_genres
+  has_many :genres, through: :song_genres
 
   def slug
     self.name.parameterize
