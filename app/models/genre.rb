60b45b56fa5ff5ba8eca7fcc7f3e0fa6c7dfca3c
Jamie Brown <jmbrowncow@gmail.com>
2018-08-13 20:16:50 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class Genre <ActiveRecord::Base
-  has_many :artists
-  has_many :songs, through: :song_genre
+  has_many :artists, through: :songs_genres
+  has_many :songs, through: :songs_genres
 end
