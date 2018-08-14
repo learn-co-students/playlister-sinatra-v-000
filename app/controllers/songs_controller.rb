caaf842ff3586ad6228c1b3294f552db1f7c41d5
Jamie Brown <jmbrowncow@gmail.com>
2018-08-14 14:39:28 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+
 class SongsController < ApplicationController
 
   get '/songs' do
@@ -33,22 +34,45 @@ class SongsController < ApplicationController
 
 
     @song.save
+    @message = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
-
-
-  # post '/owners' do
-  #   @owner = Owner.create(params["owner"])
-  #   if !params["pet"]["name"].empty?
-  #     @owner.pets.build(name: params["pet"]["name"])
-  #   end
-  #   @owner.save
-  #   redirect "owners/#{@owner.id}"
-  # end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @artists = Artist.all
+    @genres = Genre.all
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    if !params["artist"]["name"].empty? && !Artist.find_by(name: params["artist"]["name"])
+      @song.build_artist(name: params["artist"]["name"])
+    else
+      artist = Artist.find_by(name: params[:artist_name])
+      artist ||= Artist.find_by(name: params["artist"]["name"])
+      @song.artist = artist
+    end
+
+    if !params["genre"]["name"].empty? && !Genre.find_by(name: params["genre"]["name"])
+      @song.genres.build(name: params["genre"]["name"])
+    else
+      genre = Genre.find_by(name: params[:genre_name])
+      genre ||= Genre.find_by(name: params["genre"]["name"])
+      # binding.pry
+      @song.genres << genre
+    end
+
+
+    @song.save
+    @message = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
 end
