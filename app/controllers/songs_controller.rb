6c211bea917715e4eba68794e802c232be28cff2
Jamie Brown <jmbrowncow@gmail.com>
2018-08-14 03:18:31 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,47 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.new(name: params["song"]["name"])
+    # I want to not create a new arstist if Artist.find_by(name: params["artist"]["name"]) finds something
+    if !params["artist"]["name"].empty? && !Artist.find_by(name: params["artist"]["name"])
+      @song.build_artist(name: params["artist"]["name"])
+    else
+      artist = Artist.find_by(name: params[:artist_name])
+      artist ||= Artist.find_by(name: params["artist"]["name"])
+      @song.artist = artist
+    end
+
+    if !params["genre"]["name"].empty? && !Genre.find_by(name: params["genre"]["name"])
+      @song.genres.build(name: params["genre"]["name"])
+    else
+      genre = Genre.find_by(name: params[:genre_name])
+      genre ||= Genre.find_by(name: params["genre"]["name"])
+      # binding.pry
+      @song.genres << genre
+    end
+
+
+    @song.save
+    redirect "/songs/#{@song.slug}"
+  end
+
+
+  # post '/owners' do
+  #   @owner = Owner.create(params["owner"])
+  #   if !params["pet"]["name"].empty?
+  #     @owner.pets.build(name: params["pet"]["name"])
+  #   end
+  #   @owner.save
+  #   redirect "owners/#{@owner.id}"
+  # end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Artist.create(name: "Adele")
+Artist.create(name: "Florence and the Machine")
+Artist.create(name: "Muze")
 
+Song.create(name: "Somewhere over the rainbow")
+Song.create(name: "Hello")
+Song.create(name: "uprising")
+
+Genre.create(name: "Alternative")
+Genre.create(name: "Freaky Jive")
+Genre.create(name: "Jazz")
