e25a18b2ced624b07c085ec03b6ab004e3e66b30
treverandersen <trever.r.andersen@gmail.com>
2018-08-17 17:06:37 -0400

    setup controller actions to route to songs/new and post action for form, setup form for songs/new view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,25 @@ class SongsController < ApplicationController
 		erb :'/songs/index'
 	end
 
+	get '/songs/new' do 
+		erb :'/songs/new'
+	end
+
 	get '/songs/:slug' do 
 		@song = Song.find_by_slug(params[:slug])
 		erb :'/songs/show'
+	end
+
+	post '/songs' do 
+		# @song = Song.new
+		# @song.name = params[:name]
+		# @song.save
+		@song = Song.create(:name => params["Name"])
+		@song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+		@song.genre_ids = params[:genres]
+		@song.save
+		
+		redirect to '/songs/#{@songs.slug}'
 	end
 
 end
