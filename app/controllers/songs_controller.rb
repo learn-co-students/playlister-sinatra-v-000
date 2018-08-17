4102207ffe02f1057969c8490123c8ca5e2ce3d8
treverandersen <trever.r.andersen@gmail.com>
2018-08-17 15:14:35 -0400

    setup module file for slugs, referenced module in song class, setup controller action to show song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,10 @@ class SongsController < ApplicationController
 		@songs = Song.all 
 		erb :'/songs/index'
 	end
+
+	get '/songs/:slug' do 
+		@song = Song.find_by_slug(params[:slug])
+		erb :'/songs/show'
+	end
+
 end
