fff74604e426bf413e9602b6fe2723fd0513e22f
treverandersen <trever.r.andersen@gmail.com>
2018-08-17 14:07:58 -0400

    first commit. setup database tables and associations, setup file structure

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,7 @@
+class SongsController < ApplicationController
+
+	get '/songs' do
+		@songs = Song.all 
+		erb :'/songs/index'
+	end
+end
---
+++ app/models/genre.rb
@@ -0,0 +1,6 @@
+
+class Genre < ActiveRecord::Base
+	has_many :song_genres
+	has_many :songs, :through => :song_genres
+	has_many :artists, :through => :songs
+end
