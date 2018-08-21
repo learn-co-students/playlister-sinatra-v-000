a03cd710364b7d9429e234b655e7fc71ef8f02a8
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 03:42:49 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres
+  has_many :genres, through: :songs
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,18 @@
+module Slugifiable
+  module InstanceMethods
+    def slug
+      name_array = self.name.split(" ")
+      name_array = name_array.collect {|word| word.downcase}
+      name_array.join("-")
+    end
+  end
+
+  module ClassMethods
+    def self.find_by_slug(slug)
+      artists = self.all
+      artists.detect do |artist|
+        artist.slug == slug
+      end
+    end
+  end
+end
