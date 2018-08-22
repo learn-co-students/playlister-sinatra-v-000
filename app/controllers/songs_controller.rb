f3c0c668a0d171c9a9f15af0ad5c9f2b4073937e
Ron Sala <ron.sala@gmail.com>
2018-08-22 04:51:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,8 @@
 class SongsController < ApplicationController
 
   get "/songs" do
-
+    @song = Song.all
+    erb :"songs/index"
   end
 
   get "/songs/:slug" do
@@ -9,7 +10,7 @@ class SongsController < ApplicationController
   end
 
   get "/songs/new" do
-    
+
   end
 
 end
