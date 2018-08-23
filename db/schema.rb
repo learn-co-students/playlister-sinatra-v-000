3828cbdf98ff943df3c694950227fcd299cdabbb
Ron Sala <ron.sala@gmail.com>
2018-08-23 05:00:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "artists", force: :cascade do |t|
-    t.string  "name"
-    t.integer "genre_id"
+    t.string "name"
   end
 
   create_table "genres", force: :cascade do |t|
