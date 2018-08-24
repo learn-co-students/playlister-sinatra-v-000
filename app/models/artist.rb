41bd63f7bcdabef5fb8d4a18292adb80a19f80ff
Ron Sala <ron.sala@gmail.com>
2018-08-24 05:03:18 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,12 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  # has_many :genres
   has_many :genres, through: :songs
+
+  def self.slug(name)
+    
+  end
+
+  def find_by_slug
+    self.slug
+  end
 end
