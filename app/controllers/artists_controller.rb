e076dabd70f17bdcb7d642f1d10ad3164d6d5543
Brent Bauman <brentbauman86@gmail.com>
2018-08-25 01:04:50 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,9 @@
 class ArtistsController < ApplicationController
 
-  get '/artists' do
-    @artists = Artist.all
-    erb :"artists/index"
-  end
+  # get '/artists' do
+  #   @artists = Artist.all
+  #   erb :"artists/index"
+  # end
 
 
 end
