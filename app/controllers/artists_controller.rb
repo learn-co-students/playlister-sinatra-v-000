e7c9137eb3f64931f29caa951fb7d9cb453d0eb1
Brent Bauman <brentbauman86@gmail.com>
2018-09-03 15:07:45 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,8 +5,12 @@ class ArtistsController < Sinatra::Base
 
   get '/artists' do
     @artists = Artist.all
-
     erb :"artists/index"
   end
-  
+
+  get '/artists/:slug' do
+    slug = params[:slug]
+    @artist = Artist.find_by_slug(slug)
+    erb :"artists/show"
+  end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,13 @@
 class GenresController < ApplicationController
-
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
 
   get '/genres' do
-    @genres = Genres.all
-    erb :'genres/index'
+    @genres = Genre.all
+
+    erb :"genres/index"
   end
+
   
 end
