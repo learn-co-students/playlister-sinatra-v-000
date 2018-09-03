3568e1710b8a04622793d559438345815f34fa49
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-03 03:36:13 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,15 @@
 class ArtistsController < ApplicationController
+
+  get '/artists' do
+    erb :'artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    @songs = @artist.songs
+    @genres = @artist.genres
+    erb :'artists/show'
+  end
+
+
 end
