8c2d1b4323bb447c0455d63872b93ea3b47da5fb
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-09-07 04:49:08 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,15 +2,15 @@ module Slugifiable
   module InstanceMethods
     def slug
       name.downcase.split(' ').join("-")
-      binding.pry
+      #binding.pry
     end
   end
   
   module ClassMethods
-    def self.find_by_slug(slug)
+    def find_by_slug(slug)
       param = slug.split("-").join(" ")
       Artist.find_by(name: param)
-      binding.pry
+      #binding.pry
     end
   end
 end
