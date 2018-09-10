1df65b32cede61bcdb2b9d7ab15e6684780ecd2f
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-10 16:37:33 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :genres, through: :song_genres
   has_many :song_genres
+
+  def slug
+  self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+end
+
+def self.find_by_slug(slug)
+  self.all.find do |x|
+    x.slug == slug
+  end
+end
 end
