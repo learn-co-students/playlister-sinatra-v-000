89f8178b837c9e825ea56633d70da1c85f605000
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-12 22:11:02 +0000

    Done.

--- app/models/genres.rb
+++
@@ -1,5 +0,0 @@
-class Genres < ActiveRecord::Base
-  has_many :song_genres
-  has_many :songs, :through => :song_genres
-  has_many :artists, :through => :songs
-end
--- db/migrate/20180912213544_create_songs_genre.rb
+++
@@ -1,8 +0,0 @@
-class CreateSongsGenre < ActiveRecord::Migration
-  def change
-    create_table :song_genre do |t|
-      t.integer :song_id
-      t.integer :genre_id
-    end
-  end
-end
