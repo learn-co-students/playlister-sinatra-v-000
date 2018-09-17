32835064fa25bb51fe92e5910e8ddd136baed160
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-09-17 21:18:52 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,5 @@
 class ArtistsController < ApplicationController
+
+
+
 end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,13 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+
+
+end
