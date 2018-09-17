8a4f3a4524a898870c0a0273ad35f9b07e6494de
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-17 18:21:09 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,11 @@
 class GenresController < ApplicationController
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/#{@genre.slug}'
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,8 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/#{@song.slug}'
+    erb :"/songs/#{@song.slug}"
   end
 end
