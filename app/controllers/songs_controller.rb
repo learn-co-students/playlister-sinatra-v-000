6619940d83f02417760e6d1faf0ff237499801ed
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-18 16:38:22 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,11 +5,17 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
     erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  post '/songs' do
+    
   end
 end
