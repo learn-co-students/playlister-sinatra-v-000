caa8ab42fccae81aa8673e850071e675a5fe449e
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-18 16:46:39 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,14 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    
+    @song = Song.create(name: params[:name])
+    if !params[:new_artist_name].blank?
+      @artist = Artist.create(name: params[:new_artist_name])
+      @song.artist = @artist
+    else
+      @artist = Artist.find_by(params[:artist_name])
+      @song.artist = @artist
+    end
+    redirect "/songs/#{@song.slug}"
   end
 end
