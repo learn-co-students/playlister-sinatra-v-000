7cbb3aafa23699bcb459700fb886a4c82e876afa
Ron Sala <ron.sala@gmail.com>
2018-09-19 02:12:21 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,17 @@
 class Artist < ActiveRecord::Base
+  include Slug
   has_many :songs
   has_many :genres, through: :songs
 
-  def self.slug(name)
-    
+  attr accessor :name
+
+  def create(name)
+    @artist = Artist.new
+    @artist.save
+  end
+
+  def slug(name)
+    name.slug
   end
 
   def find_by_slug
