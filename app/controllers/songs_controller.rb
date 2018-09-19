e18516abf6fb09a70ef08545b4b6ecdc4ecaf261
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-09-19 15:36:28 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,19 @@
 class SongsController < ApplicationController
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
   get '/songs' do
-    # @songs = Song.all
+    @songs = Song.all
     erb :'songs/index'
   end
-
+  post '/songs' do
+    @song = Song.create(name: params[:name])
+    @song.artist = Artist.find_or_create_by(name: params[:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Song successfully created."
+    redirect to '/songs/#{@song.slug}'
+  end
+  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
-  include Slugifiable::InstanceMethod
-  extend Slugifiable::ClassMethod
+  # include Slugifiable::InstanceMethod
+  # extend Slugifiable::ClassMethod
   belongs_to :artists
   has_many :song_genres
   has_many :genres, through: :song_genres
