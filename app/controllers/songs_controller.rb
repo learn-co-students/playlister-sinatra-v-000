8f555b1baba1dfb573596770619aa23f688cdb40
Mark Lawrence <lawrence.ml@gmail.com>
2018-09-21 19:54:11 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
   
   get '/songs' do
     @song = Song.all
@@ -14,14 +17,31 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
-
+  
   post '/songs' do
     @song = Song.create(name: params[:song_name])
+    # binding.pry
     @song.artist = Artist.find_or_create_by(name: params[:artist_name])
     @song.genre_ids = params[:genres]
     @song.save
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    # binding.pry
+    @song.update(name: params[:song_name])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
 
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,13 +2,13 @@ module Slugifiable
 
   module InstanceMethods
     def slug
-      self.name.gsub(/\W/, "-").downcase
+      self.name.gsub((/\W/), "-").downcase
     end
   end
 
   module ClassMethods
     def find_by_slug(slug)
-      self.all.find{|string| string.slug == slug}
+      self.all.find{|string| string.slug == slug} 
     end
   end
   
