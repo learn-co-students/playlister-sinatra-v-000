ce17f89409572b3f515744ad12d137cdb96fc31b
Ron Sala <ron.sala@gmail.com>
2018-09-24 02:49:49 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,15 +1,15 @@
 class GenresController < ApplicationController
 
-  get "/genres/:slug" do
-
-  end
-
   get "/genres/new" do
 
   end
 
+  get "/genres/:slug" do
+
+  end
+
   get "/genres" do
-    @genre = Genre.all
+    @genres = Genre.all
     erb :"genres/index"
   end
 
