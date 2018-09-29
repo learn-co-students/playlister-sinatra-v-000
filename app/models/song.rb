97e8c4bdd2b3110d28c6020709e21187fd199f17
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-29 23:52:34 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    slug = self.name.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
+    slug
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect {|song| song.slug == slug}
+  end
 end
