d8ec9c41a02d387889e7966667eb7f3aee2aa007
Nathaniel Shuster <nshu521992@gmail.com>
2018-10-01 21:48:10 +0000

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,8 @@
+class Genre < ActiveRecord::Base
+  has_many :artists
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+end
