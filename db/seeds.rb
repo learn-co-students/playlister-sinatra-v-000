cefb8b5d8b7a30fc4f588ee3e7bd6be9ef14d030
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-09 01:43:30 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+LibraryParser.parse
+#drake = Artist.create(name: "Drake")
+#song_1 = Song.create(name: "Doop", artist: drake)
+#Genre.create(name: "rock", song: song_1, artist: drake)
