cedf207a5e69b64c1b51b98bdfb4972ca7284b1a
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-16 18:44:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,15 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
   
   get '/songs' do 
     @songs = Song.all 
-    erb :'songs/index'
+    erb :'/songs/index'
   end 
   
   get '/songs/new' do 
-    erb :'songs/new'
+    erb :'/songs/new'
   end 
   
   get '/songs/:slug' do 
@@ -15,12 +18,31 @@ class SongsController < ApplicationController
   end 
   
   post '/songs' do 
-    @song = Song.create(:name => params["name"])
+    @song = Song.create(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save 
     
-    redirect 'songs/#{@song.slug}'
+    flash[:message] = "Successfully created song." #no clue what the fuck is going on with this... ugh...
+    
+    redirect ("/songs/#{@song.slug}")
+  end 
+  
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end 
+  
+  patch '/songs/:slug' do 
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save 
+    
+    flash[:message] = "Successfully updated song." #no clue what the fuck is going on with this... ugh...
+    
+    redirect ("/songs/#{@song.slug}")
   end 
   
 end 
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,7 +4,7 @@ class Genre < ActiveRecord::Base
   has_many :songs, :through => :song_genres 
   
   def slug 
-    name.downcase.gsub(" ", "-")
+    name.downcase.gsub(" ","-")
   end 
   
   def self.find_by_slug(slug)
