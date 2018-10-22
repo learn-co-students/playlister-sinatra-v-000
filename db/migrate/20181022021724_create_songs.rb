30c98d467da162d56ba0c02f00c30760a0edecba
Adam Barton <adam.barton333@gmail.com>
2018-10-22 03:33:48 +0000

    Done.

--- db/migrate/20181022021724_create_songs.rb
+++ db/migrate/20181022021724_create_songs.rb
@@ -3,5 +3,7 @@ class CreateSongs < ActiveRecord::Migration
     create_table :songs do |t|
       t.string :name
       t.integer :artist_id
+      t.string :genres
+    end
   end
 end
--- db/migrate/20181022021730_create_genres.rb
+++ db/migrate/20181022021730_create_genres.rb
@@ -2,6 +2,6 @@ class CreateGenres < ActiveRecord::Migration
   def change
     create_table :genres do |t|
       t.string :name
-
+    end
   end
 end
