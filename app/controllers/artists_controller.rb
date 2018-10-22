ea98f6b39c048dc332b3ee04c44a6ba13df04984
Adam Barton <adam.barton333@gmail.com>
2018-10-22 21:08:25 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,6 +7,8 @@ class ArtistsController < ApplicationController
 
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
+    @songs = @artist.songs
+    @genres = @artist.genres
     erb :'/artists/show'
   end
 
