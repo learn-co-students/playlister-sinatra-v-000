b757a115257b453f7aad14953f28d36f8c46bf8b
Adam Barton <adam.barton333@gmail.com>
2018-10-23 18:13:00 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,6 +18,11 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
   post '/songs' do
     @song = Song.create(name: params[:Name])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
