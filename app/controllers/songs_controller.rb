b3be141c53f2b564ec555ed5bfdd1306d5253c4d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-24 10:12:55 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,40 @@
 class ArtistsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
   
   get '/songs' do
     @song = Song.all
      erb :index
   end
   
+  get '/songs/new' do
+    @song = Song.create(params[:song])
+    erb :'/songs/new'  
+  end
+  
+  get '/songs/:slug' do
+    
+  end
+  
+  patch '/songs/:slug' do
+  
+end
+  
   post '/songs' do
     @song = Song.create(params[:song])
       if !params['artist']['name'].empty?
        @song.artist = Song.create(name: params['artist']['name'])
+       elsif !params['genre']['name'].empty?
+       @song.genre = Song.create(name: params['genre']['name'])
       end
     @song.save
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
     erb :'/songs/show'
+  end
+  
+  get '/songs/:slug/edit'
+  
   end
     
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,12 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
+  
+  def slug(str)
+      str.strip.downcase.gsub /\W+/, '-'  
+  end
+  
+  def find_by_slug
+    
+  end
 end
