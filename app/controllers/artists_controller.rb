cc4de4d1d118ba0a5bbb519ce2247d9ced14f44a
AlwinaO <aoyewole2586@gmail.com>
2018-10-24 22:22:17 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,15 @@
 class ArtistsController < ApplicationController
 
-  get '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-
-  end
-
   get '/artists' do
     @artists = Artist.all
 
-    erb :'/artists/index'
+    erb :'artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+
   end
 
   get '/artists/:id' do
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,16 +2,16 @@ require "pry"
 
 class SongsController < ApplicationController
 
+    get '/songs' do
+    @songs = Song.all
+
+    erb :'/songs/index'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
 
     erb :'/songs/show'
-  end
-
-  get '/songs' do
-    @songs = Song.all
-
-    erb :'/songs/index'
   end
 
   get '/songs/:id' do
