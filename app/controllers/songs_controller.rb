fe57b4e2f00b3128cf4fc6402a9c741ef2683fb7
AlwinaO <aoyewole2586@gmail.com>
2018-10-25 22:14:45 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,10 +2,14 @@ require "pry"
 
 class SongsController < ApplicationController
 
-    get '/songs' do
+  get '/songs' do
     @songs = Song.all
 
     erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
@@ -16,6 +20,11 @@ class SongsController < ApplicationController
 
   get '/songs/:id' do
     @song = Song.find_by_id(params[:id])
+    erb :'/songs/show'
+  end
+  
+  post '/songs' do
+
     erb :'/songs/show'
   end
 
