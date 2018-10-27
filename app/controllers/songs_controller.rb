fc187903ea4bb0358df3e9330a0cce8d515d7de7
Christia T <christiatidwell@bellsouth.net>
2018-10-27 22:51:01 +0000

    added edit page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,13 +21,26 @@ use Rack::Flash
     @song.save
 
     flash[:message] = "Successfully created song."
-    
+
     redirect to ("/songs/#{@song.slug}")
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params["Name"]
+    @song.artist = params["Artist Name"]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
