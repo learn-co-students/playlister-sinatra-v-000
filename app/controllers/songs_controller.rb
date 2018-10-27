cff38cc424b10becf975691ce8ffd077eb0c29b3
Christia T <christiatidwell@bellsouth.net>
2018-10-27 21:01:53 +0000

    got checkbox working in songs' new page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,8 +10,12 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(name: params[:name], artist_id: params[:artist_id])
-    redirect to '/songs/#{@song.id}'
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to '/songs/#{@song.slug}'
   end
 
   get '/songs/:slug' do
