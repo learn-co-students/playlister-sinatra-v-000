925d9e45a09711c3778b4aaa71e21a4292c46708
Christia T <christiatidwell@bellsouth.net>
2018-10-28 04:42:59 +0000

    working to pass last test

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,10 +36,13 @@ use Rack::Flash
   end
 
   patch '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    @song.name = params["Name"]
-    @song.artist = params["Artist Name"]
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
     @song.save
+
+    flash[:message] = "Successfully updated song."
+
     redirect to "/songs/#{@song.slug}"
   end
 
