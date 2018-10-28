4b8be25cd7ced0502474a54608fde4b0de2a44ff
AlwinaO <aoyewole2586@gmail.com>
2018-10-27 21:03:03 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,8 @@
 require "pry"
+require "rack-flash"
 
 class SongsController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -14,18 +16,21 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'/songs/show'
   end
 
-  get '/songs/:id' do
-    @song = Song.find_by_id(params[:id])
-    erb :'/songs/show'
-  end
-  
   post '/songs' do
 
-    erb :'/songs/show'
+      @song = Song.new(:name => params["Name"])
+      @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+      @song.genre_ids = params[:genres]
+      @song.save
+      binding.pry
+
+      flash[:message] = "Successfully created song."
+
+    redirect to("/songs/#{@song.slug}")
   end
+
 
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -8,7 +8,7 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      self.all.detect{|instance| instance.slug == slug}
+      self.all.find{|instance| instance.slug == slug}
     end
   end
 
