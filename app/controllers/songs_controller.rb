4a95acc658d10a3f5f1e05a41f00e4bfd8aa2cd0
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-30 20:04:16 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,6 +19,7 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.new(name: params["Name"])
+    binding.pry
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
