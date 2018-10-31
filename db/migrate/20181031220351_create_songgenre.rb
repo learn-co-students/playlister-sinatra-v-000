976267916319ed692b63a341c9474a6c082d82c5
Emily S <emily.sugano@gmail.com>
2018-10-31 22:31:11 +0000

    Done.

--- db/migrate/20181031220351_create_songgenre.rb
+++ db/migrate/20181031220351_create_songgenre.rb
@@ -2,7 +2,7 @@ class CreateSonggenre < ActiveRecord::Migration
   def change
     create_table :song_genres do |t|
        t.integer :genre_id
-        t.integer :artist_id
+        t.integer :song_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20181031220351) do
 
   create_table "song_genres", force: :cascade do |t|
     t.integer "genre_id"
-    t.integer "artist_id"
+    t.integer "song_id"
   end
 
   create_table "songs", force: :cascade do |t|
