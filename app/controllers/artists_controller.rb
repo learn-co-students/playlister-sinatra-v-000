a6577fc91dfa8a14aec2ca56d33428b07d938004
Tomas Toledo <trtoledo1977@gmail.com>
2018-11-09 00:56:26 +0000

    views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,12 @@
 class ArtistsController < ApplicationController
 
-  get '/songs' do
-    @songs = Song.all
-    erb :'/views/song.rb'
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+   get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
   end
 end
--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -1,7 +1,12 @@
 class GenresController < ApplicationController
 
-  get '/generes' do
-    @songs = Song.all
-    erb :'/views/song.rb'
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+   get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'genres/show'
   end
 end
