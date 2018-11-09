939335e2685f429603a6dc94bd4eead3c8387d68
Emily S <emily.sugano@gmail.com>
2018-11-09 22:49:37 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,11 @@ class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs/:slug' do
     binding.pry
+    erb :'songs'
   end
 end
