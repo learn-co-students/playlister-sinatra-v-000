ec8454800dc1e4158e0285e55414a51f3b969ed9
github username <github email address>
2018-11-12 15:59:49 -0700

    working thru 05_song_form_spec line 93

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,16 @@
 class SongsController < ApplicationController
 
+  enable :sessions
+  use Rack::Flash
+
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
+  end
+
+  get "/songs/new" do
+    @genres = Genre.all
+    erb :'/songs/new'
   end
 
   get '/songs/:slug' do
@@ -10,4 +18,20 @@ class SongsController < ApplicationController
     #binding.pry
     erb :'/songs/show'
   end
+
+  post '/songs' do
+    @song = Song.new({name: params[:name]})
+    params[:genres].each do |e|
+      genre = Genre.find_by(name: e)
+      @song.genres << genre
+    end
+
+    artist = Artist.find_or_create_by({name: params[:artist_name]})
+    @song.artist = artist
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+
+  end
+
 end
