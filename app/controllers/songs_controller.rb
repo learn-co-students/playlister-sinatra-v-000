45ef0d67dcbe34fc41d02ddfea394f3e73f7f7f9
Emily S <emily.sugano@gmail.com>
2018-11-12 22:02:13 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,14 +9,18 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
-  post '/songs/new' do
-    erb :'songs/:slug'
-  end
-
   get '/songs/:slug' do
     @slug = Song.find_by_slug(params[:slug].gsub("-"," "))
     @artist = Artist.find(@slug.artist_id)
     erb :'songs/show'
+  end
+
+  post '/songs' do
+    @artist = params[:artist_name]
+    @genre = params[:genre_name]
+    new_song = Song.create(name: params[:song_name])
+    new_song.artist = Artist.create(name:@artist)
+    new_song.genres << Genre.create(name:@genre)
   end
 
 end
