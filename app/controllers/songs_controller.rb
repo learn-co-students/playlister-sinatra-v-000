4225a24d310e50a51084a347a02b583bc62fb713
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-14 00:09:16 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,10 +4,21 @@ class SongsController < ApplicationController
   end
 
   get "/songs/new" do
+    erb :"songs/new"
+  end
 
+  post "/songs" do
+    @song = Song.create(name: params[:name])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
   end
 
   get "/songs/:slug" do
     erb :"songs/show"
+  end
+
+  get "/songs/:slug/edit" do
+
   end
 end
