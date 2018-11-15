0cf4a9b4ff20a8a8de7ff33e7918e9ed7d6de958
Justin Kenney <justinkenney13@gmail.com>
2018-11-15 22:47:41 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,36 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @artist = Artist.create(name: params[:songs][:artist][:name])
+    @song = Song.create(name: params[:songs][:name], artist: @artist)
+    params[:songs][:genres].each do |genre|
+      @song.genres << Genre.find(genre)
+    end
+
+    redirect :"/songs/#{@song.id}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find(params[:slug])
     erb :'/songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find(params[:slug])
+    @genres = Genre.all
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    raise params.inspect
+    binding.pry
+    redirect :"/songs/#{params[:slug]}"
   end
 
 end
