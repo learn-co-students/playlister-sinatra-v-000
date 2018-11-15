f34429175fca85001ff4fc5d5213fe9a34e01a60
Justine B <justinegayton@gmail.com>
2018-11-15 20:48:44 +0000

    Done.

---
+++ app/models/song_genre.rb
@@ -0,0 +1,10 @@
+require_relative "./concerns/slugifiable.rb"
+
+class SongGenre < ActiveRecord::Base
+  belongs_to :song
+  belongs_to :genre
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+  
+end
---
+++ db/migrate/20181113213020_update_name_of_join_table.rb
@@ -0,0 +1,7 @@
+class UpdateNameOfJoinTable < ActiveRecord::Migration
+
+  def change
+    rename_table :songs_genres, :song_genres
+  end
+
+end
