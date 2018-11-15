460c0c6aabd792fa9c397b27fd23bd98085ddd3c
Tomas Toledo <trtoledo1977@gmail.com>
2018-11-15 01:04:38 +0000

    Done.

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
-  get '/new' do
+  get '/songs/new' do
     erb :'/songs/new'
   end
 
@@ -29,15 +29,17 @@ class SongsController < ApplicationController
    get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
-  end
+   end
+
    patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
-     flash[:message] = "Successfully updated song."
+
+    flash[:message] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
-  end
-  
+   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,26 @@
 # Add seed data here. Seed your database with `rake db:seed`
 # LibraryParser.parse
 
+pop=Genre.create(name: "Pop")
+rock=Genre.create(name: "Rock")
+jazz=Genre.create(name: "Jazz")
 #[0,1,2]
-Artist.create(name: "Alabama Shakes")
-Artist.create(name: "Herbie Hancock")
-Artist.create(name: "The Goo Goo Dolls")
+alabama = Artist.create(name: "Alabama Shakes")
+herbie = Artist.create(name: "Herbie Hancock")
+the = Artist.create(name: "The Goo Goo Dolls")
  #[0,1,2]
-Song.create(name: "Iris")
-Song.create(name: "Slide")
-Song.create(name: "Black Baloon")
+alabama.songs.create(name: "Iris", genre_ids: [Genre.first.id])
+alabama.songs.create(name: "Slide", genre_ids: [Genre.first.id])
+alabama.songs.create(name: "Black Baloon", genre_ids: [Genre.first.id])
  #[3,4,5]
-Song.create(name: "Sound & Color")
-Song.create(name: "Don't Wanna Fight")
-Song.create(name: "Dunes")
+alabama.songs.create(name: "Sound & Color", genre_ids: [Genre.first.id])
+alabama.songs.create(name: "Don't Wanna Fight", genre_ids: [Genre.first.id])
+alabama.songs.create(name: "Dunes", genre_ids: [Genre.first.id])
  #[6,7,8]
-Song.create(name: "Watermelon Man")
-Song.create(name: "Maiden Voyage")
-Song.create(name: "Cataloupe Island")
+alabama.songs.create(name: "Watermelon Man", genre_ids: [Genre.first.id])
+alabama.songs.create(name: "Maiden Voyage", genre_ids: [Genre.first.id])
+alabama.songs.create(name: "Cataloupe Island", genre_ids: [Genre.first.id])
  #[0,1,2]
-Genre.create(name: "Pop")
-Genre.create(name: "Rock")
-Genre.create(name: "Jazz") 
+
+
+# Song.first.update(genre: pop)
