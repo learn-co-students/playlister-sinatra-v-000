73292162213ede5a11f75d36957399e844e49a47
Justin Kenney <justinkenney13@gmail.com>
2018-11-15 20:51:47 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,12 @@
 class ArtistsController < ApplicationController
+
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find(params[:slug])
+    erb :'/artists/show'
+  end
 end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,13 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find(params[:slug])
+    erb :'/songs/show'
+  end
+
+end
