6fe11527f08511d8a27972706fe2d9c195f7b321
Srija S <srijakumar7@gmail.com>
2018-11-18 13:55:37 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.create(:name => params["Artist Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     #binding.pry
     @song.genre_ids = params[:genres]
     @song.save
@@ -39,7 +39,7 @@ class SongsController < ApplicationController
    @song = Song.find_by_slug(params[:slug])
    @song.update(params[:song])
    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-   @song.genre_id = params[:genres]
+   @song.genre_ids = params[:genres]
    @song.save
    flash[:message] = "Successfully updated song."
    redirect("/songs/#{@song.slug}")
