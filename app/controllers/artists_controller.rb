e14903a6f5bdb251ae9f47c2ba2ed12fb2b31614
Barbara Choy <barbchoy@gmail.com>
2018-11-29 21:33:36 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,10 @@ class ArtistsController < ApplicationController
   get '/artists' do
     erb :'artists/index'
   end
+
+  get '/artists/:slug' do
+    slug = params[:slug]
+    @artist = Artist.find_by_slug("#{slug}")
+    erb :'artists/show'
+  end
 end
