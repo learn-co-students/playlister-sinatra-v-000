3610eb1ce6a62261b372006205e65e4f7cb27432
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-30 22:13:29 +0000

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,19 @@
+module Concerns::Slugifiable
+
+  def slug
+    self.name.
+
+
+    def find_by_name(name)
+      all.find {|item| item.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      if find_by_name(name)
+        find_by_name(name)
+      else
+        self.create(name)
+      end
+    end
+
+end
