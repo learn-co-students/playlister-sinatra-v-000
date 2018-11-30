8d5acf0cac8f7058d09649674d05ed72c64cee5a
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-30 22:02:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181130214240) do
+ActiveRecord::Schema.define(version: 20181130214834) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20181130214240) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres_songs", force: :cascade do |t|
+    t.integer "genre_id"
+    t.integer "song_id"
   end
 
   create_table "songs", force: :cascade do |t|
