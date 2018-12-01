377779fd7c5cd6ea2fec05500a80999618575e9e
Marc Mills <mdmills1990@gmail.com>
2018-12-01 16:48:14 +0000

    song/new features working, capybara test issues, need to fix edits

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,6 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    binding.pry
   @song = Song.create(:name => params["Name"])
   @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
   @song.genre_ids = params[:genres]
@@ -35,7 +34,7 @@ class SongsController < ApplicationController
       erb :'songs/edit'
     end
 
-    patch "/songs/:slug" do
+  patch "/songs/:slug" do
       @song = Song.find_by_slug(params[:slug])
       @song.update(params[:song])
       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
@@ -43,7 +42,7 @@ class SongsController < ApplicationController
       @song.save
 
       flash[:message] = "Successfully updated song."
-      redirect("/songs/#{@song.slug}")
+      redirect "/songs/#{@song.slug}"
     end
 
 
