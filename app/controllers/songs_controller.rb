c61234d1b6c44b609a66b256eb3bd10b269cbbdd
Nathan  Visak <nathan.visak@gmail.com>
2018-12-02 22:43:40 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+
 class SongsController < ApplicationController
 
   get '/songs' do
@@ -10,16 +11,38 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
-  post '/songs' do
-    @song = Song.create(name: params["Name"])
-    @artist = Artist.create(name: params["Artist Name"])
-    @song.artist = @artist
-    redirect '/songs'
-  end
-
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params["Name"])
+    if @artist = Artist.find_by_name(params["Artist Name"])
+      @song.artist = @artist
+    else
+      @artist = Artist.create(name: params["Artist Name"])
+      @song.artist = @artist
+    end
+    @genre = Genre.find_by_id(params["genres"])
+    @song.genres << @genre
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist.update(name: params["Artist Name"])
+    @song.genre_ids = params["genres"]
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,19 +1,19 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :song_genres, through: :songs
-  has_many :genres, through: :song_genres
+  has_many :genres, through: :songs
+
 
 
   def slug
     self.name.downcase.strip.gsub(' ', '-')
   end
 
-  def self.find_by_slug(slug)
-    Artist.all.each do |artist|
-      if artist.slug == slug
-        @artist = artist
+
+  def self.find_by_slug(slug_version)
+    self.all.each do |artist|
+      if artist.slug == slug_version
+        return artist
       end
-      @artist
     end
   end
 
