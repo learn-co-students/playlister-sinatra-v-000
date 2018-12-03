8185e0eb81170cad3a39e546cd1c8d2c40b37753
Marc Mills <mdmills1990@gmail.com>
2018-12-03 16:50:20 +0000

    moved Rack::MethodOverride above other controllers to fix the edit issues

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,7 +34,7 @@ class SongsController < ApplicationController
       erb :'songs/edit'
     end
 
-  patch "/songs/:slug" do
+  patch '/songs/:slug' do
       @song = Song.find_by_slug(params[:slug])
       @song.update(params[:song])
       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
--- config.ru
+++ config.ru
@@ -4,9 +4,9 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+use Rack::MethodOverride
 
 use ArtistsController
 use GenresController
 use SongsController
-use Rack::MethodOverride
 run ApplicationController
