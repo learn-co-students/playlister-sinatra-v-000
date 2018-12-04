3e41f22d1fe0bb6bcc8966cd4a0fd4be1a48050b
Naudria <rima.jean@gmail.com>
2018-12-04 10:13:57 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,12 @@
 class ArtistsController < ApplicationController
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+
+  get '/artists' do 
+  	@artists = Artist.all
+  	erb :"artists/index"
+  end
+  
 end
