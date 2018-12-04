7aae5dbac49fa66d688184ab5f7a327ecf39e985
Naudria <rima.jean@gmail.com>
2018-12-04 12:17:28 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,5 +8,11 @@ class ArtistsController < ApplicationController
   	@artists = Artist.all
   	erb :"artists/index"
   end
+
+  get '/artists/:slug' do
+    slug = params[:slug]
+    @artist = Artist.find_by_slug(slug)
+    erb :"artists/show"
+  end
   
 end
