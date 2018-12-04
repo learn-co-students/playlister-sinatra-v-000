6d5bdb612c369c4e48cfa8dd00e2aeae659f4cb0
Leslie Stevens <flylady@mail.utexas.edu>
2018-12-04 23:55:40 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,5 +4,17 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'/songs/index'
   end
-  
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    #binding.pry
+    erb :'/songs/show'
+  end
+
+
+
 end
