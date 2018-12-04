4276a87c12537d9d10b06daac4f61251848e422d
Naudria <rima.jean@gmail.com>
2018-12-04 14:45:04 -0600

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,26 +1,17 @@
 module Slugifiable
+
   module InstanceMethods
     def slug
-      artist_name = self.name
-      slug = artist_name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+      name_array = self.name.split(' ')
+      array_downcase = name_array.collect {|word|word.downcase}
+      array_downcase.join('-')
     end
   end
 
   module ClassMethods
     def find_by_slug(slug)
-      @slug = slug
-      format_slug_beginning
-      results = self.where("name LIKE ?", @short_slug)
-      results.detect do |result|
-        result.slug === @slug
-      end
-    end
-
-    def format_slug_beginning
-      slug_beginning = @slug.split("-")[0]  
-      slug_beginning.prepend("%")
-      slug_beginning << "%"
-      @short_slug = slug_beginning
+      self.all.select {|instance|instance.slug == slug}[0]
     end
   end
+
 end
