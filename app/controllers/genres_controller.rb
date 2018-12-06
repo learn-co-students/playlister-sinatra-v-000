0b887462c2d476dd11d29b271208ed644ddeaaf1
Dani Lyn <dlyn1110@gmail.com>
2018-12-06 19:10:26 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,12 +2,12 @@ class GenresController < ApplicationController
 
   get '/genres' do
    @genres = Genre.all
-   erb :'genres/index'
+   erb :'/genres/index'
  end
 
  get '/genres/:slug' do
    @genre = Genre.find_by_slug(params[:slug])
-   erb :'genres/show'
+   erb :'/genres/show'
  end
 
 end
