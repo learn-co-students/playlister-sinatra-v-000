d1c3d5f6f9494242b907882090f0b9ede9ccb7ea
Leslie Stevens <flylady@mail.utexas.edu>
2018-12-06 19:19:55 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,14 +9,20 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
+  post '/songs' do
+    @song = Song.create(params[:song])
+    if !params["artist_name"].empty?
+      @artist = Artist.create(name: params["artist_name"])
+      @artist.songs << @song
+    end
+    redirect to "songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     #binding.pry
     erb :'/songs/show'
   end
-  
-
-
 
 
 end
