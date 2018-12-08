ad0cdcb43197cf494de2248928ead906636d510b
Leslie Stevens <flylady@mail.utexas.edu>
2018-12-08 01:12:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
   enable :sessions
+  use Rack::Flash
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
+  end
+
+  post '/set-flash' do
+    flash[:message] = "Successfully created song."
   end
 end
