b8ac61705c5aff199e3c21ab1341e9f9a7287c8f
Christopher Beltran <christod.beltran@gmail.com>
2018-12-08 20:29:39 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,19 @@
 class ArtistsController < ApplicationController
+  
+  #display a list of all artists in library
+  get '/artists' do 
+    @artists = Artist.all
+    erb :"artists/index"
+  end 
+  
+  
+  #display the artists songs & genres, with links to each
+  get '/artists/:slug' do 
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :"/artists/show"
+  end
+  
+  
+
+
 end
