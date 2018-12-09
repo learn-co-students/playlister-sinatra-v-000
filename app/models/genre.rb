a409ff46be5fc6acdf47d7222b952c4b0309e336
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-12-09 04:20:08 +0000

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,6 @@
+class Genre < ActiveRecord::Base
+  has_many :artists, :through => :songs
+  has_many :song_genres
+  has_many :songs, :through => :song_genres
+  
+end
---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, :through => :song_genres
+  
+  
+end
