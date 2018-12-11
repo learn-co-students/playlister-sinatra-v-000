c2fd060901cd2cc435dcf7ede674c60fd6456378
Harley Rayner <harleymteden@gmail.com>
2018-12-11 19:38:26 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,6 +10,7 @@ class ArtistsController < ApplicationController
       get '/artists/:slug' do
         slug = params[:slug]
         @artist = Artist.find_by_slug(slug)
+        erb :'artists/show'
       end
 
 end
