cd3828a2772631288c7aa97aad51628bf73cb561
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-12 09:52:37 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,15 @@
 class ArtistsController < ApplicationController
 
   get '/artists' do
-    @artists = Artist.all 
+    @artists = Artist.all
     erb :'/artists/index'
   end
-  
+
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
     erb :'/artists/show'
   end
+
   
 
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,14 +4,15 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'/songs/index'
   end
-  
-  # get '/songs/new' do
-    
-  # end
-  
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
-  
+
 end
