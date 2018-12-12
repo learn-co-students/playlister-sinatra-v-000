ec3cb6e2600518e53120faa137e893df93cb0425
brandong1 <brandon@brandonleeproductions.com>
2018-12-11 19:59:03 -0700

    created root routes for artists, genres, songs controllers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,7 @@
 class SongsController < ApplicationController
+     
+    get '/songs' do
+        @songs = Song.all
+        erb '/songs/index'
+    end
 end
-
-
- get '/songs' do
-    @songs = Song.all
-    erb '/songs/index'
- end
