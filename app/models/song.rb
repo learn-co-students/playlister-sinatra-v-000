82af2c079c5196876c5c3839ef92875b0180e192
brandong1 <brandon@brandonleeproductions.com>
2018-12-13 19:03:10 -0700

    finished all models

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
     belongs_to :artist
     has_many :song_genres
     has_many :genres, :through => :song_genres
+
+    def slug
+        name.downcase.strip.gsub(' ', '-')
+    end
+
+    def self.find_by_slug(slug)
+        Song.all.find_by_slug{|song| song.slug == slug }
+    end
+
 end
