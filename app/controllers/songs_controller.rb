53a70159115505b59b38608f415471b1045a96ad
Greg Morgan <gregm9999@gmail.com>
2018-12-16 22:40:17 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,17 @@ class SongsController < ApplicationController
     erb :"songs/new"
   end
 
+  get '/songs/:slug' do
+    @song=Song.find_by_slug(params[:slug])
+    binding.pry
+    erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song=Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
   post '/songs' do
     @song=Song.create({name: params[:name]})
     @song.artist=Artist.find_or_create_by(name: params[:artist_name])
@@ -28,11 +39,6 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
     @song=Song.find_by_slug(params[:slug])
     erb :'songs/edit'
-  end
-
-  get '/songs/:slug' do
-    @song=Song.find_by_slug(params[:slug])
-    erb :'songs/show'
   end
 
   post '/songs/:slug' do
