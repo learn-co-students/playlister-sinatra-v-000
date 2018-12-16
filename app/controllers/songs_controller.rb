ffaaf4cd81a87491c44849bb269ee068611d293a
Greg Morgan <gregm9999@gmail.com>
2018-12-16 22:48:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,8 +18,8 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song=Song.find_by_slug(params[:slug])
-    binding.pry
     erb :'songs/show'
+  end
 
   post '/songs' do
     @song=Song.create({name: params[:name]})
