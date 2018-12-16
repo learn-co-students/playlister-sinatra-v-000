ae534ab60a22347491cdf0cc0278b2193c310b37
Greg Morgan <gregm9999@gmail.com>
2018-12-16 20:51:22 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,14 +17,12 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    song=Song.create({name: params[:name]})
-    song.artist=Artist.find_or_create_by(name: params[:artist_name])
-    if song.genres != []
-      song.genres << params[:genres].collect {|genre| Genre.find(genre)}
-    end
-    song.save
+    @song=Song.create({name: params[:name]})
+    @song.artist=Artist.find_or_create_by(name: params[:artist_name])
+    @song.genres << params[:genres].collect {|g| Genre.find(g)}
+    @song.save
     flash[:message] = "Successfully created song."
-    redirect to "songs/#{song.slug}"
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
