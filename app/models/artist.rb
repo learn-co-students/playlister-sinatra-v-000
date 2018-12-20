69be28e7bb014a3a81a1eb6693dd6cd8ebb576f1
Wendy Calderon <wendycalderon@gmail.com>
2018-12-20 04:50:29 +0000

    Create #slug and #find_by_slug in Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,16 @@
+require "pry"
+
 class Artist < ActiveRecord::Base
   has_many :songs 
   has_many :artists_genres
   has_many :genres, through: :songs
+  
+  def slug 
+    self.name.gsub(" ", "-").downcase
+  end
+  
+  def self.find_by_slug(slug)
+    Artist.all.find{|artist| artist.slug == slug}
+    
+  end
 end
