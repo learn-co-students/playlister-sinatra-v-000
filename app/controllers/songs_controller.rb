759631dc4de9d918e066306daa24b8bb40c1b3f9
Wendy Calderon <wendycalderon@gmail.com>
2018-12-21 03:26:51 +0000

    Set up /songs/show and route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,9 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'songs/index' 
   end
+  
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
 end
