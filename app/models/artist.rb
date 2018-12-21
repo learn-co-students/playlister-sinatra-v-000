e925c8eb7bfc4ed623a591b5aa7a417fbd234d95
Mike Dilley <mike.dilley@gmail.com>
2018-12-21 15:38:14 +0000

    Done.

---
+++ app/models/artist.rb
@@ -0,0 +1,14 @@
+class Artist < ActiveRecord::Base
+
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def slug
+    self.name.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.detect {|a| a.slug == slug}
+  end
+
+end
