0b780687768f15ac57f58b1aa20f0cef55ed3c7c
Zachary Lehmann <zlehmann@gmail.com>
2018-12-23 22:16:08 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181223214911) do
+ActiveRecord::Schema.define(version: 20181223221140) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -27,8 +27,7 @@ ActiveRecord::Schema.define(version: 20181223214911) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string  "name"
-    t.integer "artist_id"
+    t.string "name"
   end
 
 end
