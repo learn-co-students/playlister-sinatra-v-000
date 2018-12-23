683e6cdd54113af4cadacaadda2a2380dc7dade3
Tammy Metz <tammy@tammymetz.com>
2018-12-23 13:28:00 +0000

    Done.

--- db/migrate/20181223125727_add_column_to_genres.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToGenres < ActiveRecord::Migration
-  def change
-    add_column :genres, :artist_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181223125727) do
+ActiveRecord::Schema.define(version: 20181223131859) do
+
+  create_table "artist_genres", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string  "name"
-    t.integer "artist_id"
+    t.string "name"
   end
 
   create_table "song_genres", force: :cascade do |t|
