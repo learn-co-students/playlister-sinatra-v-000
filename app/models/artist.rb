af4dd31b690e1523b15f55a912c7ce7a6834264a
Zachary Lehmann <zlehmann@gmail.com>
2018-12-23 23:09:20 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    camel_slug = self.name.gsub! " ", "-"
+    camel_slug.downcase
+  end
 end
