133fb6bf3d8d5e0b97c74bc0acbcf89f93022539
Zachary Lehmann <zlehmann@gmail.com>
2018-12-24 20:04:26 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,7 @@
 class ArtistsController < ApplicationController
+
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artist/index'
+  end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,7 @@
 class GenresController < ApplicationController
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genre/index'
+  end
 end
