cf72fc427c99de41866f00a7935aacbcb1dfdfdc
Zachary Lehmann <zlehmann@gmail.com>
2018-12-24 16:21:52 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    camel_slug = self.name.gsub! " ", "-"
+    camel_slug.downcase
+  end
+
+  def self.find_by_slug(slug)
+    name = slug.gsub! "-", " "
+    matching_song = Song.find{ |song| song.name.downcase == name }
+  end
 end
