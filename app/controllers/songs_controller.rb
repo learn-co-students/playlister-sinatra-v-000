dbc16f072f088ebed582a0fb04102f6218806a4f
Wendy Calderon <wendycalderon@gmail.com>
2018-12-27 03:34:15 +0000

    Add edit feature

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,11 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
   
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+  
   post '/songs' do 
     @song = Song.create(name: params["Name"])
     if !params["Artist Name"].empty?
@@ -32,8 +37,18 @@ class SongsController < ApplicationController
     redirect to("/songs/#{@song.slug}")
   end
   
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
-  end
+  get '/songs/:slug/edit' do
+     @song = Song.find_by_slug(params[:slug])
+     erb :'/songs/edit'
+   end
+   
+  patch '/songs/:slug' do
+     @song = Song.find_by_slug(params[:slug])
+     @song.update(params[:song])
+     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+     @song.save
+     flash[:message] = "Successfully updated song."
+     redirect to("/songs/#{@song.slug}")
+     redirect to("/songs/#{@song.slug}")
+   end
 end
