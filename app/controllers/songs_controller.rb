8e4c10b8a60d25df5f7f9d5d26202790ce4c5361
Zachary Lehmann <zlehmann@gmail.com>
2018-12-27 22:33:48 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,12 @@
 class SongsController < ApplicationController
 
+
   get '/songs' do
     @songs = Song.all
     erb :'/song/index'
   end
 
   get '/songs/new' do
-    @new_song = Song.create(name: "song_name")
     erb :'/song/new'
   end
 
@@ -15,20 +15,21 @@ class SongsController < ApplicationController
     erb :'/song/show'
   end
 
-  post '/songs/:slug' do
-    @new_song = Song.find_by_slug(params[:slug])
+  post '/songs' do
+    @new_song = Song.create(name: "song_name")
     if Artist.find_by_name(params[:"Artist Name"])
-      @new_song.artist = Artist.find_by_slug(@new_song.artist.slug)
+      @new_song.artist = Artist.find_by_name(params[:"Artist Name"])
     else
       @new_artist = Artist.create(name: params[:"Artist Name"])
       @new_song.artist = @new_artist
     end
-    binding.pry
+
     @new_song.name = params[:"Name"]
     params[:genres].each do |genre|
       @new_song.genres << Genre.find(genre)
     end
     @new_song.save
+    flash[:message] = "Successfully created song."
     redirect to "/songs/#{@new_song.slug}"
   end
 
