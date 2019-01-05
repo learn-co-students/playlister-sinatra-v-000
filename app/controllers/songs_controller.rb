74846a1907718183632ac8bea7357ccb73d31d37
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-05 22:36:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,12 +6,21 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+    @artists = Artist.all
     erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    if !params[:artist][:name].empty?
+      @song.artist << Artist.create(params[:artist])
+    end
+    redirect "/songs/#{@song.slug}"
   end
 
 end
