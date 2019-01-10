700b47d0691ef96c917dec986f55315f5f07bf13
Joshua Tan <jtsarvida@gmail.com>
2019-01-10 22:01:15 +0000

    continuing testing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,4 +27,18 @@ class SongsController < ApplicationController
     flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
+  
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+  
+  patch '/songs/:slug' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
 end
