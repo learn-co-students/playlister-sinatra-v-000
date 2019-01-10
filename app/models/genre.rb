bb879f6cd05a56e6d2ffeb6e0a5279aff45d3540
Joshua Tan <jtsarvida@gmail.com>
2019-01-10 07:34:34 +0000

    Finished the models tests

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,14 @@
 class Genre < ActiveRecord::Base 
-  has_many :artists 
-  has_many :songs 
+  has_many :artists, :through => :songs
+  has_many :songs, :through => :song_genres
+  has_many :song_genres
   
+  def slug 
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+  
+  def self.find_by_slug(slug)
+    @genres = Genre.all 
+    @genres.detect{|genre| genre.slug == slug}
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres 
   has_many :genres, :through => :song_genres
+  
+  def slug 
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+  
+  def self.find_by_slug(slug)
+    @songs = Song.all 
+    @songs.detect{|song| song.slug == slug}
+  end
 end
