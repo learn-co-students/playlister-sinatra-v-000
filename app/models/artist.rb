76f7445a490eb982574bf6bd2de780e339dc4756
Vince Andrew Pimentel <contact@vincep.me>
2019-01-14 20:48:47 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,14 +1,19 @@
+require_relative "./concerns/slugifiable"
+
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  def slug
-    self.name.downcase.gsub(" ", "-")
-  end
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 
-  def self.find_by_slug(slug)
-    Artist.all.find do |artist|
-      artist.slug == slug
-    end
-  end
+  # def slug
+  #   self.name.downcase.gsub(" ", "-")
+  # end
+  #
+  # def self.find_by_slug(slug)
+  #   Artist.all.find do |artist|
+  #     artist.slug == slug
+  #   end
+  # end
 end
