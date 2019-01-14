501387ed313c9e02748551c9fce0b385fcc917ee
Rachel Wyatt <rw126@evansville.edu>
2019-01-14 20:23:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,73 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+  
+  get '/artists' do
+    @artists = Artist.all
+    erb :'artists/index'
+  end
+  
+  get '/genres' do
+    @genres = Genre.all
+    erb :'genres/index'
+  end
+  
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+  
+  get '/songs/:slug' do 
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+  
+  get '/artists/:slug' do 
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
+  end
+
+  get '/genres/:slug' do 
+    @genres = Genre.find_by_slug(params[:slug])
+    binding.pry
+    erb :'genres/show'
+  end
+  
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params["slug"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    if !@song.genres.include?(Genre.find(params["genres"][0].to_i))
+      @song.genres << Genre.find(params["genres"][0].to_i)
+    end
+    @song.name = params["Song Name"]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(:slug)
+    @song.save
+    erb :'songs/:slug'
+  end
+
+  
+
 end
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,5 @@
 class ArtistsController < ApplicationController
 
-  get '/artists' do
-    @artists = Artist.all
-    erb :'artists/index'
-  end
 
 
 end
