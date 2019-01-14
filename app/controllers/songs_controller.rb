239aeb1bf4b3f3341b7cbd3a7aa7383c3505dae6
Vince Andrew Pimentel <contact@vincep.me>
2019-01-14 21:34:04 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,9 +6,27 @@ class SongsController < ApplicationController
     erb :"/songs/index"
   end
 
-  # get "/songs/new" do
-  #   erb :"/songs/new"
-  # end
+  get "/songs/new" do
+    @genres = Genre.all
+
+    erb :"/songs/new"
+  end
+
+  post "/songs" do
+    song = Song.create(name: params["Name"])
+
+    artist = Artist.find_or_create_by(name: params["Artist Name"])
+
+    song.artist = artist
+
+    song.genre_ids = params[:genres]
+
+    song.save
+
+    @message = "Successfully created song."
+
+    redirect "/songs/#{song.slug}"
+  end
 
   get "/songs/:slug" do
     @song = Song.find_by_slug(params[:slug])
