46cb2810640fe6c8d29293bf61efb464190972d0
Sharmaine Requerme <srqrme@gmail.com>
2019-01-15 19:43:59 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,13 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb: 'songs/show'
+  end
+
 end
