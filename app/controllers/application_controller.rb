c2c7ad1af130bf6e3caacc2fb4f9a73582c9a7c8
Rachel Wyatt <rw126@evansville.edu>
2019-01-16 01:20:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
   post '/songs' do
     @song = Song.create(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
@@ -19,37 +19,37 @@ class ApplicationController < Sinatra::Base
     @songs = Song.all
     erb :'songs/index'
   end
-  
+
   get '/artists' do
     @artists = Artist.all
     erb :'artists/index'
   end
-  
+
   get '/genres' do
     @genres = Genre.all
     erb :'genres/index'
   end
-  
+
   get '/songs/new' do
     erb :'songs/new'
   end
-  
-  get '/songs/:slug' do 
+
+  get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
-  
-  get '/artists/:slug' do 
+
+  get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
   end
 
-  get '/genres/:slug' do 
+  get '/genres/:slug' do
     @genres = Genre.find_by_slug(params[:slug])
-    binding.pry
+    #binding.pry
     erb :'genres/show'
   end
-  
+
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
@@ -73,6 +73,6 @@ class ApplicationController < Sinatra::Base
     erb :'songs/:slug'
   end
 
-  
+
 
 end
