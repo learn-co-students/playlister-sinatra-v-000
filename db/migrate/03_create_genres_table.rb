f245f4ed69d4afc8604e91c7a9b280a96128d6d6
Rafael Lopez <lpz.raf@gmail.com>
2019-01-18 01:45:02 +0000

    Done.

--- db/migrate/03_create_genres_table.rb
+++ db/migrate/03_create_genres_table.rb
@@ -1,7 +1,8 @@
-class CreateGenresTable < ActiveRecord::Migration
+class CreateSongGenresTable < ActiveRecord::Migration
   def change
-    create_table :genres do |t|
-      t.string :name
+    create_table :song_genres do |t|
+      t.integer :genre_id
+      t.integer :song_id
     end
   end
 end
