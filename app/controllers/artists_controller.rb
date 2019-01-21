d85c66f41b69eab8b836c2e38754e232a78a75bb
Rafael Lopez <lpz.raf@gmail.com>
2019-01-21 19:51:15 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,8 @@
 class ArtistsController < ApplicationController
+  
+  get "/artists/new" do
+    @songs = Song.all
+    @genres = Genre.all
+    erb :'artists/new'
+  end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,9 @@
 class GenresController < ApplicationController
+  
+  get "/genres/new" do
+    @songs = Song.all
+    @genres = Genre.all
+    erb :'genres/new'
+  end
+  
 end
