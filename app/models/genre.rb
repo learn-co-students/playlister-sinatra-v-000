f8254150a0f2e5f11700c5dc739858e545e61a76
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-27 21:51:43 -0500

    Create Genre methods

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,13 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
   has_many :song_genres
   has_many :songs, through: :song_genres
+
+  def slug
+    name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    Genre.all.each {|a| if a.slug == slug; return a; end}
+  end
+
 end
