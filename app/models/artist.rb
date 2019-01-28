495372aca071eafb2e8ff6f2775054b3f9cc8f7b
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-27 21:50:22 -0500

    Create Artist methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.each {|a| if a.slug == slug; return a; end}
+  end
+
 end
