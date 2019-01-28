b695bfbcab68e51fbd7a87f52bc094cdf8d7457e
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-27 21:51:01 -0500

    Create Song methods

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.each {|a| if a.slug == slug; return a; end}
+  end
+
 end
