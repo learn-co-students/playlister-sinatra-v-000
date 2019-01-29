3099c1ec1ea1bb52adf8deaa26b51a6521befa91
Heather Miller <heather.miller114@gmail.com>
2019-01-29 14:55:59 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
         @song.artist = Artist.find_or_create_by(name: params[:artist_name])
         @song.genre_ids = params[:genres]
         @song.save
-
+        #binding.pry
         flash[:message] = "Successfully created song."
         redirect "/songs/#{@song.slug}"
 
@@ -27,16 +27,23 @@ class SongsController < ApplicationController
     
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
-       # binding.pry
         erb :'songs/show'
     end
 
     get '/songs/:slug/edit' do
-
+        @genres = Genre.all
+        @song = Song.find_by_slug(params[:slug])
+        erb :'songs/edit'
     end
 
     patch '/songs/:slug' do
-        
+        @song = Song.find_by_slug(params[:slug])
+        @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+        @song.genre_ids = params[:genres]
+        @song.save
+
+        flash[:message] = "Successfully updated song."
+        redirect "/songs/#{@song.slug}"
     end
 
 end
