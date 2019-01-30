f259403b57a55a74eefdac2323cf08e7533b8b72
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2019-01-30 01:53:03 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,16 @@
 class GenresController < ApplicationController
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+      #solo
+    @songs = @genre.songs
+    @artists = @genre.artists
+    erb :'/genres/show'
+  end
+
 end
