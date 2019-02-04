d1716a9dcc4f88dd84333ebfef53200d882934f3
Joey Cho <cjy12182000@naver.com>
2019-02-04 05:52:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,10 +35,11 @@ class ApplicationController < Sinatra::Base
     
     song = Song.find_or_create_by(name: params[:song_name])
   
-  
-    artist = Artist.find_or_create_by(name: params[:artist_name])
-    song.artist = artist
     
+    
+    artist = Artist.find_or_create_by(name: params[:artist_name])
+    song.artist = artist  
+  
     
     
     params[:genres].each do |genre|
@@ -60,6 +61,55 @@ class ApplicationController < Sinatra::Base
     @song = Song.find_by_slug(params[:slug])
   #  binding.pry
     erb :song_s
+  end
+  
+  get "/songs/:slug/edit" do
+  
+    @song = Song.find_by_slug(params[:slug])
+  #  binding.pry
+    erb :song_e
+  end
+  
+  patch '/songs/:slug' do
+
+ 
+    
+    if params[:song_name].empty?
+    @song = Song.find_by_slug(params[:slug])
+    else
+    @song = Song.find_or_create_by(name: params[:song_name])
+    end
+    
+  
+    if params[:something]!= nil
+    artist = Artist.find_or_create_by(name: params[:something])
+    @song.artist = artist
+    elsif !params[:artist_name].empty?
+    artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.artist = artist
+    end
+    
+    
+    if params[:genres]!=nil
+      @song.genres.clear
+      @song.song_genres.clear
+      params[:genres].each do |genre|
+      gen = Genre.find_or_create_by(id: genre)  
+      @song.song_genres.build(genre: gen)
+      @song.genres << gen
+      end
+    else
+      
+    end
+    
+    @song.save
+    
+   
+  
+    flash[:updates] = "Successfully updated song."
+    
+    redirect "songs/#{@song.slug}"
+  
   end
   
   get "/artists/:slug" do
