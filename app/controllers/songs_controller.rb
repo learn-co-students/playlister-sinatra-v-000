f86c5e92e6aff582d9780fb1b304d477586f2c91
Jason Mei <jason.mei403@gmail.com>
2019-02-05 19:43:51 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,5 @@
+class SongsController < ApplicationController
+  get '/songs' do
+    erb :'/songs/index'
+  end
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
+  has_many :song_genres
+  has_many :genres, through: :song_genres
   belongs_to :artist
-  belongs_to :genre
 end
