b67035b2198c893a7dbde8de8d2d2f83a5fad8f0
github username <github email address>
2019-02-06 21:25:12 +0530

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,7 +7,7 @@ class ArtistsController < ApplicationController
     erb :'artists/index'
   end
 
-  get 'artists/:slug' do
+  get '/artists/:slug' do
     binding.pry
     @artist = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
