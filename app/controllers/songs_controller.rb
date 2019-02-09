6697afb0695434d858e904cc9f962d6cb6213ead
yuanxizhang <yuanxi.zhang@gmail.com>
2019-02-08 16:51:06 -0800

    Done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,14 +19,9 @@ class SongsController < Sinatra::Base
   end
 
   post '/songs' do
-    @song = Song.create(name: params[:song][:name])
-    @song.artist = Artist.find_or_create_by(name: params[:song][:artist])
-
-    genre_selections = params[:song][:genres]
-    genre_selections.each do |genre|
-      @song.genres << Genre.find(genre)
-    end
-
+    @song = Song.create(:name => params[:song][:name])
+    @song.artist = Artist.find_or_create_by(:name => params[:song][:artist])
+    @song.genre_ids = params[:song][:genres]
     @song.save
 
     flash[:message] = "Successfully created song."
@@ -35,28 +30,25 @@ class SongsController < Sinatra::Base
   end
 
   get '/songs/:slug' do
-    slug = params[:slug]
-    @song = Song.find_by_slug(slug)
-    erb :"songs/show"
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
   end
 
   patch '/songs/:slug' do
-    song = Song.find_by_slug(params[:slug])
-    song.name = params[:song][:name]
-
-    artist_name = params[:song][:artist]
-    song.artist = Artist.find_or_create_by(name: artist_name)
-    if song.genres
-      song.genres.clear
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params[:song][:name]
+    @song.artist = Artist.find_or_create_by(name: params[:song][:artist])
+    @song.genre_ids = params[:song][:genres]
+    if @song.genres
+      @song.genres.clear
     end
     genres = params[:song][:genres]
     genres.each do |genre|
-      song.genres << Genre.find(genre)
+      @song.genres << Genre.find(genre)
     end
-
-    song.save
+    @song.save
     flash[:message] = "Successfully updated song."
-    redirect to "songs/#{song.slug}"
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
