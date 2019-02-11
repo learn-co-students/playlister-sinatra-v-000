4ad07493ec0980db3a23201ec7a06aa90e8bb1a0
anihakutin <4hbrody@gmail.com>
2019-02-10 23:10:22 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,8 @@
+require_relative './concerns/slugifiable.rb'
 class Genre < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
   has_many :song_genres
   has_many :songs, :through => :song_genres
   has_many :artists, :through => :songs
