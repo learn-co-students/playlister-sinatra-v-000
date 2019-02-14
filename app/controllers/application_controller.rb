d5030dc6a0510b2e8b9c46395392debd5f8b4734
anihakutin <4hbrody@gmail.com>
2019-02-14 16:12:31 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 require 'sinatra/flash'
 class ApplicationController < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
-  enable :sessions
-  register Sinatra::Flash
+
 
   get '/' do
     erb :index
