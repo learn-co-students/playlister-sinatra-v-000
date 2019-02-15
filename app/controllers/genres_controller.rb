244cde254bbb4c825befa8933c4af4a701268c70
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-15 22:07:17 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,6 @@
 class GenresController < ApplicationController
 
     get '/genres' do
-        #binding.pry
         @genres = Genre.all
         erb :'genres/index'
     end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,24 +5,6 @@ class SongsController < ApplicationController
         @songs = Song.all
         erb :'songs/index'
     end
+end
 
-    get '/songs/:slug' do
-<
-        ##@song = Song.find_by_slug(params[:slug])
-        #@artist = @song.artist
-        ##erb :'/songs/show'
-    end
-
-    #post '/songs' do
-    #    @song = Song.create(name: params[:name])
-    #    if !params[:genres].empty?
-    #        @song.genre_ids = params[:genres]
-    #        @song.save
-    #    end
-    #@artist = Artist.find_or_create_by
-    #
-
-
-     flash[:message] = "Successfully created song."
-     redirect to("/songs/#{@song.slug}")
-    #end
+    
