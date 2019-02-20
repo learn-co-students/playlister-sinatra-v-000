2613bf45fe59cecc05737f4a24bb7624100b8a63
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-20 19:13:08 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
     erb :'/songs/show'
     end
 
-    patch '/songs/:slug'
+    patch '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         @song.update(params[:song])
         @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
