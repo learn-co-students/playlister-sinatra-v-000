aae3eba017bdc23d5aa3b845b877c5e8863c4717
Ashley Wong <ashwong04@gmail.com>
2019-03-08 18:00:09 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,3 +2,4 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :song_genre
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+# This is also empty, not sure if we're supposed to do it manually?
