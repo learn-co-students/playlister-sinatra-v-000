ef976a4d5295f5fe114dce99f6dd725d12b7a7cb
Ashley Wong <ashwong04@gmail.com>
2019-03-09 14:44:02 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,13 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
   
   def slug
-    self.name.downcase
+    self.name.downcase.gsub(" ", "-")
   end
   
-  def find_by_slug
-    
+  def self.find_by_slug(slug)
+    name = slug.split("-").map {|s| s.capitalize}
+    full_name = name.join(" ")
+    self.find_by(name: full_name)
   end
   
 end
