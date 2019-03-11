bfee9db4374280b61176833c4f5b791a6340178c
Ashley Wong <ashwong04@gmail.com>
2019-03-11 21:40:30 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,15 +11,6 @@ require "rack-flash"
     erb :'/songs/new'
   end
 
-  post '/songs' do
-    @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.genre_ids = params["genres"]
-    @song.save
-    redirect ('/songs/#{@song.slug}')
-  end
-
-
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
@@ -28,6 +19,15 @@ require "rack-flash"
   get '/songs/:slug' do
     @song = Song.find_by_slug(params["slug"])
     erb :'/songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.find_or_create_by(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params["genres"]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
   end
 
 
