3b352d4708373c61566c136d95667cc1fa159e7b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-12 19:31:11 +0000

    Done.

--- db/migrate/20190312191829_create_songgenres.rb
+++ db/migrate/20190312191829_create_songgenres.rb
@@ -1,10 +1,10 @@
 class CreateSonggenres < ActiveRecord::Migration
   def change
     create_table :songgenres do |t|
-      t.string :name
       t.string :songs
-      t.string :artists
+      t.string :genres
       t.string :song_id
-      t.string :artist_id
+      t.string :genre_id
+    end
   end
 end
