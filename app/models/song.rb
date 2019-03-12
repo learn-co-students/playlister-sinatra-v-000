a7de642a792118c4b9bf6c6ee52e47ce7934b74d
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-12 19:14:05 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,5 +14,14 @@ class Song < ActiveRecord::Base
     slug
   end
 
+  def self.find_by_slug(slug)
+    name = slug.split("-")
+    full_name = []
+    name.each do |word|
+      full_name << word.capitalize
+    end
+    name = full_name.join(" ")
+    Song.find_by(name: name) #uses find_by instead of find because of attribute
+  end
 
 end
