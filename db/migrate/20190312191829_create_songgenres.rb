cbfe929f8790f2221642e99cc8a812d913450915
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-13 15:44:48 +0000

    Done.

--- db/migrate/20190312191829_create_songgenres.rb
+++ db/migrate/20190312191829_create_songgenres.rb
@@ -1,8 +1,8 @@
 class CreateSonggenres < ActiveRecord::Migration #join table here
   def change
     create_table :song_genres do |t|
-      t.string :songs
-      t.string :genres
+      t.string :song
+      t.string :genre
       t.string :song_id
       t.string :genre_id
     end
