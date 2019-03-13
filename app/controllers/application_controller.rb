68665203266654ae3fd9193124d1c95b5b262779
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-13 23:11:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/songs' do
+    @songs = Song.all
     erb :'/songs/index'
   end
 
+  post '/songs' do
+    
+  end
+
   get '/artists' do
+    @artists = Artist.all
     erb :'/artists/index'
   end
 
   get '/genres' do
+    @genres = Genre.all
     erb :'/genres/index'
   end
 
