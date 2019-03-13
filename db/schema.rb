858c420388653aa5c977059aa3c29f3f6290b2a1
Samantha brake <sammy.brake@gmail.com>
2019-03-13 01:56:19 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190312141311) do
-
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
