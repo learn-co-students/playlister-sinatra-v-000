3f7871d5b02ed14ffcd496a616e5673267423038
maxwellbenton <maxwellbenton@gmail.com>
2019-03-14 16:25:49 -0400

    adds active record version to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629133457) do
+ActiveRecord::Schema.define(version: 2015_06_29_133457) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -27,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150629133457) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "artist_id"
   end
 
