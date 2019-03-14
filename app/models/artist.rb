699a8f8068487481c04a279286844f6aad2452da
Samantha brake <sammy.brake@gmail.com>
2019-03-14 02:44:50 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,12 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-binding.pry
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|artist| artist.slug == slug}
+  end
+
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,6 +1,8 @@
 
   def slug
+    name.downcase.gsub(" ","-")
   end
 
-  def find_by_slug
+  def find_by_slug(slug)
+    self.all.find{|artist| artist.slug == slug}
   end
