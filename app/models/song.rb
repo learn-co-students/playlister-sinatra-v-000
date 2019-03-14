d63017bb1dd7628d450354193985322c70009512
Samantha brake <sammy.brake@gmail.com>
2019-03-14 02:47:33 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,13 @@
-class Song < ActiveRecord::Base 
-  belongs_to :artist 
+class Song < ActiveRecord::Base
+  belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|artist| artist.slug == slug}
+  end
 end 
