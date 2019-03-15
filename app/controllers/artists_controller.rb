44341eb78f22a8b6bc24fb051d1d15aed72e4a4a
Samantha brake <sammy.brake@gmail.com>
2019-03-15 03:48:09 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,9 @@
 class ArtistsController < ApplicationController
-  #
-  # get '/artists' do
-  #   @artists = Artist.all
-  # end
-  #
+
+  get '/artists' do
+    @artists = Artist.all
+    erb :'artists/index'
+  end
+
 
 end
