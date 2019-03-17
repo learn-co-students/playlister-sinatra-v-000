0e4cbe498651271cc3bba44ad560160a3c0505e6
Samantha brake <sammy.brake@gmail.com>
2019-03-17 19:18:52 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,12 +28,19 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'songs/show'
   end
 
 
-  #
-  # get 'songs/:slug/edit' do
-  # end
+  get '/songs/:slug/edit' do
+   @song = Song.find_by_slug(params[:slug])
+   erb :'songs/edit'
+  end
+
+  patch '/songs/:slug/' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+  end
 end
