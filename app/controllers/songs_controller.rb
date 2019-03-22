7573cb5b6922bb2ac06639d05111310149bcfb3b
Sara Walker <sarawall1187@gmail.com>
2019-03-22 00:49:33 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,10 +26,26 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
-  get '/songs/:id/edit' do  #load edit form
-    @song = song.find_by_id(params[:id])
+  get '/songs/:slug/edit' do
+    @genres = Genre.all #load edit form
+    @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
   end
 
+  patch '/songs/:slug' do #edit action
+    @artist = Artist.find_or_create_by(:name => params["Artist Name"])
+
+    # @genres = params['genres'].map {|genre| Genre.find(genre)}
+
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = @artist
+    @song.genre_ids = params["genres"]
+    @song.save
+
+    # binding.pry
+
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
 
 end
