86242bfbc7b458ebe53d612ce090be5b06903e01
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-03-22 13:21:27 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,10 +27,28 @@ class SongsController < Sinatra::Base
    @song = Song.create(:name => params["Name"])
    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
    @song.genre_ids = params[:genres]
+
    @song.save
 
    flash[:message] = "Successfully created song."
-   redirect to("/songs/#{@song.slug}")
-  end 
+   redirect to ("/songs/#{@song.slug}")
+  end
+  
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to ("/songs/#{@song.slug}")
+  end
   
 end
