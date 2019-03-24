1804ca56853f4e3a368c324ebf7376f2fe3c80df
Stef Claus <stef.claus@gmail.com>
2019-03-24 21:32:05 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,12 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
 
+  def slug
+    name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end 
+  
+  def self.find_by_slug(slug)
+    Song.all.find{|song| song.slug == slug}
+  end
+  
 end
