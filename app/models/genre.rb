15bbe0331cbf8ae98a31a7b821b52572c4d70c2b
Grace Choi <oiograceoio@gmail.com>
2019-04-05 20:09:06 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,7 +4,7 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def slug
-    name.downcase.tr(' ', '-')
+    name.downcase.gsub(" ","-")
   end
 
   def self.find_by_slug(slug)
