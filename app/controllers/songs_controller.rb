e62a03ea6bb40584618260cb96557c41d3072121
Grace Choi <oiograceoio@gmail.com>
2019-04-05 19:11:25 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(name: params["Name"])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_id = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
+    @song.genre_id = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+end
