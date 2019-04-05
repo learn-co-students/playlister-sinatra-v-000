88758b21f1db5bb8323b91159760f48f0cda1b10
Christiana Vandermale <christianavdm@gmail.com>
2019-04-05 02:16:33 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def find_by_slug
+  end
+
 end
