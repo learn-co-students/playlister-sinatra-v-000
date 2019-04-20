48da9daf99a00f03d750528ec33a0ed540ffa296
sarahpaz <sarah.paz@gmail.com>
2019-04-20 08:39:38 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,7 +9,13 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
 
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
@@ -17,4 +23,13 @@ class SongsController < ApplicationController
     erb :'/songs/show'  
   end
   
+  #Be able to change everything about a song, including the genres associated with it and its artist. 
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug'  do
+  end
 end
