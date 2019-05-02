d98663535e782804ba2dd0a9266c7d13dd39bc70
Thomas Gray <tgray017@gmail.com>
2019-05-02 21:34:02 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 tom_petty = Artist.create(:name => "Tom Petty")
 last_dance_with_mary_jane = Song.create(:name => "Last Dance with Mary Jane")
+last_dance_with_mary_jane.artist = tom_petty
 rock = Genre.create(:name => "Rock")
 americana = Genre.create(:name => "Americana")
-last_dance_with_mary_jane.artist = tom_petty
 last_dance_with_mary_jane.genres << rock
 last_dance_with_mary_jane.genres << americana
 last_dance_with_mary_jane.save
@@ -12,3 +12,8 @@ laundry_room = Song.create(:name => "Laundry Room")
 laundry_room.artist = avett_brothers
 laundry_room.genres << americana
 laundry_room.save
+
+november_blue = Song.create(:name => "November Blue")
+november_blue.artist = avett_brothers
+november_blue.genres << americana
+november_blue.save
