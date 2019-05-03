88ebbfc3846226f684c2331c45b84e0d73b1a9f4
Thomas Gray <tgray017@gmail.com>
2019-05-03 16:34:45 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,7 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+  
+  extend Concerns::Slugifiable
+  include Concerns::Slugifiable
 end
