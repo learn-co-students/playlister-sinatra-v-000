4a92b757b8ba0500696af082fcade6b45195f90c
Thomas Gray <tgray017@gmail.com>
2019-05-06 13:11:27 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,9 +2,7 @@ module Concerns
   module Slugifiable
     
     def find_by_slug(slug)
-      self.all.each do |s|
-        @objekt = s if s.slug == slug
-      end
+      self.all.each {|s| @objekt = s if s.slug == slug}
       @objekt
     end
     
