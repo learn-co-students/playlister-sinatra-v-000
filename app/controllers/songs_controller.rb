31590abf521abdc5d0477ec9c5be6e7986500457
Thomas Gray <tgray017@gmail.com>
2019-05-07 12:15:38 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -58,5 +58,38 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
   
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.name = params[:song_name]
+    
+    if !params[:new_artist].empty? && Artist.find_by(:name => params[:new_artist]).nil?
+      @artist = Artist.create(:name => params[:new_artist])
+    elsif !params[:new_artist].empty?
+      @artist = Artist.find_by(:name => params[:new_artist])
+    else
+      @artist = Artist.find_by(:name => params[:existing_artist])
+    end
+    @song.artist = @artist
+    
+    @song.genres.clear
+    @genres = []
+    if !params[:new_genre].empty?
+      genre = Genre.create(:name => params[:new_genre])
+      @genres << genre
+    end
+    
+    if !params[:existing_genres].empty?
+      params[:existing_genres].each do |g|
+        genre = Genre.find_by(:name => g)
+        @genres << genre 
+      end
+    end
+    @genres.each {|g| @song.genres << g}
+    
+    #binding.pry
+    
+    redirect to "/songs/#{@song.slug}"
+  end
+  
 
 end
