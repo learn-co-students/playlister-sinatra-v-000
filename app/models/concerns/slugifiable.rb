8e7d10f66e81fc9eae0483cf22d56027289d2cf9
Dan Hintz <dhintz@live.com>
2019-05-19 04:05:54 +0000

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,14 @@
+module Slugifiable 
+  module ClassMethods
+    def self.find_by_slug(slug)
+      self.all.find {|obj| obj.slug == slug}
+    end
+  end
+  
+  module InstanceMethods
+    def slug
+      self.name.gsub!(/\s/, '-').gsub(/[^a-zA-Z\d-]/, '').downcase
+    end
+  end
+
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,8 @@
+require_relative './concerns/slugifiable.rb'
+
 class Song < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
   has_many :song_genres
   belongs_to :artist
   has_many :genres, through: :song_genres
