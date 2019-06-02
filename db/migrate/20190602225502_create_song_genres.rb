a191197615515304a096d9c6026bf3d773210907
Reno Sorensen <renojames19@gmail.com>
2019-06-02 23:15:33 +0000

    Done.

--- db/migrate/20190602225502_create_song_genres.rb
+++ db/migrate/20190602225502_create_song_genres.rb
@@ -1,4 +1,4 @@
-class CreateSongGenres < ActiveRecord::Migration[5.2]
+class CreateSongGenre < ActiveRecord::Migration[5.2]
   def change
     create_table :song_genres do |t|
       t.integer :song_id
