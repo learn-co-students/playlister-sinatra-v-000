823a4598b6eec48cc82871f5e0fc6744357637a6
Christiana Vandermale <christianavdm@gmail.com>
2019-06-03 23:35:37 +0000

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,19 @@
+module Slug
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+end
+
+module Find
+  def self.find_by_slug(slug)
+    instance = nil
+    self.all.each do |s|
+      if i.slug == slug
+        instance = i
+      end
+    end
+    instance
+  end
+
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,18 +3,6 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
 
-  def slug
-    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-  end
-
-  def self.find_by_slug(slug)
-    song = nil
-    self.all.each do |s|
-      if s.slug == slug
-        song = s
-      end
-    end
-    song
-  end
+  include Slug
 
 end
