9b263e11cb79be0b248282bb057fb2a74b532c74
Reno Sorensen <renojames19@gmail.com>
2019-06-04 03:35:06 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+
+  def slug
+    slug = self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+
+
 end
