4ab0f01d3c503911402ad88d29da46f4490295fe
Monica Marziano <mmarziano1@gmail.com>
2019-06-04 23:48:51 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,37 +1,41 @@
 require 'rack-flash'
-  
+
 class SongsController < ApplicationController
   use Rack::Flash
-  
+
   enable :sessions
-  
+
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
-  end 
-  
-  get '/songs/new' do 
-    
+  end
+
+  get '/songs/new' do
+
     @genres = Genre.all
     erb :'songs/new'
-  end 
-  
-  post '/songs' do 
-    binding.pry
-    genres = []
-    @artist = Artist.find_or_create_by(name: params["song"]["name"])
-    @song = Song.create(name: params[:song_name])
-    @song.artist = @artist 
-    @genre = Genre.find_or_create_by(name: params[:song][][:genre])
-    @song.genre_id = @genre.id
+  end
+
+  post '/songs' do
+    @artist = Artist.find_or_create_by(name: params["song"]["artist"])
+    @song = Song.create(name: params["song"]["name"])
+    @song.artist = @artist
+      params["genres"].each do |genre_id|
+        if Genre.find(genre_id)
+          @song.genre_ids << genre_id
+        else
+          @genre = Genre.create(name: params["genre"])
+          @song.genre_ids << @genre.id
+        end
+      end
     @song.save
     erb :'songs/show'
-  end 
-  
-  get '/songs/:slug' do 
-    
+  end
+
+  get '/songs/:slug' do
+
     @song = Song.find_by_slug(params[:slug])
-    
+
     erb :'songs/show'
-  end 
+  end
 end
