9246aef8c29395d601afc35f10946e6416c0c416
Christiana Vandermale <christianavdm@gmail.com>
2019-06-04 16:37:53 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_191026) do
-
-  create_table "artist_genres", force: :cascade do |t|
-    t.integer "artist_id"
-    t.integer "genre_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_03_14_185853) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
