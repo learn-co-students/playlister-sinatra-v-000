09a3e891cf90654391ab452706d0983d53588d56
Christiana Vandermale <christianavdm@gmail.com>
2019-06-04 02:26:23 +0000

    Done.

--- db/migrate/20190314191026_create_artist_genres.rb
+++
@@ -1,9 +0,0 @@
-class CreateArtistGenres < ActiveRecord::Migration[4.2]
-  def change
-    create_table :artist_genres do |t|
-      t.integer :artist_id
-      t.integer :genre_id
-      t.timestamps null: false
-    end
-  end
-end
