d9aa32df16e782c345816ca97ff98a064443a5fc
Christiana Vandermale <christianavdm@gmail.com>
2019-06-04 02:20:37 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,20 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+#  extend Slugifiable::ClassMethods
+#  include Slugifiable::InstanceMethods
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    instance = nil
+    self.all.each do |i|
+      if i.slug == slug
+        instance = i
+      end
+    end
+    instance
+  end
 end
