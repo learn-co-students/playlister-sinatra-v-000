1fbe9c67bcdcc4edc98b0aeefd0513a4abfd47c5
Monica Marziano <mmarziano1@gmail.com>
2019-06-05 00:46:20 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,15 +20,20 @@ class SongsController < ApplicationController
     @artist = Artist.find_or_create_by(name: params["song"]["artist"])
     @song = Song.create(name: params["song"]["name"])
     @song.artist = @artist
-      params["genres"].each do |genre_id|
-        if Genre.find(genre_id)
-          @song.genre_ids << genre_id
-        else
-          @genre = Genre.create(name: params["genre"])
-          @song.genre_ids << @genre.id
+      if params["genres"] != nil
+        params["genres"].each do |genre_id|
+          if Genre.find(genre_id)
+            @song.genre_ids << genre_id
+          else
+            @genre = Genre.create(name: params["genre"])
+            @song.genre_ids << @genre.id
+          end
         end
+      else
+        @genre = Genre.create(name: params["genre"][0])
       end
     @song.save
+
     erb :'songs/show'
   end
 
