cdccad783d4fad3385502ae08ebcccabbbbfd408
Monica Marziano <mmarziano1@gmail.com>
2019-06-07 21:32:43 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,9 @@
+require 'sinatra/base'
 require 'rack-flash'
 
 class SongsController < ApplicationController
-  use Rack::Flash
-
   enable :sessions
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -23,24 +23,58 @@ class SongsController < ApplicationController
       if params["genres"] != nil
         params["genres"].each do |genre_id|
           if Genre.find(genre_id)
-            @song.genre_ids << genre_id
+            @song.genres << Genre.find(genre_id)
           else
             @genre = Genre.create(name: params["genre"])
-            @song.genre_ids << @genre.id
+            @song.genres << @genre
           end
         end
       else
         @genre = Genre.create(name: params["genre"][0])
+        @genre.save
       end
     @song.save
-
-    erb :'songs/show'
+    slug = @song.slug
+    flash[:message] = "Successfully created song."
+    redirect to  :"songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
-
     @song = Song.find_by_slug(params[:slug])
-
     erb :'songs/show'
   end
+
+  post '/songs/:slug/edit' do
+    @genres = Genre.all
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug/edit' do
+
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params["song"]["name"])
+      if params["song"]["artist"] != nil || params["song"]["artist"] != ""
+        @song.artist.name = params["song"]["artist"]
+      end
+    @song.genres.clear
+      if params["genres"] != nil
+        params["genres"].each do |genre_id|
+          if Genre.find(genre_id)
+            @song.genres << Genre.find(genre_id)
+          else
+            @genre = Genre.create(name: params["genre"])
+            @song.genres << @genre
+          end
+        end
+      else
+        @genre = Genre.create(name: params["genre"][0])
+        @genre.save
+      end
+      binding.pry
+  flash[:message] = "Successfully edited song."
+  redirect to "songs/#{@song.slug}"
+  end
+
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,19 +1,20 @@
-module Slugifiable 
+module Slugifiable
   module InstanceMethods
-    def slug  
-      @slug = self.name.split(' ').join('-')
-      
-    end 
+    def slug
+      @slug = self.name.downcase.split(' ').join('-')
+
+    end
   end
-  
+
   module ClassMethods
     def find_by_slug(slug)
+
       result = self.all.find {|t| t.slug == slug}
       #a = slug.split('-')
       #b = a.collect {|new| new.capitalize}
       #name = b.join(' ')
-      
-      search_result = self.find_by("name" => result.name)
-    end 
+
+      #search_result = self.find_by("name" => result.name)
+    end
   end
-end 
+end
