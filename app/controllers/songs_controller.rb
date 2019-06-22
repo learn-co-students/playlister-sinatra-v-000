2f289e247b258870f87960df515c58a677f843d6
BusterStatus <matthew.serre@gmail.com>
2019-06-21 20:39:21 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,10 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class SongsController < ApplicationController
+
+  enable :sessions
+  use Rack::Flash
   
   get '/songs' do
     @songs = Song.all
@@ -12,7 +18,8 @@ class SongsController < ApplicationController
   end
  
   post '/songs' do
-    @song = Song.create(name: params[:song]["name"])
+    @song = Song.create(params[:song])
+    @song.save
     if !params["genre"]["name"].empty?
       genre = Genre.find_or_create_by(name: params["genre"]["name"])
       @song.genre_id = genre.id
@@ -26,6 +33,7 @@ class SongsController < ApplicationController
     @song.save
     @song.song_genres.create(genre: genre)
     @song.save
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
   
@@ -43,7 +51,13 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find(params[:id])
     @song.update(params["artist"])
+    if !params["artist"]["name"].empty?
+      artist = Artist.find_or_create_by(name: params["artist"]["name"])
+      @song.artist_id = artist.id
+      @song.artist = artist
+    end
+    flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
-  
+
 end
