7ffd24b6169bb517e70b17b0dbe4d420416211e3
Paul Fell <swellpf@gmail.com>
2019-06-24 21:34:27 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,12 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
+  def slug
+    self.name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |instance| instance.slug == slug }
+  end
+
 end
