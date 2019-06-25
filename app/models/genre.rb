6634bd2284ebfd46669073024cdfd6fca38b1df6
Paul Fell <swellpf@gmail.com>
2019-06-24 21:36:20 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,12 @@ class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
   has_many :song_genres
 
+  def slug
+    self.name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |instance| instance.slug == slug }
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |instance| instance.slug == slug }
+  end
 end
