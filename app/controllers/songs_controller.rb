2a919932ea4a5535af273fb20f174f08a37b5323
taisaoko <ttd3calvin@gmail.com>
2019-07-05 22:05:34 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,18 @@
+require 'rack-flash'
+class SongsController < ApplicationController
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  
+
+  
+
+end
