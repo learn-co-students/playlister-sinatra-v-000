83a1c66ba00b56c64ba94f6ec67db0280b93cfaa
Scott Silverman <scottms724@gmail.com>
2019-07-10 17:09:22 +0000

    Done.

---
+++ app/models/genre.rb
@@ -0,0 +1,18 @@
+class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+   def slug
+    @name = self.name
+    @name.downcase.strip.gsub(' ', '-')
+    @name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+   def self.find_by_slug(slug)
+    @name = slug.split("-").map!{|word| word.capitalize}.join(" ")
+    @name = slug.gsub(' ', '-').upcase
+    self.find_by_name(@name)
+  end
+  
+end 
