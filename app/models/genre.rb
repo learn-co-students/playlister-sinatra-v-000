644ec4234716bbb1f54e821d6e1c98a5aab3c42c
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-16 21:52:28 +0000

    spec/models/03 passed

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -6,4 +6,14 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
 
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+
+   def self.find_by_slug(slug)
+     Genre.all.find do |genre|
+       genre.slug == slug
+     end
+   end
+
 end
