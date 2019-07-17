3946e1cc05c11575c02822f5e17ccbbf6541022b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-17 15:47:26 +0000

    Creates /genres routes

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,15 @@ class GenresController < ApplicationController
   get '/genres' do
     erb :"/genres/index"
   end
+
+  get '/genres/new' do
+    erb :"/genres/new"
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    puts @genre
+    erb :"/genres/show"
+  end
+
 end
