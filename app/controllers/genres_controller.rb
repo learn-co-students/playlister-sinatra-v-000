6da8b9eff6194f1577124d80f0a631d032bbe39c
Karly Mareka <karly.mareka@gmail.com>
2019-07-31 20:58:20 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,11 @@
+class GenresController < ApplicationController
+
+  get '/genres' do 
+    erb :'/genres/index'
+  end
+  
+  get '/genres/:slug' do 
+    erb :'/genres/show'
+  end 
+  
+end 
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,11 @@
+class SongsController < ApplicationController
+  
+  get '/songs' do 
+    erb :'/songs/index' 
+  end
+  
+  get '/songs/:slug' do 
+    erb :'/songs/show'
+  end 
+  
+end 
