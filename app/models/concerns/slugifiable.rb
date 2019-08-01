985438c3e8608ea27d7b5ee6c430393ac7f8a3fb
Karly Mareka <karly.mareka@gmail.com>
2019-08-01 02:38:24 +0000

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,11 @@
+class Slugifiable < ActiveRecord::Base 
+  
+  def slug(name) 
+    
+  end 
+  
+  def find_by_slug(name)
+    
+  end 
+  
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+    t.integer "genre_id"
   end
 
   create_table "genres", force: :cascade do |t|
