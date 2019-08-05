aaf37a384e1b5a50e340eb0df6d9ec7d5abbeac2
Karly Mareka <github email address>
2019-08-05 13:52:29 -0700

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,16 @@
 class Genre < ActiveRecord::Base
+  has_many :song_genres
+  has_many :songs, through: :song_genres
   has_many :artists
-  has_many :song_genres 
-  has_many :songs, through: :song_genres 
+  has_many :artists, through: :songs
+
+  def slug
+      self.name.gsub(/ /, '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    genre_name = slug.gsub(/-/, ' ')
+    genre_name = genre_name.titleize
+    Genre.find_by name: genre_name
+  end
 end
