95ac8cacca3f6482d5273db9190e474253be6fde
Steven Crouse <sdcrouse91@gmail.com>
2019-08-08 06:59:19 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_064006) do
+ActiveRecord::Schema.define(version: 2019_08_08_065540) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
