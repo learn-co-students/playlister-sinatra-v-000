463f8cff9ec5cfda36718229a7ae011545ac46b7
Steven Crouse <sdcrouse91@gmail.com>
2019-08-09 04:29:42 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,9 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :artist_genres
   has_many :genres, through: :artist_genres
+  
+  def slug
+    # Should return taylor-swift if the artist's name is "Taylor Swift"
+    # Replace blank spaces with -
+  end
 end
