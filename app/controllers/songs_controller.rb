d8d1af3658eccb8284b1f8036dc3e027988b872e
Amil Masic <amil.masic@gmail.com>
2019-08-10 12:09:36 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,17 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get 'songs/new' do
+      @song = Song.create(params[:song])
+      erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+end
