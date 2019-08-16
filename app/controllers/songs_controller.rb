045918ceeee34fa38abbf57403d20ce85b34846b
Sdcrouse <sdcrouse91@gmail.com>
2019-08-15 20:05:43 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,12 +7,22 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
     erb :"songs/new"
   end
   
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :"songs/show"
+  end
+
+  post '/songs' do
+    song = Song.new(params[:name])
+    # song.genre_ids = params[:genres] Something's wrong here.
+    song.artist = Artist.create(name: params[:artist_name])
+    song.save
+    #binding.pry
   end
   
 end
