624a2e2db37b69f64864e9d375253a1aebc7ab9d
github username <github email address>
2019-08-17 12:46:42 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,15 @@
+require "sinatra/base"
+require 'rack-flash'
+
+class GenresController < ApplicationController 
+
+    get '/genres' do 
+        @genres = Genre.all 
+        erb :'/genres/index'
+    end
+   
+    get '/genres/:slug' do 
+        @genre = Genre.find_by_slug(params[:slug])
+        erb :'genres/show'
+    end 
+end
