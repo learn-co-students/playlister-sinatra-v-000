88f968e93346227c500ee007c268a7f56e5b701a
Sdcrouse <sdcrouse91@gmail.com>
2019-08-19 21:35:30 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,19 +26,8 @@ class SongsController < ApplicationController
     # I'm not entirely sure that I'm doing this right; my params hash looks pretty weird.
 
     song = Song.create(params[:song])
+    song.artist = find_artist_from(params)
 
-    if !params[:artist_name].blank? # The user created a new (or existing) artist.
-      song.artist = Artist.find_or_create_by(name: params[:artist_name])
-    elsif params[:artist] # The user chose an existing artist.
-      song.artist = Artist.find_or_create_by(params[:artist])
-    else # The user didn't choose an artist or select an existing one.
-      song.destroy
-      redirect to "/error"
-      # The code below doesn't work because the code outside of this "if" statement is ALSO executed:
-      # flash[:error] = "You forgot to give your song an artist."
-      # erb :error
-    end
-   
     song.save    
     flash[:message] = "Successfully created song."
     # See https://github.com/treeder/rack-flash#sinatra for how to use Rack Flash.
@@ -60,31 +49,41 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
-    # Update the song with a new name and/or artist and/or genre(s)
-    # Things to account for (some of them being my own add-ons):
-      # If the user chose another artist from the list
-      # If the user made a new artist
-        # If the user's new artist already exists
-      # If the user chose another artist AND made a new artist
-      # Which genres the user checked
-      # Whether the user checked ANY genres
-    #binding.pry
+    # Update the song with a new name and/or artist and/or genre(s).
+    # This should probably be refactored into a few helper methods.
 
     song = Song.find_by_slug(params[:slug])
+    new_artist = find_artist_from(params)
 
-    # I need to structure my params hash better for the song's artist...
-    if !params[:artist_name].blank? # The user created a new (or existing) artist.
-      new_artist = Artist.find_or_create_by(name: params[:artist_name])
-    else # The user chose another existing artist or left the song's artist unchanged.
-      new_artist = Artist.find_or_create_by(params[:artist])
+    ######## Bug fix: set the genre_ids to [] if the genre_ids are nil. ########
+    if params[:song][:genre_ids].nil?
+      params[:song][:genre_ids] = []
     end
 
-    ####### Possible bug fix: set the genre_ids to [] if the genre_ids are nil. ########
-
-    song.update(name: params[:song][:name], artist: new_artist)
+    selected_genres = params[:song][:genre_ids].collect do |genre_id|
+      Genre.find_by_id(genre_id)
+    end
+    
+    # I need to structure my params hash better for the song's artist...
+    song.update(name: params[:song][:name], artist: new_artist, genres: selected_genres)
 
     flash[:message] = "Successfully updated song."
     redirect to "/songs/#{song.slug}"   
   end
-  
+
+  helpers do
+    def find_artist_from(params)      
+      if !params[:artist_name].blank? # The user created a new (or existing) artist.
+        Artist.find_or_create_by(name: params[:artist_name])
+      elsif params[:artist] # The user chose an existing artist (or possibly left the song's artist unchanged, if editing the song).
+        Artist.find_or_create_by(params[:artist])
+      else # The user didn't choose an artist or select an existing one.
+        song.destroy
+        redirect to "/error"
+        # The code below doesn't work because the code outside of this "if" statement is ALSO executed:
+        # flash[:error] = "You forgot to give your song an artist."
+        # erb :error
+      end
+    end
+  end  
 end
