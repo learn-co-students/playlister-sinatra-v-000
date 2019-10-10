57746234cefea95ae5b39cef058345af35c4fa70
Mohawk Greene <kellyecodes@gmail.com>
2019-10-10 15:57:54 -0400

    change song/new form format to match that of edit form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,13 +19,12 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(:name => params["Name"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
 
     flash[:message] = "Successfully created song."
-
     redirect("/songs/#{@song.slug}")
   end
 
