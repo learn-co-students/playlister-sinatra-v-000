dddc86bc435a1fc68aca3caf8cfc81f6011836da
Diapause <diapause@protonmail.com>
2019-10-25 14:55:01 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,27 +39,10 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
-    # binding.pry
-    #find song object by slug
     @song = Song.find_by_slug(params[:slug])
-
-    #update name change, if any
-    # if song.name != params['song']['name']
-    #    # song.update(name: params['song']['name'])
-    #    song.name = Song.find_or_create_by(name: params[:song][:name])
-    # end
     @song.update(params[:song])
-    #update artist change, if any
-    # if song.artist.name != params['artist']['name']
-       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    # end
-
-    #update genres, if any change
-      #list all genres with checked boxes that match DB
-    # if song.genre_ids != params['genres']
-      #if checked boxes differ, update the DB
-       @song.genre_ids = params['genres']
-    # end
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params['genres']
 
     @song.save
     flash[:message] = "Successfully Updated Song"
