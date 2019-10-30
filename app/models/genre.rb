b88db728158a03bc3e40d56876ec59439689f7df
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-10-30 21:53:56 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,11 @@
 class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
-  has_many :artist, through: :songs 
+  has_many :artist, through: :songs
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+
 end
