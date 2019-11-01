0c0b28c297d4feb9049b560d8957241498d91cdd
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-01 17:15:35 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,8 @@
+class SongsController < ApplicationController
+
+  # GET /songs do
+  #   @songs = Song.all
+  #   erb :/
+  # end
+
+end
---
+++ app/models/artist.rb
@@ -0,0 +1,15 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def slug
+    name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    all.find do |artist|
+      artist.slug == slug
+    end
+  end
+
+end
