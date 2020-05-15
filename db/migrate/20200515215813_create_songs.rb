a0b73f959444da9487f280788f5c93ecfa778064
github username <github email address>
2020-05-15 18:21:54 -0400

    Done.

--- db/migrate/20200515215813_create_songs.rb
+++ db/migrate/20200515215813_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :name
-      t.interger :artist_id
+      t.integer :artist_id
     end
   end
 end
--- db/migrate/20200515220057_create_song_genres.rb
+++ db/migrate/20200515220057_create_song_genres.rb
@@ -1,8 +1,8 @@
 class CreateSongGenres < ActiveRecord::Migration[5.2]
   def change
      create_table :song_genres do |t|
-      t.interger :song_id
-      t.interger :genre_id
+      t.integer :song_id
+      t.integer :genre_id
     end
   end
 end
