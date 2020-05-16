a5e9541db79a8937309e4b2413367e89f443d929
github username <github email address>
2020-05-15 22:01:46 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,7 @@ class SongsController < ApplicationController
     end
 
     get '/songs/:slug' do
-        @songs = Song.find_by(slug:params[:slug])
+        @song = Song.find_by(slug:params[:slug])
         erb :'/songs/show'
     end
 
