174722e64345b6b0cbf6d7d18c55af701bcf07bc
github username <github email address>
2020-05-15 22:51:43 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,10 +3,10 @@ class ArtistsController < ApplicationController
         @artists = Artist.all
         erb :'/artists/index'
     end
-    
+
     get '/artists/:slug' do
         @artist = Artist.find_by_slug(params[:slug])
-        erb :'/artists/show'
+        erb :'artists/show'
     end
 end
 
