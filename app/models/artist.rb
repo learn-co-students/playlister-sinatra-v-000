c125ddf57f4c56d44a81c7c67277bc7f645e4daf
github username <github email address>
2020-05-16 21:40:06 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
     has_many :song_genres
     has_many :genres, through: :songs
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,21 @@
 # Add seed data here. Seed your database with `rake db:seed`
+artist_1 = Artist.create(:name => "Taylor Swift")
+artist_2 = Artist.create(:name => "Jay Z")
+artist_3 = Artist.create(:name => "Tame Impala")
+
+song_1 = Song.create(name:"song_1", artist_id:artist_1.id) 
+song_2= Song.create(name:"song_2", artist_id:artist_2.id)
+song_3= Song.create(name:"song_3", artist_id:artist_3.id) 
+
+genre_1 = Genre.create(name:"pop")
+genre_2 = Genre.create(name:"rap")
+genre_3 = Genre.create(name:"electronic")
+
+SongGenre.create(song_id:song_1.id, genre_id:genre_1.id)
+SongGenre.create(song_id:song_2.id, genre_id:genre_2.id)
+SongGenre.create(song_id:song_3.id, genre_id:genre_3.id)
+
+
+
+
 
