2a3c4bba0fb012d2fea667d296ae28452cb68948
dejarvzal <diannejarvisj@gmail.com>
2020-05-20 07:06:09 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,4 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/songs' do
-     erb :index
-  end 
-  
-  get '/songs/:slug' do
-    # @song = Song.find_by_slug(params[:name])
-    #  erb :index
-  end
-
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,12 @@
 class SongsController < ApplicationController
+
+    get '/songs' do
+      erb :index
+    end 
+ 
+#  get '/songs/:slug' do
+   # @song = Song.find_by_slug(params[:name])
+   #  erb :index
+#  end
+
 end
