47e92ffe04d50ee6c828a5d5772877d99be912e8
dejarvzal <diannejarvisj@gmail.com>
2020-05-22 21:19:07 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,12 +39,26 @@ class SongsController < ApplicationController
       @song = Song.find_by_slug(params[:slug])
       # @songs = Song.all
        erb :"/songs/edit"
-      # erb :"/songs/#{@song.slug}/edit"
+
    end
 
    patch '/songs/:slug/edit' do 
-      erb :"/songs/edit"
-      # erb :"/songs/#{@song.slug}/edit"
+      if !Artist.find_by_name(params[:artist][:name])
+         @artist = Artist.update(params[:artist])
+         end
+         # @song = Song.create(params[:song])
+         # @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+         # if params[:genres] != nil
+         #       params[:genres].each do |id|
+         #       @song.genres << Genre.find(id)
+         #       end
+         #    end
+         @song.save
+         # flash[:message] = "Successfully created song."
+         redirect to ("/songs/#{@song.slug}")
+
+
+      # erb :"/songs/edit"
    end
 
 end
