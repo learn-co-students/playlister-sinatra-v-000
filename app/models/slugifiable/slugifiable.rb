fbd5351beaa46695fc2cfdf2efaa55100bc0f7b3
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-07-15 19:09:26 -0700

    Updated slugifiable method

--- app/models/slugifiable/slugifiable.rb
+++ app/models/slugifiable/slugifiable.rb
@@ -3,8 +3,19 @@ module Slugifiable
     self.name.downcase.gsub!(/[^a-zA-Z]/, "-")
   end
 
+  #def find_by_slug(slug)
+  #  name = slug.split("-").each {|i| i.capitalize!}.join(" ")
+  #  self.find_by_name(name)
+  #end
+
   def find_by_slug(slug)
-    name = slug.split("-").each {|i| i.capitalize!}.join(" ")
-    self.find_by_name(name)
+    match = ""
+    self.all.each do |i|
+      if i.slug == slug
+        match = i
+      end
+    end
+    match
   end
+
 end
