6cff1767b13cfc7540fb994a1447e5d7788661e1
Laura Messa Travin <laura.travin@gmail.com>
2021-03-01 01:24:13 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,13 @@
-class Artist < ActiveRecord::BASE
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, :through => :songs
 
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find{|artist| artist.slug == slug}
+  end
 end
