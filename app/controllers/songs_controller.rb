f395901935c615a4216ed372ca961735491c21c6
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-28 02:02:26 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,11 +8,8 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @artist = Artist.find(@song.artist_id)
-    @genres = []
-    @song.genres.each do |genre|
-      @genres << genre.name
-    end
+    @artist = @song.artist
+    @genres = @song.genres
 
     erb :'songs/show'
   end
