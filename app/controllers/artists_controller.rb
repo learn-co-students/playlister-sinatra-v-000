4a1a2bb69991fde665602da2dbcbd149c0176c8c
Jesse Wenger <wengerj@gmail.com>
2021-04-12 13:52:46 +0000

    add db and models

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,10 @@
 class ArtistsController < ApplicationController
+  get '/artists' do
+    @artists = Artist.all
+    erb :'artists/index'
+  end
+
+  get '/artists/:id' do
+    erb :'artists/show'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,19 @@
 # Add seed data here. Seed your database with `rake db:seed`
+#mumford = Artist.create(name: "Mumford & Sons")
+#folk = Genre.create(name: "Folk")
+#Song.create(name: "I Will Wait",  artist: mumford, genre: folk)
 
+path = "./db/data"
+files = Dir.glob(path + "/*.mp3").collect{|file| file.split(/.+\//).last}
+artist = Artist.all
+genre = Genre.all
+
+files.each do |f|
+  f_ary = f.split(/\s[\-]\s| \[|\].mp3/)
+    f_artist = f_ary[0]
+    f_song = f_ary[1]
+    f_genre = f_ary[2]
+  a = Artist.find_or_create_by(name: f_artist)
+  g = Genre.find_or_create_by(name: f_genre)
+  Song.find_or_create_by(name: f_song,  artist: a, genre: g)
+end
