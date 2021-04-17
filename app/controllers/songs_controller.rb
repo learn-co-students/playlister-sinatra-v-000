5904d185b7dc1354c11b9b3d6c1b535f7e16cf00
Jesse Wenger <wengerj@gmail.com>
2021-04-17 16:47:32 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,10 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+
+enable :sessions
+use Rack::Flash
+
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
@@ -23,11 +29,34 @@ class SongsController < ApplicationController
     end
     @song.artist = artist
     @song.save
+    flash[:message] = "Successfully created song."
     redirect to "songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
-    @song = Song.find_by(name: params[:slug])
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
     erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params[:song_name])
+    @song.artist.update(name: params[:artist_name])
+    @song.genres.each do |genre|
+      genre_id_string = genre.id.to_s
+      if !params[:genres].include?(genre_id_string)
+        @song.genres.delete(genre.id)
+      end
+    end
+    params[:genres].each do |genre|
+      p_genre = Genre.find_by(id: genre.to_i)
+      if !@song.genres.include?(p_genre)
+        @song.song_genres.build(genre: p_genre)
+        @song.save
+      end
+    end
+    flash[:message] = "Successfully updated song."
+    redirect to "songs/#{@song.slug}"
   end
 end
